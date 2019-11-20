4fd9c83b47b874650e045efe934f1f14bf7dc498
Robert Bartel <robert.bartel@noaa.gov>
2019-11-20 10:04:26 -0600

    Refactoring up_stack.sh a bit, and also adding logic to add new Docker network (if necessary) primarily for communication with the request handler.

--- up_stack.sh
+++ up_stack.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 NAME=$(basename "${0}")
+DOCKER_READY_PUSH_REGISTRY_TIME=0
 
 usage()
 {
@@ -24,6 +25,9 @@ Options
     --no-deploy             Build the image(s) and push to registry, but do not deploy
 
     --skip-registry         Skip the step of pushing built images to registry
+
+    --no-internal-registry  Do not check for or start an internal Docker registry (requires
+                            all pushes/pulls be from other configured registries)
 "
     echo "${_O}" 1>&2
 }
@@ -56,10 +60,16 @@ DOCKER_STACK_NAME=nwm
 DOCKER_MPI_NET_NAME=mpi-net
 DOCKER_MPI_NET_SUBNET=10.0.0.0/24
 DOCKER_MPI_NET_GATEWAY=10.0.0.1
+DOCKER_MPI_NET_VLAN=4097
+
+DOCKER_REQUESTS_NET_NAME=requests-net
+DOCKER_REQUESTS_NET_SUBNET=10.0.1.0/27
+DOCKER_REQUESTS_NET_GATEWAY=10.0.1.1
 
 DOCKER_HOST_IMAGE_STORE=${_STORE}
 DOCKER_VOL_DOMAINS=${_DOMAINS_DIR}
 
+DOCKER_INTERNAL_REGISTRY_STACK_NAME:=dev_registry_stack
 DOCKER_INTERNAL_REGISTRY_HOST=127.0.0.1
 DOCKER_INTERNAL_REGISTRY_PORT=5000
 
@@ -78,18 +88,6 @@ EOF
     fi
 }
 
-create_docker_mpi_net()
-{
-    docker network create \
-        -d overlay \
-        --scope swarm \
-        --attachable \
-        -o "{\"com.docker.network.driver.overlay.vxlanid_list\": \"4097\"}" \
-        --subnet ${DOCKER_MPI_NET_SUBNET} \
-        --gateway ${DOCKER_MPI_NET_GATEWAY} \
-        mpi-net
-}
-
 # Work-around to use the .env file loading for compose files when deploying with docker stack (which doesn't by itself)
 # See: https://github.com/moby/moby/issues/29133
 deploy_docker_stack_from_compose_using_env()
@@ -97,11 +95,74 @@ deploy_docker_stack_from_compose_using_env()
     local _COMPOSE_FILE="${1}"
     local _STACK_NAME="${2}"
 
-    if docker-compose -f "${_COMPOSE_FILE}" config > /dev/null; then
-        docker stack deploy --compose-file <(docker-compose -f "${_COMPOSE_FILE}" config) "${_STACK_NAME}"
+    if docker-compose -f "${_COMPOSE_FILE}" config > /dev/null 2>&1; then
+        docker stack deploy --compose-file <(docker-compose -f "${_COMPOSE_FILE}" config 2>/dev/null) "${_STACK_NAME}"
     else
         echo "Error: invalid docker-compose file; cannot start stack; exiting"
         exit 1
+    fi
+}
+
+init_if_not_exist_docker_networks()
+{
+    # Make sure the Docker mpi-net network is defined
+    if [[ $(docker network ls --filter name=${DOCKER_MPI_NET_NAME:=mpi-net} -q | wc -l) -eq 0 ]]; then
+        docker network create \
+            -d overlay \
+            --scope swarm \
+            --attachable \
+            -o "{\"com.docker.network.driver.overlay.vxlanid_list\": \"${DOCKER_MPI_NET_VLAN:=4097}\"}" \
+            --subnet ${DOCKER_MPI_NET_SUBNET} \
+            --gateway ${DOCKER_MPI_NET_GATEWAY} \
+            ${DOCKER_MPI_NET_NAME}
+    fi
+
+    # Make sure the Docker requests-net network is defined
+    if [[ $(docker network ls --filter name=${DOCKER_REQUESTS_NET_NAME:=requests-net} -q | wc -l) -eq 0 ]]; then
+        docker network create \
+            -d bridge \
+            --attachable \
+            --subnet ${DOCKER_REQUESTS_NET_SUBNET} \
+            --gateway ${DOCKER_REQUESTS_NET_GATEWAY} \
+            ${DOCKER_REQUESTS_NET_NAME:=requests-net}
+    fi
+}
+
+init_registry_service_if_needed()
+{
+    # Make sure the internal Docker image registry container is running if configured to use it
+
+    [[ -z "${DOCKER_INTERNAL_REGISTRY_STACK_NAME:-}" ]] && DOCKER_INTERNAL_REGISTRY_STACK_NAME=dev_registry_stack
+    [[ -z "${DOCKER_INTERNAL_REGISTRY_SERVICE_NAME:-}" ]] && DOCKER_INTERNAL_REGISTRY_SERVICE_NAME="${DOCKER_INTERNAL_REGISTRY_STACK_NAME}_registry"
+
+    if [[ ${DO_SKIP_INTERNAL_REGISTRY} == 'true' ]]; then
+        echo "Options set to not use internal Docker registry for pushing or pulling of images; skipping init steps"
+    elif [[ $(docker stack services -q --filter "name=${DOCKER_INTERNAL_REGISTRY_SERVICE_NAME}" "${DOCKER_INTERNAL_REGISTRY_STACK_NAME}" | wc -l) -eq 0 ]]; then
+        echo "Starting internal Docker registry"
+        #docker stack deploy --compose-file docker-registry.yml "${DOCKER_STACK_NAME}"
+        deploy_docker_stack_from_compose_using_env docker-registry.yml "${DOCKER_INTERNAL_REGISTRY_STACK_NAME}"
+        # If starting, set our "ready-to-push" time to 5 seconds in the future
+        DOCKER_READY_PUSH_REGISTRY_TIME=$((5+$(date +%s)))
+    else
+        echo "Internal Docker registry is online"
+        # If the registry was already started, we can push starting right now
+        DOCKER_READY_PUSH_REGISTRY_TIME=$(date +%s)
+    fi
+}
+
+build_docker_images()
+{
+    echo "Building custom Docker images"
+    if [[ -n "${DO_UPDATE:-}" ]]; then
+        if ! docker-compose -f docker-build.yml build --no-cache nwm; then
+            echo "Previous build command failed; exiting"
+            exit 1
+        fi
+    else
+        if ! docker-compose -f docker-build.yml build; then
+            echo "Previous build command failed; exiting"
+            exit 1
+        fi
     fi
 }
 
@@ -145,7 +206,10 @@ while [[ ${#} -gt 0 ]]; do
             exit
             ;;
         --skip-registry)
-            DO_SKIP_REGISTRY='true'
+            DO_SKIP_REGISTRY_PUSH='true'
+            ;;
+        --no-internal-registry)
+            DO_SKIP_INTERNAL_REGISTRY='true'
             ;;
         --no-deploy)
             DO_SKIP_DEPLOY='true'
@@ -163,44 +227,17 @@ done
 
 # TODO: consider doing some sanity checking on sourced .env values
 
-# Make sure the Docker mpi-net network is defined
-if [[ $(docker network ls --filter name=mpi-net -q | wc -l) -eq 0 ]]; then
-    create_docker_mpi_net
-fi
+init_if_not_exist_docker_networks
 
 # Make sure the local SSH keys are generated for the base image
 [[ ! -d ./base/ssh ]] && mkdir ./base/ssh
 [[ ! -e ./base/ssh/id_rsa ]] && ssh-keygen -t rsa -N '' -f ./base/ssh/id_rsa
 
-# Make sure if we start the registry here that we give it a little time to come all the way up before pushing
-# TODO: maybe change this to a health check later
-DOCKER_READY_PUSH_REGISTRY_TIME=0
-# Make sure the internal Docker image registry container is running
-if [[ $(docker stack services -q --filter "name=${DOCKER_STACK_NAME}_registry" "${DOCKER_STACK_NAME}" | wc -l) -eq 0 ]]; then
-    echo "Starting internal Docker registry"
-    #docker stack deploy --compose-file docker-registry.yml "${DOCKER_STACK_NAME}"
-    deploy_docker_stack_from_compose_using_env docker-registry.yml "${DOCKER_STACK_NAME}"
-    # If starting, set our "ready-to-push" time to 5 seconds in the future
-    DOCKER_READY_PUSH_REGISTRY_TIME=$((5+$(date +%s)))
-else
-    echo "Internal Docker registry is online"
-    # If the registry was already started, we can push starting right now
-    DOCKER_READY_PUSH_REGISTRY_TIME=$(date +%s)
-fi
 
-echo "Building custom Docker images"
-#docker-compose -f docker-build.yml build
-if [[ -n "${DO_UPDATE:-}" ]]; then
-    if ! docker-compose -f docker-build.yml build --no-cache nwm; then
-        echo "Previous build command failed; exiting"
-        exit 1
-    fi
-else
-    if ! docker-compose -f docker-build.yml build; then
-        echo "Previous build command failed; exiting"
-        exit 1
-    fi
-fi
+init_registry_service_if_needed
+
+# Build (or potentially just update) our images
+build_docker_images
 
 # Bail here if option set to only build
 if [[ -n "${DO_BUILD_ONLY:-}" ]]; then
@@ -209,8 +246,10 @@ fi
 # Otherwise, proceed ...
 
 # Push to registry (unless we should skip)
-if [[ -z "${DO_SKIP_REGISTRY:-}" ]]; then
+if [[ -z "${DO_SKIP_REGISTRY_PUSH:-}" ]]; then
     # Make sure we wait until the "ready-to-push" time determine in the logic for starting the registry service
+    # Make sure if we start the registry here that we give it a little time to come all the way up before pushing
+    # TODO: maybe change this to a health check later
     while [[ $(date +%s) -lt ${DOCKER_READY_PUSH_REGISTRY_TIME} ]]; do
         sleep 1
     done
