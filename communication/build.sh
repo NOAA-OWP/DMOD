7f4889574de27ba44f95dd404d9ce268ca594054
Robert Bartel <robert.bartel@noaa.gov>
2019-11-26 09:25:50 -0600

    Updating communications package dist build helper script to use bourne shell, since Bash may not always be available in container images.

--- communication/build.sh
+++ communication/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 NAME="`basename ${0}`"
 
@@ -13,7 +13,7 @@ usage()
 
 get_egg_info_dir()
 {
-    if [[ -z "${EGG_DIR:-}" ]]; then
+    if [ -z "${EGG_DIR:-}" ]; then
         EGG_DIR="${BASE_DIR:?}/$(ls ${BASE_DIR:?} | grep -e '.*\.egg-info$')"
     fi
 }
@@ -26,27 +26,27 @@ build()
 clean_all()
 {
     python setup.py clean --all 2>/dev/null
-    [[ -d ${BASE_DIR:?}/build ]] && echo "Removing ${BASE_DIR}/build" && rm -r ${BASE_DIR}/build
-    [[ -d ${BASE_DIR:?}/dist ]] && echo "Removing ${BASE_DIR}/dist" && rm -r ${BASE_DIR}/dist
+    [ -d ${BASE_DIR:?}/build ] && echo "Removing ${BASE_DIR}/build" && rm -r ${BASE_DIR}/build
+    [ -d ${BASE_DIR:?}/dist ] && echo "Removing ${BASE_DIR}/dist" && rm -r ${BASE_DIR}/dist
 
     get_egg_info_dir
 
-    [[ -d ${EGG_DIR:?} ]] && echo "Removing ${EGG_DIR}" && rm -r "${EGG_DIR}"
+    [ -d ${EGG_DIR:?} ] && echo "Removing ${EGG_DIR}" && rm -r "${EGG_DIR}"
 }
 
-while [[ ${#} -gt 0 ]]; do
+while [ ${#} -gt 0 ]; do
     case "${1}" in
         -h|--help|-help)
             usage
             exit
             ;;
         -d|--base_dir)
-            [[ -n "${BASE_DIR:-}" ]] && usge && exit 1
+            [ -n "${BASE_DIR:-}" ] && usge && exit 1
             BASE_DIR="${2}"
             shift
             ;;
         build|clean)
-            [[ -n "${ACTION:-}" ]] && usge && exit 1
+            [ -n "${ACTION:-}" ] && usge && exit 1
             ACTION="${1}"
             ;;
         *)
@@ -57,12 +57,12 @@ while [[ ${#} -gt 0 ]]; do
    shift
 done
 
-[[ -z "${BASE_DIR:-}" ]] && BASE_DIR=$(cd $(dirname ${0}); pwd)
-[[ ! -d "${BASE_DIR:?}" ]] && echo "Error: base dir ${BASE_DIR} does not exist" 1>&2 && usage && exit
+[ -z "${BASE_DIR:-}" ] && BASE_DIR="$(cd "$(dirname "${0}")"; pwd)"
+[ ! -d "${BASE_DIR:?}" ] && echo "Error: base dir ${BASE_DIR} does not exist" 1>&2 && usage && exit
 
-if [[ "${ACTION}" == "build" ]]; then
+if [ "${ACTION}" == "build" ]; then
     build
-elif [[ "${ACTION}" == "clean" ]]; then
+elif [ "${ACTION}" == "clean" ]; then
     clean_all
 else
     echo "Error: unknown action '${ACTION}'"
