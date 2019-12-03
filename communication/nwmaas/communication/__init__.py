cb4136b5eddb10534bfaf1796668309b0e668149
Robert Bartel <robert.bartel@noaa.gov>
2019-12-03 13:53:19 -0600

    In communication package, updating version, and updating package init to include the __version__ variable.

--- communication/nwmaas/communication/__init__.py
+++ communication/nwmaas/communication/__init__.py
@@ -3,6 +3,7 @@ from .maas_request import get_available_models, get_available_outputs, get_distr
     get_request, Distribution, MaaSRequest, NWMRequest, Scalar
 from .message import MessageEventType, Message, Response
 from .validator import SessionInitMessageJsonValidator, NWMRequestJsonValidator, MessageJsonValidator
+from ._version import __version__
 from .websocket_interface import EchoHandler, NoOpHandler, WebSocketInterface
 
 name = 'communication'
