48770690a167d47b6f1db59edd54e6b79c742e12
shengting cui <shengting.cui@nwcal-apd-dev1.nwc.nws.noaa.gov>
2019-12-04 16:56:08 +0000

    Added a function metadata_mgmt to reduce code duplication. Added a function job_allocation_and_setup that simplifies test_scheduler(). Modified scheduler.py and entry.sh so that two duplicate domains can be run, see comment in entry.sh for operation details.
    Conflicts:
    	scheduler/src/scheduler.py

--- scheduler/entry.sh
+++ scheduler/entry.sh
@@ -6,37 +6,47 @@
 # INSTEAD, simply run the code that relies on the docker socket via sudo
 #
 ## sudo python3 scheduler.py
+
 cd ..
 sudo python3 -m scheduler.src.scheduler
+cd scheduler
+mv ../hostfile .
+sudo chown mpi:mpi hostfile
 #
 # cd ..
 # sudo python3 -m unittest discover -v > unittest_outfile
 echo
 echo "back from python3 scheduler.py"
 
-sleep 30
+# If run_option=2, run 2 domains, otherwise, skip the following block of codes
+# For the moment, this is set manually in this script
+run_option=0        # Also, set run_option = 1 in scheduler.py to run run the 2 domain case
 
-# Some hackery to get things into workers correctly
-# workers=`cat hostfile`
-# master=`cat masterfile`
+if [ $run_option -eq 2 ]; then
 
-nwm_dir=/nwm
-domain_location=/nwm/domains
-run_dir=$domain_location/example_case/Gridded
-domain_dir=$run_dir/DOMAIN
-restart_dir=$run_dir/RESTART
-# ref_dir=$run_dir/referenceSim
-# obs_dir=$domain_dir/nudgingTimeSliceObs
+  # The following codes are for running another copy of the domain.
+  # Can be commented out if running only one copy which is handled through the scheduler.py code
+  sleep 60
 
-#ssh -t $master "sudo cp /nwm/wrf_hydro_nwm_public/trunk/NDHMS/Run/*.TBL $run_dir"
+  # Some hackery to get things into workers correctly
+  nwm_dir=/nwm
+  domain_location=/nwm/domains
+  run_dir=$domain_location/example_case1/NWM
 
+  # temporary woker container name: "nwm_mpi-worker_tmp0"
+  # should be made more general/systematic in future
+  worker0=nwm_mpi-worker_tmp0
+  src_dir=$domain_location/example_case/NWM
 
-# ssh $master "cd $run_dir && /usr/local/bin/mpirun -n 2 -hosts nwm_mpi-worker_tmp0 ./wrf_hydro.exe &" &
+  # Using hostfile to determine the total number CPUs for the MPI processes
+  total_cpus=0
+  for host in `cat hostfile`; do
+      cpus=`echo $host | cut -d ':' -f 2`
+      ((total_cpus = total_cpus+$cpus))
+  done
 
-# ssh $master "cd $run_dir && /usr/local/bin/mpirun -n 2 -hosts nwm_mpi-worker_tmp0,nwm_mpi-worker_tmp1 ./wrf_hydro.exe &" &
-# run_dir1=$domain_location/example_case1/Gridded
-# ssh $master "cd $run_dir1 && /usr/local/bin/mpirun -n 2 -hosts nwm_mpi-worker_tmp0,nwm_mpi-worker_tmp1 ./wrf_hydro.exe"
+  exec_dir=/nwm/wrf_hydro_nwm_public/trunk/NDHMS/Run
+  ssh $worker0 "cd $run_dir && cp $src_dir/hostfile . && /usr/local/bin/mpirun -f hostfile -n $total_cpus $exec_dir/wrf_hydro.exe"
+fi
 
-# scp hostfile  mpi@$master:$domain_location/example_case/Gridded
-# ssh $master "cd $run_dir && /usr/local/bin/mpirun -f hostfile -n $total_cpus ./wrf_hydro.exe"
 sudo /usr/sbin/sshd -D    # This command keeps the scheduler up
