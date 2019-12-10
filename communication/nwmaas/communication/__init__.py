3745d4feec513e8ab2ad594623ad5424f041f4af
Robert Bartel <robert.bartel@noaa.gov>
2019-12-10 09:33:37 -0600

    Updates to communication package as part of larger process of incorporating the communications package into the requests and scheduler packages: - updating package __init__.py to expose SchedulerClient,   SchedulerRequestMessage, and SchedulerRequestResponse classes

--- communication/nwmaas/communication/__init__.py
+++ communication/nwmaas/communication/__init__.py
@@ -1,8 +1,9 @@
 from ._version import __version__
-from .client import MaasRequestClient
+from .client import MaasRequestClient, SchedulerClient
 from .maas_request import get_available_models, get_available_outputs, get_distribution_types, get_parameters, \
     get_request, Distribution, MaaSRequest, NWMRequest, NWMRequestResponse, Scalar
 from .message import MessageEventType, Message, Response, InvalidMessage, InvalidMessageResponse
+from .scheduler_request import SchedulerRequestMessage, SchedulerRequestResponse
 from .session import Session, FullAuthSession, SessionInitMessage, SessionInitResponse, SessionManager, RedisBackendSessionManager
 from .validator import SessionInitMessageJsonValidator, NWMRequestJsonValidator, MessageJsonValidator
 from .websocket_interface import EchoHandler, NoOpHandler, WebSocketInterface, WebSocketSessionsInterface
