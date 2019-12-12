caf272982ba967b0a6c54f07fc9c309f85e44b11
Robert Bartel <robert.bartel@noaa.gov>
2019-12-12 17:09:10 -0600

    Updating setup.py for request handler package to reflect change to namespace package.

--- request_handler/setup.py
+++ request_handler/setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 with open('README.md', 'r') as readme:
     long_description = readme.read()
 
-exec(open('request_handler/_version.py').read())
+exec(open('nwmaas/request_handler/_version.py').read())
 
 setup(
-    name='request_handler',
+    name='nwmaas-request-handler',
     version=__version__,
     description='',
     long_description=long_description,
@@ -15,5 +15,5 @@ setup(
     url='',
     license='',
     install_requires=['websockets', 'nwmaas-communication>=0.1.3'],
-    packages=find_packages(exclude=('tests', 'schemas', 'ssl', 'src'))
+    packages=find_namespace_packages(exclude=('tests', 'schemas', 'ssl', 'src'))
 )
