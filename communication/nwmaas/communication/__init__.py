adc7baa0f993a6e712d0c05ee4787bd1c09765ac
Robert Bartel <robert.bartel@noaa.gov>
2019-12-13 13:43:20 -0600

    Updating nwmaas.communication package to import/expose newly created FailedSessionInitInfo class and other directly related SessionInitFailureReason class.

--- communication/nwmaas/communication/__init__.py
+++ communication/nwmaas/communication/__init__.py
@@ -4,7 +4,8 @@ from .maas_request import get_available_models, get_available_outputs, get_distr
     get_request, Distribution, MaaSRequest, NWMRequest, NWMRequestResponse, Scalar
 from .message import MessageEventType, Message, Response, InvalidMessage, InvalidMessageResponse
 from .scheduler_request import SchedulerRequestMessage, SchedulerRequestResponse
-from .session import Session, FullAuthSession, SessionInitMessage, SessionInitResponse, SessionManager, RedisBackendSessionManager
+from .session import Session, FullAuthSession, SessionInitMessage, SessionInitResponse, FailedSessionInitInfo, \
+    SessionInitFailureReason, SessionManager, RedisBackendSessionManager
 from .validator import SessionInitMessageJsonValidator, NWMRequestJsonValidator, MessageJsonValidator
 from .websocket_interface import EchoHandler, NoOpHandler, WebSocketInterface, WebSocketSessionsInterface
 
