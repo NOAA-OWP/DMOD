fe6b22378b83be398c99db2e654ce1593671b292
Robert Bartel <robert.bartel@noaa.gov>
2019-12-16 12:19:57 -0600

    Adding entrypoint.sh script for scheduler service image, in the template of the like-named script used for other images.

---
+++ scheduler/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#set -e
+
+# A virtual environment path may be supplied using this environmental shell variable
+if [ -n "${VENV_DIR:-}" ]; then
+    # Initialize if virtual environment directory either doesn't exists, or exists but is empty ...
+    if [ ! -d "${VENV_DIR}" ] || [ $(find "${VENV_DIR}" -maxdepth 0 -empty 2>/dev/null | wc -l) -eq 1 ]; then
+        python -m venv "${VENV_DIR}"
+    fi
+    . "${VENV_DIR}/bin/activate"
+    pip install --update -r /code/requirements.txt
+fi
+
+#set +e
+
+python -m ${PACKAGE_NAME:?} \
+    --port ${LISTEN_PORT:?} \
+    --ssl-dir ${SERVICE_SSL_DIR:?}
