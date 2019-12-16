294c23fd8c723aa4ad9f7d1efbfefe008a40483f
Robert Bartel <robert.bartel@noaa.gov>
2019-12-16 11:08:56 -0600

    Updating scheduler package with setup.py file, customized build.sh build script (using similar template as other packages), and other necessary files to get initial builds of package distribution files working.

---
+++ scheduler/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_namespace_packages
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+exec(open('nwmaas/scheduler/_version.py').read())
+
+setup(
+    name='nwmaas-scheduler',
+    version=__version__,
+    description='',
+    long_description=long_description,
+    author='',
+    author_email='',
+    url='',
+    license='',
+    install_requires=['docker', 'redis', 'Faker', 'websockets', 'nwmaas-communication>=0.1.3'],
+    packages=find_namespace_packages(exclude=('tests', 'test', 'deprecated', 'conf', 'schemas', 'ssl', 'src'))
+)
