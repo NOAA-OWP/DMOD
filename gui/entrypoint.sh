40d1b61d14b974532f7b5782f992d517abdb96e8
Nels Frazier <nels.frazier@noaa.gov>
2019-12-20 14:47:43 +0000

    Enable logfile writing for guniucorn

--- gui/entrypoint.sh
+++ gui/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-
+export PYTHONUNBUFFERED=1
 # Execute the migration scripts on the designated database
 python manage.py migrate
 
@@ -9,4 +9,4 @@ python manage.py collectstatic --no-input
 
 # Run the commands passed in from elsewhere
 #exec "$@"
-gunicorn maas_experiment.wsgi:application --bind 0.0.0.0:8000
+gunicorn maas_experiment.wsgi:application --bind 0.0.0.0:8000 --access-logfile '-' --error-logfile '-'
