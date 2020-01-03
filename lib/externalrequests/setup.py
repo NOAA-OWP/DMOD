c9c740c1bc757c6679a840b943912e6dbb6bfaaa
Robert Bartel <robert.bartel@noaa.gov>
2020-01-03 11:36:16 -0600

    Bumping access and externalrequests packages to require communication package version 0.2.0.

--- lib/externalrequests/setup.py
+++ lib/externalrequests/setup.py
@@ -14,6 +14,6 @@ setup(
     author_email='',
     url='',
     license='',
-    install_requires=['websockets', 'nwmaas-communication>=0.1.3', 'nwmaas-access>=0.1.0'],
+    install_requires=['websockets', 'nwmaas-communication>=0.2.0', 'nwmaas-access>=0.1.0'],
     packages=find_namespace_packages(exclude=('tests', 'schemas', 'ssl', 'src'))
 )
