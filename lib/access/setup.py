49b2a5396ab3e693d65c120617bf29d8b239cf69
Robert Bartel <robert.bartel@noaa.gov>
2020-01-03 10:55:33 -0600

    Creating nwmaas.access library package and initializing its version

---
+++ lib/access/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_namespace_packages
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+exec(open('nwmaas/access/_version.py').read())
+
+setup(
+    name='nwmaas-access',
+    version=__version__,
+    description='Library package with service-side classes for handling client-side access details',
+    long_description=long_description,
+    author='',
+    author_email='',
+    url='',
+    license='',
+    install_requires=['websockets', 'nwmaas-communication>=0.1.3'],
+    packages=find_namespace_packages(exclude=('tests', 'schemas', 'ssl', 'src'))
+)
