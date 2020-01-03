c74bd28c9def9699a12d64b4a6d9e8b996b01210
Robert Bartel <robert.bartel@noaa.gov>
2020-01-03 09:12:00 -0600

    Adding new AbstractRequestHandler class to communication package.

---
+++ lib/communication/nwmaas/communication/request_handler.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from .message import AbstractInitRequest, Response
+
+
+class AbstractRequestHandler(ABC):
+
+    @abstractmethod
+    async def handle_request(self, request: AbstractInitRequest, **kwargs) -> Response:
+        """
+        Handle the given request message as is appropriate for the implementation, and return the resulting ``Response``
+        of the appropriate type.
+
+        Parameters
+        ----------
+        request: AbstractInitRequest
+            A ``AbstractInitRequest`` message instance to be handled
+
+        kwargs: dict
+            A ``dict`` of other arguments appropriate for the particular implementation
+
+        Returns
+        -------
+        response: Response
+            An appropriate ``Response`` object.
+        """
+        pass
