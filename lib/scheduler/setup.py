587280708e7afba17a29464471063c8bfef9a4c3
Robert Bartel <robert.bartel@noaa.gov>
2020-01-06 13:21:06 -0600

    Adding new scheduler library package and copying over utils/ scripts sub- directory and main scheduler.py file (and Scheduler class).

---
+++ lib/scheduler/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_namespace_packages
+
+try:
+    with open('README.md', 'r') as readme:
+        long_description = readme.read()
+except:
+    long_description = ''
+
+exec(open('nwmaas/scheduler/_version.py').read())
+
+setup(
+    name='nwmaas-scheduler',
+    version=__version__,
+    description='',
+    long_description=long_description,
+    author='',
+    author_email='',
+    url='',
+    license='',
+    install_requires=['docker', 'redis', 'Faker', 'nwmaas-communication>=0.2.0'],
+    packages=find_namespace_packages(exclude=('test', 'src'))
+)
