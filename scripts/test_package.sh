8a9d51acc7ec329ec2b571a26198204888dc4a83
Robert Bartel <robert.bartel@noaa.gov>
2020-01-13 13:20:24 -0600

    Adding options to test_package.sh to manually run just setup or just teardown for integration testing, without any tests.

--- scripts/test_package.sh
+++ scripts/test_package.sh
@@ -40,6 +40,20 @@ Options:
         files named '${DEFAULT_INTEGRATION_TEST_FILE_PATTERN}') instead of typical unit tests
         (identified as being in files named '${DEFAULT_UNIT_TEST_FILE_PATTERN}')
 
+    --setup-it
+        Rather than run any tests, just execute the setup
+        logic for integration tests for this package, without
+        any teardown logic.  The expectation is that this be
+        run to facilitate IDE-based execution (and debugging)
+        of tests.
+
+    --teardown-it
+        Rather than run any tests, just execute the teardown
+        logic for integration tests for this package, without
+        any setup logic.  The expectation is that this be run
+        once any desired testing is finished after having run
+        --setup-it as described above.
+
     --venv <dir>
         Set the directory of the virtual environment to use.
         By default, the following directories will be checked,
@@ -92,6 +106,14 @@ while [ ${#} -gt 0 ]; do
             [ -n "${TEST_DIR_BASENAME:-}" ] && usage && exit 1
             TEST_DIR_BASENAME="${2}"
             shift
+            ;;
+        --setup-it)
+            [ -n "${DO_TEARDOWN_IT:-}" ] && usage && exit 1
+            DO_SETUP_IT='true'
+            ;;
+        --teardown-it)
+            [ -n "${DO_SETUP_IT:-}" ] && usage && exit 1
+            DO_TEARDOWN_IT='true'
             ;;
         --venv)
             [ -n "${VENV_DIR:-}" ] && usage && exit 1
@@ -168,7 +190,17 @@ find_and_exec_test_files()
 
 echo "==========================================================================="
 
-if [ "${TEST_FILE_PATTERN}" == "both" ]; then
+if [ "${DO_SETUP_IT}" = "true" ]; then
+    # Source the setup file
+    . "${PACKAGE_TEST_DIRECTORY}/${INTEGRATION_TEST_SETUP_FILE_BASENAME}"
+    # Then run the setup function
+    ${INTEGRATION_TEST_SETUP_FUNC}
+elif [ "${DO_TEARDOWN_IT}" = "true" ]; then
+    # Source the setup file
+    . "${PACKAGE_TEST_DIRECTORY}/${INTEGRATION_TEST_SETUP_FILE_BASENAME}"
+    # Then run the teardown function
+    ${INTEGRATION_TEST_TEARDOWN_FUNC}
+elif [ "${TEST_FILE_PATTERN}" == "both" ]; then
     echo "Running unit tests:"
     echo "--------------------------"
     find_and_exec_test_files "${DEFAULT_UNIT_TEST_FILE_PATTERN}"
