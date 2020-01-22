09d8d9a4d72dc626fb1a57dc687ba3d953f9ec0c
Robert Bartel <robert.bartel@noaa.gov>
2020-01-22 12:04:16 -0600

    Importing InitRequestResponseReason into parent package.

--- lib/communication/nwmaas/communication/__init__.py
+++ lib/communication/nwmaas/communication/__init__.py
@@ -2,7 +2,8 @@ from ._version import __version__
 from .client import MaasRequestClient, SchedulerClient
 from .maas_request import get_available_models, get_available_outputs, get_distribution_types, get_parameters, \
     get_request, Distribution, MaaSRequest, MaaSRequestResponse, NWMRequest, NWMRequestResponse, Scalar
-from .message import AbstractInitRequest, MessageEventType, Message, Response, InvalidMessage, InvalidMessageResponse
+from .message import AbstractInitRequest, MessageEventType, Message, Response, InvalidMessage, InvalidMessageResponse, \
+    InitRequestResponseReason
 from .request_handler import AbstractRequestHandler
 from .scheduler_request import SchedulerRequestMessage, SchedulerRequestResponse
 from .session import Session, FullAuthSession, SessionInitMessage, SessionInitResponse, FailedSessionInitInfo, \
