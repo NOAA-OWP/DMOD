c2582c0dea44dc58e4d3dd6fb7eab6ccadc86fa5
Robert Bartel <robert.bartel@noaa.gov>
2020-01-28 16:18:36 -0600

    Updating schedulerservice Python package to include pyyaml dependency, then updating schedulerservice Dockerfile to no longer run a pip command to install pyyaml individually.

--- schedulerservice/setup.py
+++ schedulerservice/setup.py
@@ -14,6 +14,6 @@ setup(
     author_email='',
     url='',
     license='',
-    install_requires=['docker', 'redis', 'Faker', 'nwmaas-scheduler>=0.1.0'],
+    install_requires=['docker', 'redis', 'Faker', 'pyyaml', 'nwmaas-scheduler>=0.1.0'],
     packages=find_namespace_packages(exclude=('tests', 'test', 'deprecated', 'conf', 'schemas', 'ssl', 'src'))
 )
