345d67a38339c755adc367d2be46f058cd8ed2db
Robert Bartel <robert.bartel@noaa.gov>
2020-01-31 12:19:36 -0600

    Minor update to comments in GUI entrypoint.sh.

--- gui/entrypoint.sh
+++ gui/entrypoint.sh
@@ -4,7 +4,7 @@ export PYTHONUNBUFFERED=1
 # Execute the migration scripts on the designated database
 python manage.py migrate
 
-# Collect all static Django resources into one place where nginx can access them
+# Collect all static Django resources into one place where the web server can access them
 python manage.py collectstatic --no-input
 
 # Run the commands passed in from elsewhere
