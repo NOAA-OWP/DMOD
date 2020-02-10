042aa427aeac1a96c1d144e33309af6f03669b83
Robert Bartel <robert.bartel@noaa.gov>
2020-02-10 09:26:19 -0600

    Updating secrets_provider.sh to have echo's all use double-quoted strings, and to consolidate and improve the initial pattern added for Redis Python client, to make more general.

--- scripts/secrets/secrets_provider.sh
+++ scripts/secrets/secrets_provider.sh
@@ -3,7 +3,8 @@
 # A basic script that serves as a git-secrets secrets provider for the project, just echoing secret patterns
 
 # Redis config file
-echo 'requirepass\s+[A-Za-z0-9][^\s]*\s*$'
-# In particular, for when initializing a Redis Python client object, but easily could apply to other situations
-echo 'password\s*=\s*"[^"]+"'
-echo "password\s*=\s*'[^']+'"
+echo "requirepass\s+[\"'A-Za-z0-9][^$][^\s]*\s*$"
+# For setting any variable or named parameter ending in a substring like 'password' (i.e., any of the substrings that
+# become 'password' when converted to lowercase).
+# In particular, for when initializing a Redis Python client object.
+echo "[Pp][Aa][Ss][Ss][Ww][Oo][Rr][Dd]\s*=\s*('[^']+'|\"[^\"$]+\")"
