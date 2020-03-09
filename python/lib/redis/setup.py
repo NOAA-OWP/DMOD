b34fa89fd8443ccf62b27e72960d9699176a9fc1
Robert Bartel <robert.bartel@noaa.gov>
2020-03-09 12:47:59 -0500

    Adding new redis library package for reusable Redis code.

---
+++ python/lib/redis/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_namespace_packages
+
+try:
+    with open('README.md', 'r') as readme:
+        long_description = readme.read()
+except:
+    long_description = ''
+
+exec(open('nwmaas/redis/_version.py').read())
+
+setup(
+    name='nwmaas-redis',
+    version=__version__,
+    description='Library package with utility classes and functions commonly used Redis operations',
+    long_description=long_description,
+    author='',
+    author_email='',
+    url='',
+    license='',
+    install_requires=['redis'],
+    packages=find_namespace_packages(exclude=('test'))
+)
