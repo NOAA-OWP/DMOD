983629623212bba4a27811be463a6a936d23fd20
Nels Frazier <nels.frazier@noaa.gov>
2020-03-25 11:46:02 -0500

    preparing monitor testing

---
+++ python/lib/monitor/nwmaas/test/setup_it_env.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env sh
+
+IT_REDIS_CONTAINER_NAME='redis_integration_test_instance'
+IT_REDIS_CONTAINER_HOST_PORT=19379
+IT_REDIS_CONTAINER_PASS='***REMOVED***'
+
+it_redis_startup()
+{
+    docker run \
+        --detach \
+        --rm \
+        --name ${IT_REDIS_CONTAINER_NAME} \
+        -p 127.0.0.1:${IT_REDIS_CONTAINER_HOST_PORT}:6379/tcp \
+        --entrypoint=redis-server \
+        redis \
+         --requirepass "${IT_REDIS_CONTAINER_PASS}"
+}
+
+do_setup()
+{
+    # Need Docker container with Redis instance
+    it_redis_startup
+}
+
+do_teardown()
+{
+    # Shutdown (and cleanup) Docker container with Redis instance
+    docker stop ${IT_REDIS_CONTAINER_NAME}
+}
