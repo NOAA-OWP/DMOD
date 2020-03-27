a0c0b62e306c1476536623f448e6a1612da543f5
Nels Frazier <nels.frazier@noaa.gov>
2020-03-27 13:19:15 -0500

    Stubs for monitor service

---
+++ python/services/monitorservice/nwmaas/monitorservice/service.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#TODO implement monitor service
+class MonitorService():
+    __init__(self):
+        pass
+
+    def run():
+        while True:
+            print("Not Implemented")
+            break
+
+if __name__ == '__main__':
+    raise RuntimeError('Module {} called directly; use main package entrypoint instead')
