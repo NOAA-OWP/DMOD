2ac9a582513527d0faf7af89f3fc571948a96a26
Robert Bartel <robert.bartel@noaa.gov>
2020-03-30 11:11:14 -0500

    Updating setup_it_env.sh files for integration testing in several packages that were hard coding certain values, instead having those functions rely upon the necessary variables already being available and set.

--- python/lib/access/nwmaas/test/setup_it_env.sh
+++ python/lib/access/nwmaas/test/setup_it_env.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env sh
 
-IT_REDIS_CONTAINER_NAME='redis_integration_test_instance'
-IT_REDIS_CONTAINER_HOST_PORT=19379
-IT_REDIS_CONTAINER_PASS='***REMOVED***'
-
 it_redis_startup()
 {
     docker run \
         --detach \
         --rm \
-        --name ${IT_REDIS_CONTAINER_NAME} \
-        -p 127.0.0.1:${IT_REDIS_CONTAINER_HOST_PORT}:6379/tcp \
+        --name ${IT_REDIS_CONTAINER_NAME:?} \
+        -p 127.0.0.1:${IT_REDIS_CONTAINER_HOST_PORT:?}:6379/tcp \
         --entrypoint=redis-server \
         redis \
-         --requirepass "${IT_REDIS_CONTAINER_PASS}"
+         --requirepass "${IT_REDIS_CONTAINER_PASS:?}"
 }
 
 do_setup()
@@ -25,5 +21,5 @@ do_setup()
 do_teardown()
 {
     # Shutdown (and cleanup) Docker container with Redis instance
-    docker stop ${IT_REDIS_CONTAINER_NAME}
+    docker stop ${IT_REDIS_CONTAINER_NAME:?}
 }
