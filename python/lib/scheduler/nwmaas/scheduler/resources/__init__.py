e359f2421df967d7fad041d09c13fae25ffe02e2
Robert Bartel <robert.bartel@noaa.gov>
2020-03-30 10:48:12 -0500

    Patching changes to scheduler.py that seem to have gotten accidentally removed, likely during a rebase.

--- python/lib/scheduler/nwmaas/scheduler/resources/__init__.py
+++ python/lib/scheduler/nwmaas/scheduler/resources/__init__.py
@@ -1,2 +1,4 @@
 from .resource import Resource, ResourceAvailability, ResourceState
 from .resource_allocation import ResourceAllocation
+from .redis_manager import RedisManager
+from .resource_manager import ResourceManager
