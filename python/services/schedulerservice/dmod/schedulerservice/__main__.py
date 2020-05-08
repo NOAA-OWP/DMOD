c271340367faef78d649ec0706dc5af7b1536059
Robert Bartel <robert.bartel@noaa.gov>
2020-05-08 12:07:28 -0500

    Logic for Redis params in scheduler service.
    Adding logic for processing parameters for Redis connections from
    either command line arguments, environment variables, or defaults,
    within the __main__ function of the scheduler service package.

--- python/services/schedulerservice/dmod/schedulerservice/__main__.py
+++ python/services/schedulerservice/dmod/schedulerservice/__main__.py
@@ -1,5 +1,6 @@
 import argparse
 import yaml
+from os import getenv
 from pathlib import Path
 from . import name as package_name
 from dmod.scheduler import Scheduler
@@ -18,6 +19,18 @@ def _handle_args():
                         dest='port',
                         type=int,
                         default=3013)
+    parser.add_argument('--redis-host',
+                        help='Set the host value for making Redis connections',
+                        dest='redis_host',
+                        default=None)
+    parser.add_argument('--redis-pass',
+                        help='Set the password value for making Redis connections',
+                        dest='redis_pass',
+                        default=None)
+    parser.add_argument('--redis-port',
+                        help='Set the port value for making Redis connections',
+                        dest='redis_port',
+                        default=None)
     # TODO: improve to be more intelligent about the argument to accept and making it a Path (argparse Action perhaps)
     parser.add_argument('--ssl-dir',
                         #help='Change the base directory when using SSL certificate and key files with default names',
@@ -32,10 +45,68 @@ def _handle_args():
     parser.prog = package_name
     return parser.parse_args()
 
+
 def read_resource_list(resource_file : Path):
     with open(resource_file) as file:
         resource_list = yaml.load(file, Loader=yaml.FullLoader)
         return resource_list['resources']
+
+
+def _get_parsed_or_env_val(parsed_val, env_var_suffix, fallback):
+    """
+    Return either a passed parsed value, if it is not ``None``, the value from one of several environmental variables
+    with a standard beginning to their name, if one is found with a non-``None`` value, or a given fallback value.
+
+    When proceeding to check environment variables, the following variables are checked in the following order, with the
+    first existing and non-``None`` value returned:
+
+        - DOCKER_SECRET_REDIS_<SUFFIX>
+        - REDIS_<SUFFIX>
+        - DOCKER_REDIS_<SUFFIX>
+
+    The actual names have ``<SUFFIX>`` replaced with the provided suffix parameter.
+
+    Finally, the fallback value is returned when necessary.
+
+    Parameters
+    ----------
+    parsed_val
+    env_var_suffix
+    fallback
+
+    Returns
+    -------
+    The appropriate value, given the params and the existing environment variables.
+    """
+    if parsed_val is not None:
+        return parsed_val
+    env_prefixes = ['DOCKER_SECRET_REDIS_', 'REDIS_', 'DOCKER_REDIS_']
+    for prefix in env_prefixes:
+        env_var = prefix + env_var_suffix
+        if getenv(env_var, None) is not None:
+            return getenv(env_var)
+    return fallback
+
+
+def redis_params(parsed_args) -> tuple:
+    """
+    Return the Redis host, port, and password for connections, obtained from the parsed arguments and/or environment.
+
+    Parameters
+    ----------
+    parsed_args
+
+    Returns
+    -------
+    tuple
+        The Redis host, port, and password for connections.
+    """
+    host = _get_parsed_or_env_val(parsed_args.redis_host, 'HOST', 'localhost')
+    port = _get_parsed_or_env_val(parsed_args.redis_port, 'PORT', 6379)
+    passwd = _get_parsed_or_env_val(parsed_args.redis_pass, 'PASS', '')
+
+    return host, port, passwd
+
 
 def main():
     args = _handle_args()
@@ -45,13 +116,16 @@ def main():
     #   run_dev_stuff()
     #else: run_prod()
     resource_list = read_resource_list(Path(args.resource_list_file))
+
+    # Obtain Redis params
+    redis_host, redis_port, redis_pass = redis_params(args)
+
     # instantiate the resource manager for the scheduler
     #TODO configure redis here, i.e. host, port, pass?  Or rely on env?
     resource_manager = RedisManager("maas")
     resource_manager.set_resources(resource_list)
     # instantiate the scheduler
     # TODO: look at handling if the value in args.images_and_domains_yaml doesn't correspond to an actual file
-    # instantiate the scheduler
     scheduler = Scheduler(images_and_domains_yaml=args.images_and_domains_yaml, resource_manager=resource_manager, type="dev")
 
     #Instansite the handle_job_request
