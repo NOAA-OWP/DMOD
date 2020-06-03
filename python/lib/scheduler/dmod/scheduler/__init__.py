2e4708b064d84bfecdfa2cd62b2ada374646bdd2
Robert Bartel <robert.bartel@noaa.gov>
2020-06-03 13:13:07 -0500

    Update package class exposure as need for usage.

--- python/lib/scheduler/dmod/scheduler/__init__.py
+++ python/lib/scheduler/dmod/scheduler/__init__.py
@@ -3,7 +3,7 @@ from .ssh_key_util import SshKeyUtil, SshKeyUtilImpl
 from .resources.resource_manager import ResourceManager
 from .resources.redis_manager import RedisManager
 from .utils import *
-from .job import Job, JobManager, JobManagerFactory
+from .job import Job, JobManager, JobManagerFactory, JobStatus
 
 
 name = 'scheduler'
