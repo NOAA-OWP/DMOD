2247169977ea3cd31014072137d9b5d5f2b2c79a
Robert Bartel <robert.bartel@noaa.gov>
2020-06-08 14:36:34 -0500

    Updating scheduler package for new classes.
    Updating scheduler package and subpackages to make new JobExecPhase,
    JobExecStep, and JobImpl classes accessible.

--- python/lib/scheduler/dmod/scheduler/__init__.py
+++ python/lib/scheduler/dmod/scheduler/__init__.py
@@ -3,7 +3,7 @@ from .ssh_key_util import SshKeyUtil, SshKeyUtilImpl
 from .resources.resource_manager import ResourceManager
 from .resources.redis_manager import RedisManager
 from .utils import *
-from .job import Job, JobManager, JobManagerFactory, JobStatus
+from .job import Job, JobManager, JobManagerFactory, JobExecPhase, JobExecStep, JobImpl, JobStatus
 
 
 name = 'scheduler'
