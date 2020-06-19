857c9c012e7f355428393b603f206d5ef6f00a7a
Robert Bartel <robert.bartel@noaa.gov>
2020-06-19 14:14:43 -0500

    Fixing package exposure for RsaKeyPair class.

--- python/lib/scheduler/dmod/scheduler/__init__.py
+++ python/lib/scheduler/dmod/scheduler/__init__.py
@@ -1,5 +1,6 @@
+from .rsa_key_pair import RsaKeyPair
 from .scheduler import Launcher
-from .ssh_key_util import RsaKeyPair, SshKeyUtil, SshKeyUtilImpl
+from .ssh_key_util import SshKeyUtil, SshKeyUtilImpl
 from .utils import *
 
 name = 'scheduler'
