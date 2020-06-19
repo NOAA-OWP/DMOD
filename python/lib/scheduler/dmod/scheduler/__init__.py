dc45170cd68ddfd119befe7c94d99355c10a96ec
Robert Bartel <robert.bartel@noaa.gov>
2020-06-19 13:56:01 -0500

    Adjust setup for scheduler package.
    Adjusting scheduler package to not "pull" subpackage modules directly
    into main package so they are importable from there, thereby requiring
    they be imported from their actual parent package.

--- python/lib/scheduler/dmod/scheduler/__init__.py
+++ python/lib/scheduler/dmod/scheduler/__init__.py
@@ -1,9 +1,5 @@
 from .scheduler import Launcher
-from .ssh_key_util import SshKeyUtil, SshKeyUtilImpl
-from .resources.resource_manager import ResourceManager
-from .resources.redis_manager import RedisManager
+from .ssh_key_util import RsaKeyPair, SshKeyUtil, SshKeyUtilImpl
 from .utils import *
-from .job import Job, JobManager, JobManagerFactory, JobExecPhase, JobExecStep, JobImpl, JobStatus
-
 
 name = 'scheduler'
