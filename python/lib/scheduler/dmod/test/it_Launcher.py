b022fbdf07de04bf3f824f969b4978761dba0e98
Nels Frazier <nels.frazier@noaa.gov>
2020-07-07 15:45:49 -0500

    Basic docker service integration test stub

---
+++ python/lib/scheduler/dmod/test/it_Launcher.py
@@ -0,0 +1,34 @@
+import unittest
+from pathlib import Path
+from ..scheduler.scheduler import Launcher
+from . import mock_job
+from .utils import logTest
+import logging
+
+class IntegrationTestLauncher(unittest.TestCase):
+
+    def setUp(self) -> None:
+        yaml_file = Path(__file__).parent/"image_and_domain.yaml"
+        self.user_name = 'test'
+        self.clean_up = []
+        #Create a launcher
+        self.launcher = Launcher(images_and_domains_yaml=yaml_file)
+
+    def tearDown(self) -> None:
+        for service in self.clean_up:
+            service.remove()
+        self.launcher.docker_client.close()
+
+    @logTest(logging.DEBUG)
+    def test_start_job(self) -> None:
+        job = mock_job(allocations=1)
+        success, service = self.launcher.start_job(job)
+        self.assertTrue(success)
+        self.assertIsNotNone(service)
+
+        self.clean_up.append(service)
+
+    #TODO test
+    @unittest.skip("Not implemented")
+    def test_create_service(self):
+        pass
