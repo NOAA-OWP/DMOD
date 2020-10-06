555c5e7ef5e22dfa236a7dae1fab91f0784fd013
Shengting Cui <shengting.cui@noaa.gov>
2020-10-06 16:08:48 +0000

    Modify the mpi_hello_world.f90 code

--- docker/main/mpich/mpich-test/mpi_hello_world.f90
+++ docker/main/mpich/mpich-test/mpi_hello_world.f90
@@ -1,39 +1,5 @@
 program main
 
-!*****************************************************************************80
-!
-!! MAIN is the main program for HELLO_MPI.
-!
-!  Discussion:
-!
-!    HELLO is a simple MPI test program.
-!
-!    Each process prints out a "Hello, world!" message.
-!
-!    The master process also prints out a short message.
-!
-!  Licensing:
-!
-!    This code is distributed under the GNU LGPL license.
-!
-!  Modified:
-!
-!    30 October 2008
-!
-!  Author:
-!
-!    John Burkardt
-!
-!  Reference:
-!
-!    William Gropp, Ewing Lusk, Anthony Skjellum,
-!    Using MPI: Portable Parallel Programming with the
-!    Message-Passing Interface,
-!    Second Edition,
-!    MIT Press, 1999,
-!    ISBN: 0262571323,
-!    LC: QA76.642.G76.
-!
   use mpi
 
   integer ( kind = 4 ) error
