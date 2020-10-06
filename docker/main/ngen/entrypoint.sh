b6b290125c24cca5a512fecf09ad91a7fc49ce0d
Nels Frazier <nels.frazier@noaa.gov>
2020-10-06 13:13:29 -0500

    cleanup and document entrypoints for models, standardize args slightly

--- docker/main/ngen/entrypoint.sh
+++ docker/main/ngen/entrypoint.sh
@@ -1,19 +1,27 @@
 #!/bin/sh
+# $1 will have the number of nodes associated with this run
+# $2 will have the host string in MPI form, i.e. hostname:N, hostname:M
+# $3 will have the unique job id
 
-# Wait a bit to make sure all service workers are up
-sleep 15
-
-req_id = $1
-echo ${req_id}
-
+#Make sure we are in workdir
 cd ${WORKDIR}
-domain_location=${WORKDIR}
-tmp_domain=${WORKDIR}/data/tmp_${req_id}
+#This is the input location that image_and_domain.yaml specificies as the run time mount location
+domain_location=/ngen/data
+#This is the output location that image_and_domain.yaml specifices as the run time mount location
+output_dir=/ngen/output
+#Create a tmp dir based on the job id to dump output to
+tmp_domain=$output_dir/tmp_$3
 mkdir -p $tmp_domain
-ln -s $domain_location/data/$2/* $tmp_domain/
-
+#Soft link the mounted static inputs
+ln -s $domain_location $tmp_domain/
+#cd to the tmp dir to run
 cd $tmp_domain
-
-ngen ./data/$1/catchment_data.geojson "" ./data/$1/nexus_data.geojson "" ./data/$1/realization_config.json
-
-echo 'ngen returned with a return value: ' $?
+#Execute the model on the linked data
+ngen ./data/catchment_data.geojson "" ./data/nexus_data.geojson "" ./data/refactored_example_realization_config.json > std_out.log 2> std_err.log
+#Capture the return value to use as service exit code
+ngen_return=$?
+echo 'ngen returned with a return value: ' $ngen_return
+#Remove soft link, which will have the same name as the last element of domain_location path
+unlink data
+#Exit with the model's exit code
+exit $ngen_return
