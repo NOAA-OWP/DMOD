a70bd62885b5044c9d7e188fe3b94bf4a6fbb75e
Robert Bartel <robert.bartel@noaa.gov>
2021-04-07 14:16:28 -0500

    Update modeldata readme and setup after renaming.

--- python/lib/modeldata/setup.py
+++ python/lib/modeldata/setup.py
@@ -3,10 +3,10 @@ from setuptools import setup, find_namespace_packages
 with open('README.md', 'r') as readme:
     long_description = readme.read()
 
-exec(open('dmod/forcingservice/_version.py').read())
+exec(open('dmod/modeldata/_version.py').read())
 
 setup(
-    name='dmod-forcingservice',
+    name='dmod-modeldata',
     version=__version__,
     description='',
     long_description=long_description,
