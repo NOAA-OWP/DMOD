2406e8c5b745e91894e68e5ffdcbcc4585af0707
Robert Bartel <robert.bartel@noaa.gov>
2021-04-07 14:17:14 -0500

    Update modeldata naming in inner directory.

--- python/lib/modeldata/dmod/test/test_subset_service_impl.py
+++ python/lib/modeldata/dmod/test/test_subset_service_impl.py
@@ -5,7 +5,7 @@ from geopandas import GeoDataFrame
 from pandas import DataFrame
 from pathlib import Path
 from typing import Dict, Optional, Union
-from ..forcingservice.subset import SubsetHandlerImpl
+from ..modeldata.subset import SubsetHandlerImpl
 
 
 class TestSubsetServiceImpl(unittest.TestCase):
