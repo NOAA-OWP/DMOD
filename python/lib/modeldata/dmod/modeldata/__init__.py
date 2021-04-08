a883fce32b9db92dbde4a622e67b1f631184ccbd
Robert Bartel <robert.bartel@noaa.gov>
2021-04-08 12:30:57 -0500

    Fix defined package name after change.

--- python/lib/modeldata/dmod/modeldata/__init__.py
+++ python/lib/modeldata/dmod/modeldata/__init__.py
@@ -2,4 +2,4 @@ from ._version import __version__
 from .subset import SubsetDefinition, HydrofabricSubset, SimpleHydrofabricSubset
 #from .service import ForcingDataHandler
 
-name = 'forcingservice'
+name = 'modeldata'
