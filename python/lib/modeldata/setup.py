4542a7aaf166f89008a29b32a6e3d76605ab0d16
Robert Bartel <robert.bartel@noaa.gov>
2021-04-16 14:29:06 -0500

    Require dmod comms in modeldata setup.
    Due to having SubsetDefinition now extend Serializable.

--- python/lib/modeldata/setup.py
+++ python/lib/modeldata/setup.py
@@ -14,7 +14,7 @@ setup(
     author_email='',
     url='',
     license='',
-    install_requires=['numpy>=1.20.1', 'pandas', 'geopandas',
+    install_requires=['numpy>=1.20.1', 'pandas', 'geopandas', 'dmod-communication>=0.3.0',
                       'hypy@git+git://github.com/noaa-owp/hypy@master#egg=hypy&subdirectory=python',
                       'hydrotools-nwis_client@git+git://github.com/noaa-owp/hydrotools#egg=hydrotools-nwis_client&subdirectory=python/nwis_client'],
     packages=find_namespace_packages(exclude=('tests', 'schemas', 'ssl', 'src'))
