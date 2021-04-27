f9ee91a5ba0ac2e565b2221e012a647ca75894f1
Robert Bartel <robert.bartel@noaa.gov>
2021-04-27 13:05:36 -0500

    Rework SubsetHandler design.
    Changing SubsetHandler by making it expect a Hydrofabric object for its
    backing hydrofabric attribute, updating it to have a SubsetValidator
    attribute, and having it no longer be abstract (moving implementation
    details from SubsetHandlerImpl type and then removing the latter).

--- python/lib/modeldata/dmod/modeldata/subset/subset_handler.py
+++ python/lib/modeldata/dmod/modeldata/subset/subset_handler.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
-from hypy import Catchment, HydroLocation, Nexus
+from hypy import Catchment, Nexus
 from queue import Queue
-from typing import Collection, Dict, Optional, Set, Union
+from typing import Collection, Optional, Set, Tuple, Union
 from .subset_definition import SubsetDefinition
 from .hydrofabric import Hydrofabric, GeoJsonHydrofabricReader, MappedGraphHydrofabric
 
@@ -133,16 +133,20 @@ class BasicSubsetValidator(SubsetValidator):
 
         return None
 
-    def __init__(self, hydrofabric_graph):
+
+class SubsetHandler:
+
+    def __init__(self, hydrofabric: Hydrofabric, validator: Optional[SubsetValidator] = None):
         """
 
         Parameters
         ----------
-        hydrofabric_graph
-            The graph of catchments and nexuses
+        hydrofabric
+            The hydrofabric of catchments and nexuses
 
         """
-        self._hydrofabric_graph = hydrofabric_graph
+        self._hydrofabric = hydrofabric
+        self._validator = validator if validator else BasicSubsetValidator(hydrofabric)
 
     @abstractmethod
     def get_catchment_by_id(self, catchment_id: str) -> Catchment:
@@ -159,7 +163,7 @@ class BasicSubsetValidator(SubsetValidator):
         Catchment
             The appropriate catchment object from the hydrograph.
         """
-        pass
+        return self._hydrofabric.get_catchment_by_id(catchment_id)
 
     def get_subset_for(self, catchment_ids: Collection[str]) -> SubsetDefinition:
         """
@@ -240,7 +244,6 @@ class BasicSubsetValidator(SubsetValidator):
 
         return SubsetDefinition(catchment_ids=cat_ids, nexus_ids=nex_ids)
 
-    @abstractmethod
     def is_catchment_recognized(self, catchment_id: str) -> bool:
         """
         Test whether a catchment is recognized in the current hydrograph.
@@ -255,128 +258,25 @@ class BasicSubsetValidator(SubsetValidator):
         bool
             Whether the catchment is recognized.
         """
-        pass
+        return self._hydrofabric.is_catchment_recognized(catchment_id)
 
-
-class SubsetHandlerImpl(SubsetHandler):
-
-    @classmethod
-    def read_hydrofabric_files(cls, catchment_data, nexus_data, cross_walk):
-        id_error_msg = 'Unexpected format of {} file {}, without \'id\' or \'ID\' column'
-
-        catchment_hydro_fabric = gpd.read_file(catchment_data)
-        catchment_hydro_fabric.columns = catchment_hydro_fabric.columns.astype(str).str.lower()
-        if 'id' not in catchment_hydro_fabric.columns:
-            raise RuntimeError(id_error_msg.format('catchment hydrofabric', catchment_data))
-        catchment_hydro_fabric.set_index('id', inplace=True)
-
-        nexus_hydro_fabric = gpd.read_file(nexus_data)
-        nexus_hydro_fabric.columns = nexus_hydro_fabric.columns.astype(str).str.lower()
-        if 'id' not in nexus_hydro_fabric.columns:
-            raise RuntimeError(id_error_msg.format('nexus hydrofabric', catchment_data))
-        nexus_hydro_fabric.set_index('id', inplace=True)
-
-        x_walk = pd.read_json(cross_walk, dtype=str)
-        return catchment_hydro_fabric, nexus_hydro_fabric, x_walk
-
-    def __init__(self, catchment_data, nexus_data, cross_walk):
-        self._catchment_geo_df, self._nexus_geo_df, self._x_walk_df = self.read_hydrofabric_files(catchment_data,
-                                                                                                  nexus_data,
-                                                                                                  cross_walk)
-        super(SubsetHandlerImpl, self).__init__(hydrofabric_graph=self._generate_hydrofabric_graph())
-
-    def _generate_hydrofabric_graph(self) -> Dict[str, Union[Catchment, Nexus]]:
+    def validate(self, subset: SubsetDefinition) -> Tuple[bool, Optional[str]]:
         """
-        Generate the hydrofabric graph and a dictionary of the elements by their ids.
+        Validate the subset and give feedback if invalid.
 
-        Generate a hydrofabric graph of associated ::class:`Catchment` and ::class:`Nexus` objects from the data
-        provided in the parsed catchment and nexus geodataframes.  As part of the graph's construction, create a
-        dictionary data structure, keyed by catchment/nexus id, of the individual graph nodes (i.e., each
-        ::class:`Catchment` or ::class:`Nexus` object) for easy lookups.  Return this dictionary once generation is
-        complete.
-
-        Returns
-        -------
-        Dict[str, Union[Catchment, Nexus]]
-            A dictionary of the nodes of the graph keyed by each's string id value.
-        """
-        # Keys of nexus id to lists of catchment ids for the catchments receiving water from this nexus
-        nexus_receiving_cats = dict()
-        # Keys of nexus id to lists of catchment ids for the catchments contributing water to this nexus
-        nexus_contrib_cats = dict()
-        known_catchment_ids = set()
-        known_nexus_ids = set()
-        cat_to = dict()
-        cat_from = dict()
-
-        for cat_id in self._catchment_geo_df.index:
-            known_catchment_ids.add(cat_id)
-            # TODO: do we need to account for more than one downstream?
-            to_nex_id = self._catchment_geo_df.loc[cat_id]['toid'].strip()
-            known_nexus_ids.add(to_nex_id)
-            cat_to[cat_id] = to_nex_id
-            if to_nex_id in nexus_contrib_cats:
-                nexus_contrib_cats[to_nex_id].add(cat_id)
-            else:
-                nexus_contrib_cats[to_nex_id] = {cat_id}
-            # TODO: do we need to account for contained/containing/conjoined?
-        for nex_id in self._nexus_geo_df.index:
-            known_nexus_ids.add(nex_id)
-            to_cats = self._nexus_geo_df.loc[nex_id]['toid']
-            # Handle the first one with conditional check separate, to optimize later ones
-            first_cat_id = to_cats.split(',')[0].strip()
-            if nex_id in nexus_receiving_cats and to_cats:
-                nexus_receiving_cats[nex_id].add(first_cat_id)
-            else:
-                nexus_receiving_cats[nex_id] = {first_cat_id}
-            known_catchment_ids.add(first_cat_id)
-            cat_from[first_cat_id] = nex_id
-            # Now add any remaining
-            for cat_id in to_cats[1:]:
-                clean_cat_id = cat_id.strip()
-                nexus_receiving_cats[nex_id].add(clean_cat_id)
-                known_catchment_ids.add(clean_cat_id)
-                cat_from[cat_id] = nex_id
-
-        hf = dict()
-        # Create the catchments first, just without any upstream/downstream connections
-        for cat_id in known_catchment_ids:
-            # TODO: do params need to be something different?
-            hf[cat_id] = Catchment(catchment_id=cat_id, params=dict())
-        # Create the nexuses next, applying the right collections of catchments for contrib and receiv
-        for nex_id in known_nexus_ids:
-            contributing = set()
-            for cid in nexus_contrib_cats[nex_id]:
-                contributing.add(hf[cid])
-            receiving = set()
-            for cid in nexus_receiving_cats[nex_id]:
-                receiving.add(hf[cid])
-            hf[nex_id] = Nexus(nexus_id=nex_id, hydro_location=HydroLocation(realized_nexus=nex_id),
-                               receiving_catchments=list(receiving), contributing_catchments=list(contributing))
-        # Now go back and apply the right to/from relationships for catchments
-        for cat_id, nex_id in cat_to.items():
-            hf[cat_id]._outflow = hf[nex_id]
-        for cat_id, nex_id in cat_from.items():
-            hf[cat_id]._inflow = hf[nex_id]
-        # TODO: again, do we need to worry about contained/containing/conjoined?
-        # Finally ...
-        return hf
-
-    def get_catchment_by_id(self, catchment_id: str) -> Catchment:
-        """
-        Get the catchment object for the given id.
+        Check whether the given subset is valid and, in the case when the subset is invalid, obtain at least a partial
+        description of why it is invalid.  Return these as a tuple.
 
         Parameters
         ----------
-        catchment_id : str
-            The catchment id.
+        subset : SubsetDefinition
+            The subset in question.
 
         Returns
         -------
-        Catchment
-            The appropriate catchment object from the hydrograph.
+        Tuple[bool, Optional[str]]
+            Whether the subset is valid and, if not, a partial description of why not (or ``None`` if it is valid).
         """
-        return self._hydrofabric_graph[catchment_id]
+        description = self._validator.invalid_reason(subset)
+        return description is None, description
 
-    def is_catchment_recognized(self, catchment_id: str) -> bool:
-        return catchment_id in self._hydrofabric_graph
