28e1cc695b3013a31ca642cd00de51aaeb2c99ac
Robert Bartel <robert.bartel@noaa.gov>
2021-04-27 16:19:05 -0500

    Update modeldata subpackage inits.

--- python/lib/modeldata/dmod/modeldata/__init__.py
+++ python/lib/modeldata/dmod/modeldata/__init__.py
@@ -1,5 +1,5 @@
 from ._version import __version__
-from .subset import SubsetDefinition, SubsetHandler, SubsetHandlerImpl, HydrofabricSubset, SimpleHydrofabricSubset
+from .subset import SubsetDefinition, SubsetHandler, HydrofabricSubset, SimpleHydrofabricSubset
 #from .service import ForcingDataHandler
 
 name = 'modeldata'
--- python/lib/modeldata/dmod/modeldata/subset/__init__.py
+++ python/lib/modeldata/dmod/modeldata/subset/__init__.py
@@ -1,3 +1,4 @@
 from .subset_definition import SubsetDefinition
 from .hydrofabric_subset import HydrofabricSubset, SimpleHydrofabricSubset
-from .subset_handler import SubsetHandler, SubsetHandlerImpl
+from .subset_handler import SubsetHandler
+from .hydrofabric import Hydrofabric, GeoJsonHydrofabricReader, MappedGraphHydrofabric
