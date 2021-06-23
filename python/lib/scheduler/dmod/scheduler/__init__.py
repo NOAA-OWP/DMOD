4a9f8b8ca5892691e6fa2d8df25db378c26858ea
Robert Bartel <robert.bartel@noaa.gov>
2021-06-23 13:48:17 -0500

    Expose scheduler package SimpleDockerUtil class.

--- python/lib/scheduler/dmod/scheduler/__init__.py
+++ python/lib/scheduler/dmod/scheduler/__init__.py
@@ -1,6 +1,6 @@
 from .rsa_key_pair import RsaKeyPair
 from .ssh_key_util import SshKeyUtil, SshKeyUtilImpl
 from .utils import *
-from .scheduler import Launcher
+from .scheduler import SimpleDockerUtil, Launcher
 from .resources import RedisManager, Resource
 name = 'scheduler'
