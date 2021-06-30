f2244865ea6c7bfb23cb36ed4813bdc18b809a51
Nels Frazier <nels.frazier@noaa.gov>
2021-06-30 19:14:19 +0000

    configure the django app to use the postgres DB if defined in the ENV

--- python/gui/maas_experiment/settings.py
+++ python/gui/maas_experiment/settings.py
@@ -140,8 +140,12 @@ WSGI_APPLICATION = 'maas_experiment.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': os.environ.get('SQL_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': os.environ.get('SQL_DATABASE', os.path.join(BASE_DIR, 'db.sqlite3')),
+        'USER': os.environ.get('SQL_USER', 'user'),
+        'PASSWORD': os.environ.get('SQL_PASSWORD', 'password'),
+        'HOST': os.environ.get('SQL_HOST', 'localhost'),
+        'PORT': os.environ.get('SQL_PORT', '5432'),
     }
 }
 
