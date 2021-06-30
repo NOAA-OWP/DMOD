8919583216813494f666b821f146194cf2b6e5fa
Nels Frazier <nels.frazier@noaa.gov>
2021-06-30 19:13:16 +0000

    add a postgres DB to the gui stack

--- docker/nwm_gui/app_server/entrypoint.sh
+++ docker/nwm_gui/app_server/entrypoint.sh
@@ -2,8 +2,33 @@
 
 export PYTHONUNBUFFERED=1
 #export PYTHONASYNCIODEBUG=1
+
+# check the database is ready before starting app
+if [ "$DATABASE" = "postgres" ]
+then
+    echo "Waiting for postgres service..."
+    while ! nc -z $SQL_HOST $SQL_PORT; do
+        sleep 0.1
+    done
+fi
+echo "Starting dmod app"
+
+#########
+# These lines will wipe and restablish the database tables
+# Not good in production if you want to presist data!
+# You can always run these adhoc to initialize the DB
+# docker-compose exec app_server python manage.py flush --no-input
+# or docker exec <stack>_app_server python manage.py flush --no-input
+########
+#python manage.py flush --no-input
+#python manage.py migrate
+
 # Execute the migration scripts on the designated database
-python manage.py migrate
+#python manage.py migrate
+
+#Extract the DB secrets into correct ENV variables
+POSTGRES_SECRET_FILE="/run/secrets/${DOCKER_SECRET_POSTGRES_PASS:?}"
+export SQL_PASSWORD="$(cat ${POSTGRES_SECRET_FILE})"
 
 # Collect all static Django resources into one place where the web server can access them
 python manage.py collectstatic --no-input
