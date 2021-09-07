5cf136f5a3bad8b6bd086990e36d301b4f3d422d
Robert Bartel <robert.bartel@noaa.gov>
2021-09-07 12:11:13 -0500

    Adjust modeldata comms version dep to 0.5.0.

--- python/lib/modeldata/setup.py
+++ python/lib/modeldata/setup.py
@@ -14,7 +14,7 @@ setup(
     author_email='',
     url='',
     license='',
-    install_requires=['numpy>=1.19.5', 'pandas', 'geopandas', 'dmod-communication>=0.3.0',
+    install_requires=['numpy>=1.19.5', 'pandas', 'geopandas', 'dmod-communication>=0.5.0',
                       'hypy@git+https://github.com/noaa-owp/hypy@master#egg=hypy&subdirectory=python',
                       ],
     packages=find_namespace_packages(exclude=('tests', 'schemas', 'ssl', 'src'))
