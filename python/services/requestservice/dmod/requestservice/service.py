c64428a8db5a9e5600796c1842c13d2eff26edcf
Robert Bartel <robert.bartel@noaa.gov>
2022-02-16 11:38:55 -0600

    Remove custom UnsupportedMessageTypeResponse.
    Removing custom UnsupportedMessageTypeResponse within requestservice
    service.py, and instead updating that module to import the equivalent
    class now available in the communication package.

--- python/services/requestservice/dmod/requestservice/service.py
+++ python/services/requestservice/dmod/requestservice/service.py
@@ -9,8 +9,8 @@ import websockets
 from websockets import WebSocketServerProtocol
 
 from dmod.access import DummyAuthUtil, RedisBackendSessionManager
-from dmod.communication import Response, InvalidMessageResponse, MessageEventType, \
-    WebSocketInterface, WebSocketSessionsInterface, SchedulerClient
+from dmod.communication import InvalidMessageResponse, MessageEventType, WebSocketSessionsInterface, SchedulerClient, \
+    UnsupportedMessageTypeResponse
 from dmod.externalrequests import AuthHandler, NWMRequestHandler
 
 logging.basicConfig(
@@ -18,18 +18,6 @@ logging.basicConfig(
     format="%(asctime)s,%(msecs)d %(levelname)s: %(message)s",
     datefmt="%H:%M:%S"
 )
-
-
-class UnsupportedMessageTypeResponse(Response):
-
-    def __init__(self, actual_event_type: MessageEventType, listener_type: Type[WebSocketInterface],
-                 message: str = None, data=None):
-        if message is None:
-            message = 'The {} event type is not supported by this {} listener'.format(
-                actual_event_type, listener_type.__name__)
-        super().__init__(success=False, reason='Message Event Type Unsupported', message=message, data=data)
-        self.actual_event_type = actual_event_type
-        self.listener_type = listener_type
 
 
 class RequestService(WebSocketSessionsInterface):
