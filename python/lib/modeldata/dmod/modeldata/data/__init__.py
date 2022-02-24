346a5fbd55846dca353543b9d78d8496bd738d55
Robert Bartel <robert.bartel@noaa.gov>
2022-02-24 10:17:56 -0600

    Update modeldata.data init for new/removed types.

--- python/lib/modeldata/dmod/modeldata/data/__init__.py
+++ python/lib/modeldata/dmod/modeldata/data/__init__.py
@@ -1,3 +1,4 @@
 from .catchment_data import CatchmentData
 from .data_subset import AbstractDataSubset
+from .dataset import Dataset, DatasetManager
 from .forcing_data_handler import ForcingDataHandler
