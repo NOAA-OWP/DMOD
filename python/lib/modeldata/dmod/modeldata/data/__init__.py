7dd48bc62a5c3433e6e96b72eff753c9a21941fd
Robert Bartel <robert.bartel@noaa.gov>
2022-02-24 10:42:56 -0600

    Adjust modeldata.data init for circular imports.

--- python/lib/modeldata/dmod/modeldata/data/__init__.py
+++ python/lib/modeldata/dmod/modeldata/data/__init__.py
@@ -1,5 +1,3 @@
 from .catchment_data import CatchmentData
 from .data_subset import AbstractDataSubset
-from .dataset import Dataset, DatasetManager
 from .forcing_data_handler import ForcingDataHandler
-
