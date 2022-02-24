47e893bcc51a72f476d3ac33c95c78f6f40e783e
Robert Bartel <robert.bartel@noaa.gov>
2022-02-24 10:22:31 -0600

    Add DataDomain to modeldata.data package init.

--- python/lib/modeldata/dmod/modeldata/data/__init__.py
+++ python/lib/modeldata/dmod/modeldata/data/__init__.py
@@ -2,4 +2,5 @@ from .catchment_data import CatchmentData
 from .data_subset import AbstractDataSubset
 from .dataset import Dataset, DatasetManager
 from .forcing_data_handler import ForcingDataHandler
-from .meta_data import ContinuousRestriction, DiscreteRestriction, DataRequirement, DataCategory, DataFormat, TimeRange
+from .meta_data import ContinuousRestriction, DiscreteRestriction, DataDomain, DataRequirement, DataCategory, \
+    DataFormat, TimeRange
