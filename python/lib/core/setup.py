aedc1124151731189aa48771c8b12d133fb750b8
Robert Bartel <robert.bartel@noaa.gov>
2022-03-02 12:33:10 -0600

    Creating dmod.core package for core types.
    Creating new package for (pending) relocation of core types that will
    need to be migrated here to avoid circular dependencies.

---
+++ python/lib/core/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_namespace_packages
+
+with open('README.md', 'r') as readme:
+    long_description = readme.read()
+
+exec(open('dmod/core/_version.py').read())
+
+setup(
+    name='dmod-core',
+    version=__version__,
+    description='',
+    long_description=long_description,
+    author='',
+    author_email='',
+    url='',
+    license='',
+    install_requires=[],
+    packages=find_namespace_packages(exclude=('tests', 'schemas', 'ssl', 'src'))
+)
