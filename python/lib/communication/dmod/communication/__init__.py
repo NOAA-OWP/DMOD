26dadcc5a5faa7d41c6d7742c808f27f26b07f03
Robert Bartel <robert.bartel@noaa.gov>
2022-03-29 09:32:54 -0500

    Add new classes to communication package init.
    Adding new service client class types and partitioning message types to
    dmod.communication package init.

--- python/lib/communication/dmod/communication/__init__.py
+++ python/lib/communication/dmod/communication/__init__.py
@@ -1,11 +1,13 @@
 from ._version import __version__
-from .client import ModelExecRequestClient, MaasRequestClient, SchedulerClient
+from .client import InternalServiceClient, ModelExecRequestClient, MaasRequestClient, PartitionerServiceClient, \
+    SchedulerClient
 from .maas_request import get_available_models, get_available_outputs, get_distribution_types, get_parameters, \
     get_request, Distribution, MaaSRequest, MaaSRequestResponse, ModelExecRequest, ModelExecRequestResponse, \
     NWMRequest, NWMRequestResponse, Scalar, NGENRequest, NGENRequestResponse
 from .message import AbstractInitRequest, MessageEventType, Message, Response, InvalidMessage, InvalidMessageResponse, \
     InitRequestResponseReason
 from .metadata_message import MetadataPurpose, MetadataMessage, MetadataResponse
+from .partition_request import PartitionRequest, PartitionResponse
 from .request_handler import AbstractRequestHandler
 from .scheduler_request import SchedulerRequestMessage, SchedulerRequestResponse
 from .session import Session, FullAuthSession, SessionInitMessage, SessionInitResponse, FailedSessionInitInfo, \
