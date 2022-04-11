528ff154149f11cead992db0b04cbc9aa7cc754a
Robert Bartel <robert.bartel@noaa.gov>
2022-04-11 14:37:18 -0500

    Add partitionerservice entrypoint script.
    Add a custom entrypoint script for partitionerservice to support
    identifying DMOD Python package wheels when present in a specified
    directory, and update those packages when starting the service, as a
    faster alternative for deploying certain code changes that would
    otherwise require one or more images to be fully rebuilt, potentially
    10 minutes per change depending on the image.

---
+++ docker/main/partitionerservice/entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+#set -e
+
+# A virtual environment path may be supplied using this environmental shell variable
+if [ -n "${VENV_DIR:-}" ]; then
+    # Initialize if virtual environment directory either doesn't exists, or exists but is empty ...
+    if [ ! -d "${VENV_DIR}" ] || [ $(find "${VENV_DIR}" -maxdepth 0 -empty 2>/dev/null | wc -l) -eq 1 ]; then
+        python3 -m venv "${VENV_DIR}"
+    fi
+    . "${VENV_DIR}/bin/activate"
+    pip install --update -r /code/requirements.txt
+fi
+
+# If we find this directory, and if there are wheels in it, then install those
+if [ -d ${UPDATED_PACKAGES_DIR:=/updated_packages} ]; then
+    if [ $(ls ${UPDATED_PACKAGES_DIR}/*.whl | wc -l) -gt 0 ]; then
+        for srv in $(pip -qq freeze | grep dmod | awk -F= '{print $1}' | awk -F- '{print $2}'); do
+            if [ $(ls ${UPDATED_PACKAGES_DIR} | grep dmod.${srv}- | wc -l) -eq 1 ]; then
+                pip uninstall -y --no-input $(pip -qq freeze | grep dmod.${srv} | awk -F= '{print $1}')
+                pip install $(ls ${UPDATED_PACKAGES_DIR}/*.whl | grep dmod.${srv}-)
+            fi
+        done
+        #pip install ${UPDATED_PACKAGES_DIR}/*.whl
+    fi
+fi
+
+python -m ${SERVICE_PACKAGE_NAME:?} ${@}
