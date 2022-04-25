3e98a54686113ad1290d379bece42b802230b7e4
Robert Bartel <robert.bartel@noaa.gov>
2022-04-25 14:16:16 -0500

    Update GUI entrypoint script for debugging.

--- docker/nwm_gui/app_server/entrypoint.sh
+++ docker/nwm_gui/app_server/entrypoint.sh
@@ -3,6 +3,10 @@
 export PYTHONUNBUFFERED=1
 #export PYTHONASYNCIODEBUG=1
 
+# The value to use for the gunicorn --timeout argument; the default/standard is 30
+# Need to set explicitly in order to be able to set differently when required for doing debugging
+_WORKER_TIMEOUT=30
+
 # check the database is ready before starting app
 if [ "$DATABASE" = "postgres" ]
 then
@@ -22,6 +26,7 @@ echo "Starting dmod app"
 ########
 #python manage.py flush --no-input
 #python manage.py migrate
+#########
 
 # Execute the migration scripts on the designated database
 #python manage.py migrate
@@ -30,9 +35,15 @@ echo "Starting dmod app"
 POSTGRES_SECRET_FILE="/run/secrets/${DOCKER_SECRET_POSTGRES_PASS:?}"
 export SQL_PASSWORD="$(cat ${POSTGRES_SECRET_FILE})"
 
+# Handle for debugging when appropriate
+if [ "$(echo "${PYCHARM_REMOTE_DEBUG_ACTIVE:-false}" | tr '[:upper:]' '[:lower:]' | tr -d '[:space:]')" == "true" ]; then
+    # Set the timeout to longer when debugging
+    _WORKER_TIMEOUT=9999
+fi
+
 # Collect all static Django resources into one place where the web server can access them
 python manage.py collectstatic --no-input
 
 # Run the commands passed in from elsewhere
 #exec "$@"
-gunicorn maas_experiment.wsgi:application --bind 0.0.0.0:8000 --access-logfile '-' --error-logfile '-'
+gunicorn maas_experiment.wsgi:application --bind 0.0.0.0:8000 --access-logfile '-' --error-logfile '-' --timeout ${_WORKER_TIMEOUT}
