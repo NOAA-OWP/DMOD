77b3cd83ea1403adf759cfe5a63ecff826329193
Robert Bartel <robert.bartel@noaa.gov>
2022-04-25 14:14:52 -0500

    Add client_debug_helper.py to GUI image.
    Adding script to assist with running the CLI client via the GUI service
    container and still utilize host debugging.

---
+++ docker/nwm_gui/app_server/client_debug_helper.py
@@ -0,0 +1,19 @@
+import sys
+import os
+sys.path.append('/pydevd-pycharm.egg')
+import pydevd_pycharm
+import dmod.client
+from dmod.client.__main__ import main
+
+gui_debug_port = int(os.environ.get('MAAS_PORTAL_DEBUG_PORT', 55875))
+debug_port = gui_debug_port + 1
+debug_server = 'host.docker.internal'
+
+try:
+    pydevd_pycharm.settrace(debug_server, port=debug_port, stdoutToServer=True, stderrToServer=True)
+    main()
+except Exception as error:
+    msg = 'Warning: could not set GUI debugging trace to {} on {} due to {} - {}'
+    print(msg.format(debug_server, debug_port, error.__class__.__name__, str(error)))
+
+
