7ab750567455048e74418ccaab7c49aded6f0f8a
Robert Bartel <robert.bartel@noaa.gov>
2022-05-17 15:58:28 -0500

    Update client debug helper for non-debug use.
    Adding option to be able to use it without debugging, to simplify
    switching back and forth.

--- docker/nwm_gui/app_server/client_debug_helper.py
+++ docker/nwm_gui/app_server/client_debug_helper.py
@@ -9,8 +9,12 @@ gui_debug_port = int(os.environ.get('MAAS_PORTAL_DEBUG_PORT', 55875))
 debug_port = gui_debug_port + 1
 debug_server = 'host.docker.internal'
 
+# Support an ENV var "NO_DEBUG" for not doing this
+skip_debug = os.getenv('NO_DEBUG')
+
 try:
-    pydevd_pycharm.settrace(debug_server, port=debug_port, stdoutToServer=True, stderrToServer=True)
+    if skip_debug is None:
+        pydevd_pycharm.settrace(debug_server, port=debug_port, stdoutToServer=True, stderrToServer=True)
     main()
 except Exception as error:
     msg = 'Warning: could not set GUI debugging trace to {} on {} due to {} - {}'
