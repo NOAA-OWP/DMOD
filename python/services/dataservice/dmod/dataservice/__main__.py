491d7970e4eed590ab85e423310e3239e1283b7d
Robert Bartel <robert.bartel@noaa.gov>
2022-05-31 13:21:08 -0500

    Add async task for data provisioning.
    Adding async task for dataservice manager manage_data_provision function
    in package __main__.

--- python/services/dataservice/dmod/dataservice/__main__.py
+++ python/services/dataservice/dmod/dataservice/__main__.py
@@ -140,6 +140,7 @@ def main():
 
     # Setup other required async tasks
     service_manager.add_async_task(service_manager.manage_required_data_checks())
+    service_manager.add_async_task(service_manager.manage_data_provision())
 
     service_manager.run()
 
