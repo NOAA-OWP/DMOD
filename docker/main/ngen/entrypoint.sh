1a5e76b1d4e3ffec36db56c0b4d46d202088e043
Robert Bartel <robert.bartel@noaa.gov>
2022-06-02 13:09:28 -0500

    17: Adjust ngen entrypoint scripts.
    Adjustments for how hosts args come in, how datasets are handled, and
    how the SSH daemon needs to be started.

--- docker/main/ngen/entrypoint.sh
+++ docker/main/ngen/entrypoint.sh
@@ -2,7 +2,7 @@
 # Managed by the _generate_docker_cmd_args function in scheduler.py of dmod.scheduler
 #
 # $1 will have the number of nodes associated with this run
-# $2 will have the host string in MPI form, i.e. hostname:N, hostname:M
+# $2 will have comma-delimited host strings in MPI form; e.g., hostname:N,hostname:M
 # $3 will have the unique job id
 # $4 is the worker index
 # $5 will be the name of the output dataset (which will imply a directory location)
@@ -10,8 +10,6 @@
 # $7 will be the name of the realization configuration dataset (which will imply a directory location)
 # $8 will be the name of the BMI configuration dataset (which will imply a directory location)
 # $9 will be the name of the partition configuration dataset (which will imply a directory location)
-# $10 and beyond will have colon-joined category+name strings (e.g., FORCING:aorc_csv_forcings_1) for Minio object store
-#       datasets to mount
 
 MPI_NODE_COUNT="${1:?No MPI node count given}"
 MPI_HOST_STRING="${2:?No MPI host string given}"
@@ -29,56 +27,29 @@ DOCKER_SECRETS_DIR="/run/secrets"
 ACCESS_KEY_FILE="${DOCKER_SECRETS_DIR}/${ACCESS_KEY_SECRET}"
 SECRET_KEY_FILE="${DOCKER_SECRETS_DIR}/${SECRET_KEY_SECRET}"
 
-ALL_DATASET_DIR="/dmod/dataset"
-OUTPUT_DATASET_DIR="${ALL_DATASET_DIR}/output/main"
-HYDROFABRIC_DATASET_DIR="${ALL_DATASET_DIR}/hydrofabric/main"
-REALIZATION_CONFIG_DATASET_DIR="${ALL_DATASET_DIR}/config/realization"
-BMI_CONFIG_DATASET_DIR="${ALL_DATASET_DIR}/config/bmi"
-PARTITION_DATASET_DIR="${ALL_DATASET_DIR}/config/partitions"
+MPI_USER="mpi"
+if [ "$(whoami)" = "${MPI_USER}" ]; then
+    MPI_HOSTS_FILE="${HOME}/.mpi_hosts"
+else
+    MPI_HOSTS_FILE="$(su ${MPI_USER} -c 'echo "${HOME}"')/.mpi_hosts"
+fi
 
-S3FS_PASSWD_FILE="${HOME}/.passwd-s3fs"
+MPI_RUN="mpirun"
+#NGEN_EXECUTABLE="ngen"
+NGEN_EXECUTABLE="/ngen/ngen/cmake_build/ngen"
 
-MPI_HOSTS_FILE="${HOME}/.mpi_hosts"
+ALL_DATASET_DIR="/dmod/datasets"
+OUTPUT_DATASET_DIR="${ALL_DATASET_DIR}/output/${OUTPUT_DATASET_NAME}"
+HYDROFABRIC_DATASET_DIR="${ALL_DATASET_DIR}/hydrofabric/${HYDROFABRIC_DATASET_NAME}"
+REALIZATION_CONFIG_DATASET_DIR="${ALL_DATASET_DIR}/config/${REALIZATION_CONFIG_DATASET_NAME}"
+BMI_CONFIG_DATASET_DIR="${ALL_DATASET_DIR}/config/${BMI_CONFIG_DATASET_NAME}"
+PARTITION_DATASET_DIR="${ALL_DATASET_DIR}/config/${PARTITION_DATASET_NAME}"
 
-# Mount an object store dataset of the given name and data category (which implies mount point directory)
-mount_object_store_dataset()
+RUN_SENTINEL="/home/${MPI_USER}/.run_sentinel"
+
+print_date()
 {
-    # Dataset name is $1
-    # Dataset category (lower case) is $2
-    case "${1}" in
-        "${OUTPUT_DATASET_NAME}")
-            _MOUNT_DIR="${OUTPUT_DATASET_DIR}"
-            ;;
-        "${HYDROFABRIC_DATASET_NAME}")
-            _MOUNT_DIR="${HYDROFABRIC_DATASET_DIR}"
-            ;;
-        "${REALIZATION_CONFIG_DATASET_NAME}")
-            _MOUNT_DIR="${REALIZATION_CONFIG_DATASET_DIR}"
-            ;;
-        "${BMI_CONFIG_DATASET_NAME}")
-            _MOUNT_DIR="${BMI_CONFIG_DATASET_DIR}"
-            ;;
-        "${PARTITION_DATASET_NAME}")
-            _MOUNT_DIR="${PARTITION_DATASET_DIR}"
-            ;;
-        *)
-            _MOUNT_DIR="${ALL_DATASET_DIR}/${2:?}/${1}"
-    esac
-
-    # TODO (later): this is a non-S3 implementation URL; add support for S3 directly also
-    # This is based on the nginx proxy config (hopefully)
-    _URL="http://minio_proxy:9000/"
-    s3fs ${1} ${_MOUNT_DIR} -o passwd_file=${HOME}/.passwd-s3fs -o url=${_URL} -o use_path_request_style
-}
-
-parse_object_store_strings()
-{
-    while [ ${#} -gt 0 ]; do
-        _CAT="$(echo "${1}"| sed -e 's/\([^:]*\):.*/\1/' | awk '{print tolower($0)}')"
-        _NAME="$(echo "${1}"| sed -e 's/\([^:]*\):\(.*\)/\2/')"
-        mount_object_store_dataset ${_NAME} ${_CAT}
-        shift
-    done
+    date "+%Y-%m-%d,%H:%M:%S"
 }
 
 check_for_dataset_dir()
@@ -91,29 +62,69 @@ check_for_dataset_dir()
     fi
 }
 
-# Read Docker Secrets files for Object Store access, if they exist
-if [ -e "${ACCESS_KEY_FILE}" ]; then
-    ACCESS_KEY="$(cat "${ACCESS_KEY_FILE}")"
-fi
-if [ -e "${SECRET_KEY_FILE}" ]; then
-    SECRET_KEY="$(cat "${SECRET_KEY_FILE}")"
-fi
-
-# Execute object store routine if we have an access key
-if [ -n "${ACCESS_KEY:-}" ]; then
-    # Of course, bail if we don't have the secret key also
-    if [ -z "${SECRET_KEY:-}" ]; then
-        echo "Error: ACCESS_KEY provided for Minio object store access, but no SECRET_KEY provided" 2>&1
-        exit 1
+load_object_store_keys_from_docker_secrets()
+{
+    # Read Docker Secrets files for Object Store access, if they exist
+    if [ -z "${ACCESS_KEY_FILE:-}" ]; then
+        echo "WARN: Cannot load object store access key when Docker secret file name not set"
+    elif [ -e "${ACCESS_KEY_FILE}" ]; then
+        ACCESS_KEY="$(cat "${ACCESS_KEY_FILE}")"
+    else
+        echo "WARN: Cannot load object store access key when Docker secret file does not exist"
     fi
 
-    # Configure auth for s3fs
-    echo ${ACCESS_KEY}:${SECRET_KEY} > "${S3FS_PASSWD_FILE}"
-    chmod 600 "${S3FS_PASSWD_FILE}"
+    if [ -z "${SECRET_KEY_FILE:-}" ]; then
+            echo "WARN: Cannot load object store secret key when Docker secret file name not set"
+    elif [ -e "${SECRET_KEY_FILE}" ]; then
+        SECRET_KEY="$(cat "${SECRET_KEY_FILE}")"
+    else
+        echo "WARN: Cannot load object store secret key when Docker secret file does not exist"
+    fi
 
-    # Parse args and mount any object stores datasets appropriately
-    parse_object_store_strings ${@:10}
-fi
+    test -n "${ACCESS_KEY:-}" && test -n "${SECRET_KEY:-}"
+}
+
+exec_main_worker_ngen_run()
+{
+    # Write (split) hoststring to a proper file
+    if [ -e "${MPI_HOSTS_FILE}" ]; then
+        rm "${MPI_HOSTS_FILE}"
+    fi
+
+    echo "$(print_date) Preparing hosts file and checking ${MPI_NODE_COUNT} worker hosts are online for SSH"
+    for i in $(echo "${MPI_HOST_STRING}" | sed 's/,/ /g'); do
+        #echo "${i}" | awk -F: '{print $1 " slots=" $2}' >> "${MPI_HOSTS_FILE}"
+        echo "${i}" | awk -F: '{print $1 ":" $2}' >> "${MPI_HOSTS_FILE}"
+        _HOST_NAME=$(echo "${i}" | awk -F: '{print $1}')
+        # Make sure all hosts are reachable, this also covers localhost
+        until ssh -q ${_HOST_NAME} exit >/dev/null 2>&1; do :; done
+        echo "DEBUG: Confirmed MPI host ${_HOST_NAME} is online for SSH"
+    done
+
+    # Execute the model on the linked data
+    echo "$(print_date) Executing mpirun command for ngen on ${MPI_NODE_COUNT} workers"
+    ${MPI_RUN:?} -f "${MPI_HOSTS_FILE}" -n ${MPI_NODE_COUNT} \
+        ${NGEN_EXECUTABLE:?} ${HYDROFABRIC_DATASET_DIR}/catchment_data.geojson "" \
+                ${HYDROFABRIC_DATASET_DIR}/nexus_data.geojson "" \
+                ${REALIZATION_CONFIG_DATASET_DIR}/realization_config.json \
+                ${PARTITION_DATASET_DIR}/partition_config.json
+
+    #Capture the return value to use as service exit code
+    NGEN_RETURN=$?
+
+    echo "$(print_date) ngen mpirun command finished with return value: ${NGEN_RETURN}"
+
+    # Close the other workers by removing this file
+    for i in $(echo "${MPI_HOST_STRING}" | sed 's/,/ /g'); do
+        _HOST_NAME=$(echo "${i}" | awk -F: '{print $1}')
+        ssh -q ${_HOST_NAME} rm ${RUN_SENTINEL} >/dev/null 2>&1
+    done
+
+    echo "$(print_date) DEBUG: closed other worker SSH processes"
+
+    # Exit with the model's exit code
+    return ${NGEN_RETURN}
+}
 
 # Sanity check that the output, hydrofabric, and config datasets are available (i.e., their directories are in place)
 check_for_dataset_dir "${REALIZATION_CONFIG_DATASET_DIR}"
@@ -126,35 +137,34 @@ check_for_dataset_dir "${OUTPUT_DATASET_DIR}"
 cd ${OUTPUT_DATASET_DIR}
 
 if [ "${WORKER_INDEX}" = "0" ]; then
-    echo "Starting SSH daemon on main worker"
-    sudo /usr/sbin/sshd -D &
+    if [ "$(whoami)" = "${MPI_USER}" ]; then
+        exec_main_worker_ngen_run
+    else
+        echo "$(print_date) Starting SSH daemon on main worker"
+        /usr/sbin/sshd -D &
+        _SSH_D_PID="$!"
 
-    # Write hoststring to file
-    echo "${MPI_HOST_STRING}" > "${MPI_HOSTS_FILE}"
+        # Start the SSH daemon as a power user, but then actually run the model as our MPI_USER
+        echo "$(print_date) Running exec script as '${MPI_USER:?}'"
+        # Do this by just re-running this script with the same args, but as the other user
+        # The script will modify its behavior as needed depending on current user (see associated "if" for this "else")
+        _EXEC_STRING="${0} ${@}"
+        su ${MPI_USER:?} --session-command "${_EXEC_STRING}"
+        #time su ${MPI_USER:?} --session-command "${_EXEC_STRING}"
 
-    CONFIGURED_CPU_COUNT=0
-    # COUNT total CPUS and make sure hosts are running ssh
-    for _HOST in $(cat "${MPI_HOSTS_FILE}"); do
-        _CPUS=$(echo "${_HOST}" | cut -d ':' -f 2)
-        _HOST_NAME=$(echo "${_HOST}" | cut -d ':' -f 1)
-        CONFIGURED_CPU_COUNT=$((CONFIGURED_CPU_COUNT+_CPUS))
-        #Make sure all hosts are reachable, this also covers localhost
-        until ssh -q ${_HOST_NAME} exit >/dev/null 2>&1; do :; done
+        # Once running the model finishes, kill the SSH daemon process
+        kill ${_SSH_D_PID}
+    fi
+else
+    echo "$(print_date) Starting SSH daemon, waiting for main job"
+    /usr/sbin/sshd -D &
+    _SSH_D_PID="$!"
+
+    touch ${RUN_SENTINEL}
+    chown ${MPI_USER} ${RUN_SENTINEL}
+    while [ -e ${RUN_SENTINEL} ]; do
+        sleep 5
     done
 
-    # Execute the model on the linked data
-    /usr/local/bin/mpirun -f hostfile -n ${CONFIGURED_CPU_COUNT} \
-        ngen ${HYDROFABRIC_DATASET_DIR}/catchment_data.geojson "" \
-                ${HYDROFABRIC_DATASET_DIR}/nexus_data.geojson "" \
-                ${REALIZATION_CONFIG_DATASET_DIR}/realization_config.json \
-                ${PARTITION_DATASET_DIR}/partition_config.json > std_out.log 2> std_err.log
-
-    #Capture the return value to use as service exit code
-    NGEN_RETURN=$?
-    echo 'ngen returned with a return value: ' {NGEN_RETURN}
-    # Exit with the model's exit code
-    exit ${NGEN_RETURN}
-else
-    echo "Starting SSH daemon, waiting for main job"
-    sudo /usr/sbin/sshd -D
+    kill ${_SSH_D_PID}
 fi
