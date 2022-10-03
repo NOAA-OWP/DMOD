da982c4343373771f4afc664a1fc05e9ed7569e9
Robert Bartel <robert.bartel@noaa.gov>
2022-10-03 11:48:48 -0500

    Refactor DatasetManagementView inheritance.
    Make DatasetManagementView inherit from AbstractDatasetView so certain
    required pieces could be centrally located and reused.

--- python/gui/MaaS/cbv/DatasetManagementView.py
+++ python/gui/MaaS/cbv/DatasetManagementView.py
@@ -2,26 +2,20 @@
 Defines a view that may be used to configure a MaaS request
 """
 import asyncio
-import os
 from django.http import HttpRequest, HttpResponse
-from django.views.generic.base import View
 from django.shortcuts import render
 
 import dmod.communication as communication
-from dmod.client.request_clients import DatasetExternalClient
 from dmod.core.meta_data import DataCategory, DataFormat
 
 import logging
 logger = logging.getLogger("gui_log")
 
-from pathlib import Path
-
-from .DMODProxy import DMODMixin, GUI_STATIC_SSL_DIR
 from .utils import extract_log_data
-from typing import Dict
+from .AbstractDatasetView import AbstractDatasetView
 
 
-class DatasetManagementView(View, DMODMixin):
+class DatasetManagementView(AbstractDatasetView):
 
     """
     A view used to configure a dataset management request or requests for transmitting dataset data.
@@ -29,7 +23,6 @@ class DatasetManagementView(View, DMODMixin):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._dataset_client = None
 
     def _process_event_type(self, http_request: HttpRequest) -> communication.MessageEventType:
         """
@@ -48,17 +41,6 @@ class DatasetManagementView(View, DMODMixin):
         """
         # TODO:
         raise NotImplementedError("{}._process_event_type not implemented".format(self.__class__.__name__))
-
-    @property
-    def dataset_client(self) -> DatasetExternalClient:
-        if self._dataset_client is None:
-            self._dataset_client = DatasetExternalClient(endpoint_uri=self.maas_endpoint_uri,
-                                                         ssl_directory=GUI_STATIC_SSL_DIR)
-        return self._dataset_client
-
-    async def _get_datasets(self) -> Dict[str, dict]:
-        serial_datasets = await self.dataset_client.get_serialized_datasets()
-        return serial_datasets
 
     def get(self, http_request: HttpRequest, *args, **kwargs) -> HttpResponse:
         """
@@ -81,7 +63,7 @@ class DatasetManagementView(View, DMODMixin):
         errors, warnings, info = extract_log_data(kwargs)
 
         # Gather map of serialized datasets, keyed by dataset name
-        serial_dataset_map = asyncio.get_event_loop().run_until_complete(self._get_datasets())
+        serial_dataset_map = asyncio.get_event_loop().run_until_complete(self.get_datasets())
         serial_dataset_list = [serial_dataset_map[d] for d in serial_dataset_map]
 
         dataset_categories = [c.name.title() for c in DataCategory]
@@ -96,7 +78,6 @@ class DatasetManagementView(View, DMODMixin):
             'warnings': warnings
         }
 
-        # TODO: create this file
         return render(http_request, 'maas/dataset_management.html', payload)
 
     def post(self, http_request: HttpRequest, *args, **kwargs) -> HttpResponse:
