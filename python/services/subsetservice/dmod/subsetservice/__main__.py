b47516c86650b18f8c0721796b7f0cf632ad71e3
Robert Bartel <robert.bartel@noaa.gov>
2022-10-31 14:12:33 -0500

    Adjust subsetservice to multiple geojson handlers.
    Adjust service to use GeoJsonBackedSubsetHandler handlers specifically
    and to potentially support multiple hydrofabrics when not in CLI mode.

--- python/services/subsetservice/dmod/subsetservice/__main__.py
+++ python/services/subsetservice/dmod/subsetservice/__main__.py
@@ -1,9 +1,10 @@
 import argparse
 import flask
 import json
-from dmod.modeldata import SubsetDefinition, SubsetHandler
+from dmod.modeldata import SubsetDefinition
+from dmod.modeldata.subset.subset_handler import GeoJsonBackedSubsetHandler
 from pathlib import Path
-from typing import Optional
+from typing import Dict
 from .cli import Cli
 from . import name as package_name
 
@@ -17,7 +18,7 @@ logging.basicConfig(
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-subset_handler: SubsetHandler = None
+subset_handlers: Dict[str, GeoJsonBackedSubsetHandler] = dict()
 
 
 @app.route('/', methods=['GET'])
@@ -29,7 +30,8 @@ def home():
 @app.route('/subset/cat_id_valid', methods=['POST'])
 def is_catchment_id_valid():
     record = json.loads(flask.request.data)
-    # Expect JSON with 'id' key and then a single string id
+    # Expect JSON with 'id' key, 'fabric_name' key for the hydrofabric, and then a single string id
+    subset_handler = subset_handlers[record['fabric_name']]
     is_recognized = subset_handler.is_catchment_recognized(record['id'])
     return flask.jsonify({'catchment_id': record['id'], 'valid': is_recognized})
 
@@ -38,7 +40,8 @@ def is_catchment_id_valid():
 @app.route('/subset/for_cat_id', methods=['POST'])
 def get_subset_for_catchment_id():
     record = json.loads(flask.request.data)
-    # Expect JSON with 'ids' key and then a list of ids
+    subset_handler = subset_handlers[record['fabric_name']]
+    # Expect JSON with 'ids' key, 'fabric_name' key for the hydrofabric, and then a list of ids
     subset = subset_handler.get_subset_for(record['ids'])
     return flask.jsonify(subset.to_dict())
 
@@ -47,7 +50,8 @@ def get_subset_for_catchment_id():
 @app.route('/subset/upstream', methods=['POST'])
 def get_upstream_subset():
     record = json.loads(flask.request.data)
-    # Expect JSON with 'ids' key and then a list of ids
+    # Expect JSON with 'ids' key, 'fabric_name' key for the hydrofabric, and then a list of ids
+    subset_handler = subset_handlers[record['fabric_name']]
     # Potentially a limit key can also be provided
     limit = None
     if 'limit' in record:
@@ -60,7 +64,7 @@ def get_upstream_subset():
     return flask.jsonify(subset.to_dict())
 
 
-def _validate_subset(json_data):
+def _validate_subset(subset_handler, json_data):
     subset = SubsetDefinition.factory_init_from_deserialized_json(json_data)
     if subset is None:
         return flask.jsonify({'valid': False, 'reason': 'Could not deserialize to subset definition object'})
@@ -71,15 +75,17 @@ def _validate_subset(json_data):
 @app.route('/subset/validate', methods=['POST'])
 def validate_subset():
     record = json.loads(flask.request.data)
-    return _validate_subset(json_data=record['subset'] if 'subset' in record else record)
+    return _validate_subset(subset_handler=subset_handlers[record['fabric_name']],
+                            json_data=record['subset'] if 'subset' in record else record)
 
 
 @app.route('/subset/validate_file', methods=['POST'])
 def validate_subset_file():
     uploaded_file = flask.request.files['file']
+    record = json.loads(flask.request.data)
     if uploaded_file.filename == '':
         return flask.jsonify({'valid': False, 'reason': 'Invalid file or filename provided to validation routine'})
-    return _validate_subset(json_data=json.load(uploaded_file))
+    return _validate_subset(subset_handler=subset_handlers[record['fabric_name']], json_data=json.load(uploaded_file))
 
 
 def _handle_args():
@@ -177,8 +183,53 @@ def exec_cli_op(cli, args) -> bool:
                                  output_file_name=args.output_file)
 
 
+def _is_hydrofabric_dir(directory: Path, cat_file_name: str, nexus_file_name: str) -> bool:
+    catchment_data_path = directory.joinpath(cat_file_name)
+    nexus_data_path = directory.joinpath(nexus_file_name)
+    return directory.is_dir() and catchment_data_path.is_file() and nexus_data_path.is_file()
+
+
+def _determine_xwalk(hy_dir: Path, first_name: str) -> Path:
+    # Do some extra checking for crosswalk for alternative path/suffix
+    # TODO: this needs to be improved somehow
+    crosswalk_path: Path = hy_dir.joinpath(first_name)
+    if crosswalk_path.exists():
+        return crosswalk_path
+    else:
+        alt_xwalk = crosswalk_path.with_suffix('.csv')
+        if not alt_xwalk.exists():
+            msg = "Required crosswalk file to load hydrofabric not found at {} or {}"
+            logging.error(msg.format(crosswalk_path, alt_xwalk))
+            raise RuntimeError(msg.format(crosswalk_path, alt_xwalk))
+        else:
+            return alt_xwalk
+
+
+def _load_subset_handler(hy_dir: Path, catchment_file_name: str, nexus_file_name: str, crosswalk_name: str) -> GeoJsonBackedSubsetHandler:
+    # TODO: add more intelligence to file base names and detection
+    #logging.info("Loading hydrofabric files from {}".format(args.files_directory))
+    logging.info("Loading hydrofabric files from {}".format(hy_dir))
+
+    #catchment_data_path = args.files_directory.joinpath(args.catchment_data_file)
+    #nexus_data_path = args.files_directory.joinpath(args.nexus_data_file)
+    catchment_data_path = hy_dir.joinpath(catchment_file_name)
+    nexus_data_path = hy_dir.joinpath(nexus_file_name)
+
+    try:
+        crosswalk_path: Path = _determine_xwalk(hy_dir=hy_dir, first_name=crosswalk_name)
+    except RuntimeError as e:
+        msg = "Subset service start failed: {}".format(str(e))
+        logging.error(msg)
+        raise RuntimeError(msg)
+
+    subset_handler = GeoJsonBackedSubsetHandler(catchment_data=catchment_data_path, nexus_data=nexus_data_path,
+                                                cross_walk=crosswalk_path)
+    logging.info("{} hydrofabric loaded into subset handler".format(hy_dir.name))
+    return subset_handler
+
+
 def main():
-    global subset_handler
+    global subset_handlers
     args = _handle_args()
 
     if args.pycharm_debug:
@@ -206,21 +257,41 @@ def main():
     # TODO: try to split off functionality so that Flask stuff (though declared globally) isn't started for CLI ops
 
     if not args.files_directory.is_dir():
-        print("Error: given param '{}' for files directory is not an existing directory".format(args.files_directory))
+        logging.error("Given param '{}' for files directory is not an existing directory".format(args.files_directory))
 
-    catchment_data_path = args.files_directory.joinpath(args.catchment_data_file)
-    nexus_data_path = args.files_directory.joinpath(args.nexus_data_file)
-    crosswalk_path = args.files_directory.joinpath(args.crosswalk_file)
+    running_cli = args.partition_file or args.do_simple_subset or args.do_upstream_subset
 
-    subset_handler = SubsetHandler.factory_create_from_geojson(catchment_data=catchment_data_path,
-                                                               nexus_data=nexus_data_path,
-                                                               cross_walk=crosswalk_path)
+    subdirs = [d for d in args.files_directory.blob('*') if d.is_dir()]
 
-    if args.partition_file or args.do_simple_subset or args.do_upstream_subset:
-        cli = Cli(catchment_geojson=catchment_data_path, nexus_geojson=nexus_data_path, crosswalk_json=crosswalk_path,
-                  partition_file_str=args.partition_file, subset_handler=subset_handler)
+    if len(subdirs) == 0 and _is_hydrofabric_dir(directory=args.files_directory, cat_file_name=args.catchment_data_file,
+                                                 nexus_file_name=args.nexus_data_file):
+        sub_handler = _load_subset_handler(hy_dir=args.files_directory, catchment_file_name=args.catchment_data_file,
+                                           nexus_file_name=args.nexus_data_file, crosswalk_name=args.crosswalk_file)
+        subset_handlers[args.files_directory.name] = sub_handler
+
+        if running_cli:
+            cli = Cli(catchment_geojson=args.files_directory.joinpath(args.catchment_data_file),
+                      nexus_geojson=args.files_directory.joinpath(args.catchment_data_file),
+                      crosswalk_json=_determine_xwalk(hy_dir=args.files_directory, first_name=args.crosswalk_file),
+                      partition_file_str=args.partition_file, subset_handler=sub_handler)
+        else:
+            cli = None
+    else:
+        cli = None
+        for hy_dir in subdirs:
+            if _is_hydrofabric_dir(hy_dir, args.catchment_data_file, args.nexus_data_file):
+                subset_handlers[hy_dir.name] = _load_subset_handler(hy_dir=hy_dir,
+                                                                    catchment_file_name=args.catchment_data_file,
+                                                                    nexus_file_name=args.nexus_data_file,
+                                                                    crosswalk_name=args.crosswalk_file)
+            else:
+                logging.info("Skipping subdirectory {} without hydrofabric data files from".format(hy_dir))
+
+    if running_cli and cli is None:
+        logging.error('Cannot run subset CLI functionality without valid directory containing a single hydrofabric')
+        exit(1)
+    elif running_cli and cli is not None:
         result = exec_cli_op(cli, args)
-
     else:
         logging.info("Starting app API service on port {}".format(args.port))
         app.run(host=args.host, port=args.port)
