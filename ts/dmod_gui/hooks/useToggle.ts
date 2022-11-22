ef6d4a36ef289fd85e5b5cbda69b8a5add07fa7c
Austin Raney <austin.raney@noaa.gov>
2022-11-22 08:48:40 -0500

    add useArray, useLocalStorage, and useToggle hooks

---
+++ ts/dmod_gui/hooks/useToggle.ts
@@ -0,0 +1,9 @@
+import { useCallback, useState } from 'react';
+
+export const useToggle = (initialState: boolean = false): [boolean, any] => {
+    const [state, setState] = useState<boolean>(initialState);
+    const toggle = useCallback((): void => setState(state => !state), []);
+    return [state, toggle]
+}
+
+export default useToggle;
