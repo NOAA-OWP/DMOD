5309b9c0bcc20bfded8b33104e9f354789c24c18
Austin Raney <austin.raney@noaa.gov>
2022-11-22 09:12:18 -0500

    add calibration page

---
+++ ts/dmod_gui/pages/calibration/index.tsx
@@ -0,0 +1,12 @@
+import Box from "@mui/material/Box";
+import CalibrationWizard from "../../components/ngen/CalibrationWizard";
+
+export const Index = () => {
+  return (
+    <Box sx={{ margin: "2em 10em" }}>
+      <CalibrationWizard />
+    </Box>
+  );
+};
+
+export default Index;
