5c6e754d77151a3da99c7f976d49902d5b59f35e
Robert Bartel <robert.bartel@noaa.gov>
2022-11-23 09:34:40 -0600

    Update dataset mgr view with full create support.

--- python/gui/MaaS/cbv/DatasetManagementView.py
+++ python/gui/MaaS/cbv/DatasetManagementView.py
@@ -4,9 +4,11 @@ Defines a view that may be used to configure a MaaS request
 import asyncio
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.core.files.uploadedfile import UploadedFile
+from django.conf import settings
 
 import dmod.communication as communication
-from dmod.core.meta_data import DataCategory, DataFormat
+from dmod.core.meta_data import DataCategory, DataDomain, DataFormat
 
 import logging
 logger = logging.getLogger("gui_log")
@@ -14,6 +16,9 @@ logger = logging.getLogger("gui_log")
 from .utils import extract_log_data
 from .AbstractDatasetView import AbstractDatasetView
 from .DatasetManagementForms import DatasetForm, DatasetFormatForm
+from typing import List
+
+DT_FORMAT = settings.DATE_TIME_FORMAT
 
 
 class DatasetManagementView(AbstractDatasetView):
@@ -42,6 +47,32 @@ class DatasetManagementView(AbstractDatasetView):
         """
         # TODO:
         raise NotImplementedError("{}._process_event_type not implemented".format(self.__class__.__name__))
+
+    def _create_dataset(self, name: str, category: str, data_format: str, *args, **kwargs) -> bool:
+        d_format = DataFormat.get_for_name(data_format)
+        if format is None:
+            return False
+        else:
+            try:
+                domain = DataDomain.factory_init_from_restriction_collections(d_format, **kwargs)
+            except Exception as e:
+                msg = 'Failed to create dataset {}: {} creating domain ({})'.format(name, e.__class__.__name__, str(e))
+                logger.error(msg)
+                raise RuntimeError(msg)
+        return asyncio.get_event_loop().run_until_complete(
+            self.dataset_client.create_dataset(name=name, category=DataCategory.get_for_name(category), domain=domain))
+
+    def _upload_files_to_dataset(self, dataset_name: str, files: List[UploadedFile]) -> bool:
+        # TODO: (later) consider modifying files to account for DMOD-specific characteristics (e.g., file paths for
+        #  inside worker containers)
+        minio_client = self.factory_minio_client()
+        result = True
+        for f in files:
+            result_obj = minio_client.put_object(bucket_name=dataset_name, object_name=f.name, data=f.file,
+                                                 length=f.file.getbuffer().nbytes)
+            # TODO: (later) try to do something based on result_obj.last_modified
+            result = result and result_obj.bucket_name == dataset_name and result_obj.object_name == f.name
+        return result
 
     def get(self, http_request: HttpRequest, *args, **kwargs) -> HttpResponse:
         """
@@ -102,18 +133,48 @@ class DatasetManagementView(AbstractDatasetView):
         -------
         A rendered page.
         """
-        # TODO: implement this to figure out whether DATASET_MANAGEMENT or DATA_TRANSMISSION
-        event_type = self._process_event_type(http_request)
-        client, session_data, dmod_response = self.forward_request(http_request, event_type)
+        # Should get a list of file-type objects, with a ``name`` property and a ``file`` BytesIO property
+        files = http_request.FILES.getlist('files')
 
-        # TODO: this probably isn't exactly correct, so review once closer to completion
-        if dmod_response is not None and 'dataset_id' in dmod_response.data:
-            session_data['new_dataset_id'] = dmod_response.data['dataset_id']
+        # name (dataset name), category, data_format, and any other applicable dynamic form items
+        # e.g., catchment_id, hydrofabric_id, data_id, etc.
+        dataset_details = dict([(k, v) for k, v in http_request.POST.items() if k != 'csrfmiddlewaretoken'])
+        dataset_name = dataset_details['name']
 
-        http_response = self.get(http_request=http_request, errors=client.errors, warnings=client.warnings,
-                                 info=client.info, *args, **kwargs)
+        # TODO: consider reading files to validate/replace domain details from form
 
-        for k, v in session_data.items():
-            http_response.set_cookie(k, v)
+        # If present, parse catchment ids string to list of individual ids
+        if 'catchment_id' in dataset_details:
+            dataset_details['catchment_id'] = [s.strip() for s in dataset_details.pop('catchment_id').split(',')]
+
+        # Fix keys for start and end times
+        if 'start_time' in dataset_details and 'end_time' in dataset_details:
+            dataset_details['time'] = {'start': dataset_details.pop('start_time'), 'end': dataset_details.pop('end_time')}
+        elif 'start_time' in dataset_details or 'end_time' in dataset_details:
+            # TODO: figure out best way to handle this; for now ...
+            raise RuntimeError('Cannot create a dataset of this format unless both a start and end time are given')
+
+        was_created = self._create_dataset(**dataset_details)
+
+        if not was_created:
+            err_msg = 'Could not created dataset {}'.format(dataset_name)
+            logger.error(err_msg)
+            http_response = self.get(http_request=http_request, errors=[err_msg], *args, **kwargs)
+        elif files is None or len(files) == 0:
+            info_msg = 'Created empty dataset {}'.format(dataset_name)
+            logger.info(info_msg)
+            http_response = self.get(http_request=http_request, info_msg=[info_msg], *args, **kwargs)
+        # With this condition test (if we get here), put files in dataset
+        elif not self._upload_files_to_dataset(dataset_name=dataset_name, files=files):
+            err_msg = 'Could not upload requested files to dataset {}'.format(dataset_name)
+            logger.error(err_msg)
+            http_response = self.get(http_request=http_request, errors=[err_msg], *args, **kwargs)
+        else:
+            info_msg = 'Created dataset {} with {} files uploaded'.format(dataset_name, len(files))
+            logger.info(info_msg)
+            http_response = self.get(http_request=http_request, info_msg=[info_msg], *args, **kwargs)
+
+        #for k, v in session_data.items():
+        #    http_response.set_cookie(k, v)
 
         return http_response
