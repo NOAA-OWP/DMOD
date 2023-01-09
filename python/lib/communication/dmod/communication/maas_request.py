d3add6f838eb593ab59fdddcb4d0a313d6393e1a
Austin Raney <austin.raney@noaa.gov>
2023-01-09 09:07:16 -0500

    refactor maas request parameter classes into module

--- python/lib/communication/dmod/communication/maas_request.py
+++ python/lib/communication/dmod/communication/maas_request.py
@@ -12,26 +12,6 @@ from numbers import Number
 from typing import Dict, List, Optional, Set, Union
 
 
-class Scalar(object):
-    """
-    Represents a parameter value that is bound to a single number
-    """
-    def __init__(self, scalar: int):
-        """
-        :param int scalar: The value for the parameter
-        """
-        self.scalar = scalar
-
-    def to_dict(self):
-        return {'scalar': self.scalar}
-
-    def __str__(self):
-        return str(self.scalar)
-
-    def __repr__(self):
-        return self.__str__()
-
-
 class Distribution(object):
     """
     Represents the definition of a distribution of numbers
@@ -406,28 +386,6 @@ class ModelExecRequestResponse(ExternalRequestResponse, ABC):
             return InitRequestResponseReason[self.reason]
         except:
             return InitRequestResponseReason.UNKNOWN
-
-
-class Parameter(object):
-    """
-        Base clase for model parameter descriptions that a given model may expose to DMOD for dynamic parameter selection.
-    """
-    def __init__(self, name):
-        """
-            Set the base meta data of the parameter
-        """
-        self.name = name
-
-
-class ScalarParameter(Parameter):
-    """
-        A Scalar parameter is a simple interger parameter who's valid range are integer increments between
-        min and max, inclusive.
-    """
-
-    def __init__(self, min, max):
-        self.min = min
-        self.max=max
 
 
 class NWMConfig:
