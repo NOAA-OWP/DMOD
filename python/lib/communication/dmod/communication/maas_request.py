0e3d76fd5ca94894026936a8d2a41240034b1811
Austin Raney <austin.raney@noaa.gov>
2023-01-09 09:08:25 -0500

    refactor maas request distribution classes into module

--- python/lib/communication/dmod/communication/maas_request.py
+++ python/lib/communication/dmod/communication/maas_request.py
@@ -12,30 +12,6 @@ from numbers import Number
 from typing import Dict, List, Optional, Set, Union
 
 
-class Distribution(object):
-    """
-    Represents the definition of a distribution of numbers
-    """
-    def __init__(self, minimum: int = 0, maximum: int = 0, distribution_type: str = 'normal'):
-        """
-        :param int minimum: The lower bound for the distribution
-        :param int maximum: The upper bound of the distribution
-        :param str distribution_type: The type of the distribution
-        """
-        self.minimum = minimum
-        self.maximum = maximum
-        self.distribution_type = distribution_type
-
-    def to_dict(self):
-        return {'distribution': {'min': self.minimum, 'max': self.maximum, 'type': self.distribution_type}}
-
-    def __str__(self):
-        return str(self.to_dict())
-
-    def __repr__(self):
-        return self.__str__()
-
-
 class ExternalRequest(AbstractInitRequest, ABC):
     """
     The base class underlying all types of externally-initiated (and, therefore, authenticated) MaaS system requests.
