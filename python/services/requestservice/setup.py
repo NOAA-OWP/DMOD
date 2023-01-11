0d61686274400b590df3289794f5cf712e72dfb6
Austin Raney <austin.raney@noaa.gov>
2023-01-11 15:01:49 -0500

    service readme's and version references now relative to their setup.py

--- python/services/requestservice/setup.py
+++ python/services/requestservice/setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup, find_namespace_packages
+from pathlib import Path
 
-with open('README.md', 'r') as readme:
+ROOT = Path(__file__).resolve().parent
+
+with open(ROOT / 'README.md', 'r') as readme:
     long_description = readme.read()
 
-exec(open('dmod/requestservice/_version.py').read())
+exec(open(ROOT / 'dmod/requestservice/_version.py').read())
 
 setup(
     name='dmod-requestservice',
