bf1a475217ea6a83801bc3513e17eb8fbde6205c
Austin Raney <austin.raney@noaa.gov>
2023-01-12 16:12:40 -0500

    refactor parameter module

--- python/lib/communication/dmod/communication/maas_request/parameter.py
+++ python/lib/communication/dmod/communication/maas_request/parameter.py
@@ -1,16 +1,11 @@
-class Scalar:
+from dmod.core.serializable import Serializable
+
+
+class Scalar(Serializable):
     """
     Represents a parameter value that is bound to a single number
     """
-
-    def __init__(self, scalar: int):
-        """
-        :param int scalar: The value for the parameter
-        """
-        self.scalar = scalar
-
-    def to_dict(self):
-        return {"scalar": self.scalar}
+    scalar: int
 
     def __str__(self):
         return str(self.scalar)
@@ -19,16 +14,11 @@ class Scalar:
         return self.__str__()
 
 
-class Parameter:
+class Parameter(Serializable):
     """
     Base clase for model parameter descriptions that a given model may expose to DMOD for dynamic parameter selection.
     """
-
-    def __init__(self, name):
-        """
-        Set the base meta data of the parameter
-        """
-        self.name = name
+    name: str
 
 
 class ScalarParameter(Parameter):
@@ -36,7 +26,5 @@ class ScalarParameter(Parameter):
     A Scalar parameter is a simple interger parameter who's valid range are integer increments between
     min and max, inclusive.
     """
-
-    def __init__(self, min, max):
-        self.min = min
-        self.max = max
+    min: int
+    max: int
