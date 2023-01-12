845b8e340cd6b2dc92b3ea70a6daa95b03940dac
Austin Raney <austin.raney@noaa.gov>
2023-01-12 14:06:32 -0500

    refactor UnsupportedMessageTypeResponse
    message field should not be optional

--- python/lib/communication/dmod/communication/unsupported_message.py
+++ python/lib/communication/dmod/communication/unsupported_message.py
@@ -4,12 +4,16 @@ from typing import Type
 
 
 class UnsupportedMessageTypeResponse(Response):
+    actual_event_type: MessageEventType
+    listener_type: Type[WebSocketInterface]
+    message: str
+
+    success = False
+    reason = "Message Event Type Unsupported"
 
     def __init__(self, actual_event_type: MessageEventType, listener_type: Type[WebSocketInterface],
-                 message: str = None, data=None):
+                 message: str = None, data=None, **kwargs):
         if message is None:
             message = 'The {} event type is not supported by this {} listener'.format(
                 actual_event_type, listener_type.__name__)
-        super().__init__(success=False, reason='Message Event Type Unsupported', message=message, data=data)
-        self.actual_event_type = actual_event_type
-        self.listener_type = listener_type
+        super().__init__(message=message, data=data, actual_event_type=actual_event_type, listener_type=listener_type)
