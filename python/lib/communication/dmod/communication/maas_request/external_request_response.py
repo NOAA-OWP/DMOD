70e10353991b7205185884585aa947fd6d303da3
Austin Raney <austin.raney@noaa.gov>
2023-01-12 16:31:14 -0500

    refactor ExternalRequestResponse

--- python/lib/communication/dmod/communication/maas_request/external_request_response.py
+++ python/lib/communication/dmod/communication/maas_request/external_request_response.py
@@ -1,13 +1,12 @@
 from abc import ABC
 
-from ..message import Response
+from ..message import AbstractInitRequest, Response
 from .external_request import ExternalRequest
+
+from typing import ClassVar, Type
 
 
 class ExternalRequestResponse(Response, ABC):
 
-    response_to_type = ExternalRequest
+    response_to_type: ClassVar[Type[AbstractInitRequest]] = ExternalRequest
     """ The type of :class:`AbstractInitRequest` for which this type is the response"""
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
