dcd0d1627643a6ac4d2b33fd8814d441dd9d02ac
Austin Raney <austin.raney@noaa.gov>
2023-01-12 16:30:00 -0500

    refactor distribution module

--- python/lib/communication/dmod/communication/maas_request/distribution.py
+++ python/lib/communication/dmod/communication/maas_request/distribution.py
@@ -1,7 +1,27 @@
-class Distribution:
+from dmod.core.serializable import Serializable
+
+from typing import Literal
+
+
+class DistributionBounds(Serializable):
+    minimum: int = 0
+    maximum: int = 0
+    distribution_type: Literal["normal"] = "normal"
+
+    class Config:
+        feilds = {
+            "distribution_type": {"alias": "type"},
+            "minimum": {"alias": "min"},
+            "maximum": {"alias": "max"},
+        }
+
+
+class Distribution(Serializable):
     """
     Represents the definition of a distribution of numbers
     """
+
+    distribution: DistributionBounds
 
     def __init__(
         self, minimum: int = 0, maximum: int = 0, distribution_type: str = "normal"
@@ -11,18 +31,38 @@ class Distribution:
         :param int maximum: The upper bound of the distribution
         :param str distribution_type: The type of the distribution
         """
-        self.minimum = minimum
-        self.maximum = maximum
-        self.distribution_type = distribution_type
+        super().__init__(
+            distribution=DistributionBounds(
+                minimum=minimum, maximum=maximum, distribution_type=distribution_type
+            )
+        )
 
-    def to_dict(self):
-        return {
-            "distribution": {
-                "min": self.minimum,
-                "max": self.maximum,
-                "type": self.distribution_type,
-            }
-        }
+    @property
+    def minimum(self) -> int:
+        """The lower bound for the distribution"""
+        return self.distribution.minimum
+
+    @minimum.setter
+    def minimum(self, value: int):
+        self.distribution.minimum = value
+
+    @property
+    def maximum(self) -> int:
+        """The upper bound for the distribution"""
+        return self.distribution.maximum
+
+    @maximum.setter
+    def maximum(self, value: int):
+        self.distribution.maximum = value
+
+    @property
+    def distribution_type(self) -> str:
+        """The type of the distribution"""
+        return self.distribution.distribution_type
+
+    @distribution_type.setter
+    def distribution_type(self, value: str) -> str:
+        self.distribution.distribution_type = value
 
     def __str__(self):
         return str(self.to_dict())
