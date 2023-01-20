7759c155f745ed08b4dc2ebd4d2975c2928a3dc9
Austin Raney <austin.raney@noaa.gov>
2023-01-20 17:31:22 -0500

    refactor DataTransmitResponse

--- python/lib/communication/dmod/communication/data_transmit_message.py
+++ python/lib/communication/dmod/communication/data_transmit_message.py
@@ -1,7 +1,8 @@
 from dmod.core.serializable import Serializable
+from pydantic import Extra
 from .message import AbstractInitRequest, MessageEventType, Response
 from pydantic import Field
-from typing import ClassVar, Dict, Optional, Union
+from typing import ClassVar, Dict, Optional, Type, Union
 from numbers import Number
 from uuid import UUID
 
@@ -63,6 +64,12 @@ class DataTransmitMessage(DataTransmitUUID, AbstractInitRequest):
     is_last: bool = Field(False, description="Whether this is the last data transmission message in this series.")
 
 
+class DataTransmitResponseBody(DataTransmitUUID):
+
+    class Config:
+        extra = Extra.allow
+
+
 class DataTransmitResponse(Response):
     """
     A ::class:`Response` subtype corresponding to ::class:`DataTransmitMessage`.
@@ -71,38 +78,24 @@ class DataTransmitResponse(Response):
     series of which it is a part.
     """
 
-    response_to_type = DataTransmitMessage
+    response_to_type: ClassVar[Type[AbstractInitRequest]] = DataTransmitMessage
 
-    _KEY_SERIES_UUID = response_to_type._KEY_SERIES_UUID
+    data: DataTransmitResponseBody
 
-    @classmethod
-    def factory_init_from_deserialized_json(cls, json_obj: dict) -> 'DataTransmitResponse':
-        """
-        Factory create a new instance of this type based on a JSON object dictionary deserialized from received JSON.
+    # `series_uuid` required in prior version of code
+    def __init__(self, series_uuid: Union[str, UUID] = None, **kwargs):
+        # assume no need for backwards compatibility
+        if series_uuid is None:
+            super().__init__(**kwargs)
+            return
 
-        Parameters
-        ----------
-        json_obj
+        if "data" not in kwargs:
+            kwargs["data"] = dict()
 
-        Returns
-        -------
-        response_obj : Response
-            A new object of this type instantiated from the deserialize JSON object dictionary, or none if the provided
-            parameter could not be used to instantiated a new object.
-        """
-        try:
-            return cls(success=json_obj['success'], reason=json_obj['reason'], message=json_obj['message'],
-                       series_uuid=json_obj['data'][cls._KEY_SERIES_UUID], data=json_obj['data'])
-        except Exception as e:
-            return None
-
-    def __init__(self, series_uuid: Union[str, UUID], *args, **kwargs):
-        if 'data' not in kwargs:
-            kwargs['data'] = dict()
-        kwargs['data'][self._KEY_SERIES_UUID] = str(series_uuid)
-        super(DataTransmitResponse, self).__init__(*args, **kwargs)
+        kwargs["data"]["series_uuid"] = series_uuid
+        super().__init__(**kwargs)
 
     @property
     def series_uuid(self) -> UUID:
-        return UUID(self.data[self._KEY_SERIES_UUID])
+        return self.data.series_uuid
 
