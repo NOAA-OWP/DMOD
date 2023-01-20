31b61fdba07d02a8055efb6adfab9f34b9e61dff
Austin Raney <austin.raney@noaa.gov>
2023-01-20 16:46:37 -0500

    refactor ModelExecRequestResponse

--- python/lib/communication/dmod/communication/maas_request/model_exec_request_response.py
+++ python/lib/communication/dmod/communication/maas_request/model_exec_request_response.py
@@ -1,34 +1,30 @@
 from abc import ABC
+from typing import Any, ClassVar, Dict, Optional, Type, Union
+from pydantic import validator
 
-from typing import Optional
-
-from ..message import InitRequestResponseReason
+from ..scheduler_request import SchedulerRequestResponse, UNSUCCESSFUL_JOB
+from ..message import AbstractInitRequest, InitRequestResponseReason
 from .external_request_response import ExternalRequestResponse
 from .model_exec_request import ModelExecRequest
+from .model_exec_request_response_body import ModelExecRequestResponseBody
 
 
 class ModelExecRequestResponse(ExternalRequestResponse, ABC):
 
-    _data_dict_key_job_id = "job_id"
-    _data_dict_key_output_data_id = "output_data_id"
-    _data_dict_key_scheduler_response = "scheduler_response"
-    response_to_type = ModelExecRequest
+    response_to_type: ClassVar[Type[AbstractInitRequest]] = ModelExecRequest
     """ The type of :class:`AbstractInitRequest` for which this type is the response"""
 
-    @classmethod
-    def _convert_scheduler_response_to_data_attribute(cls, scheduler_response=None):
-        if scheduler_response is None:
-            return None
-        elif isinstance(scheduler_response, dict) and len(scheduler_response) == 0:
-            return {}
-        elif isinstance(scheduler_response, dict):
-            return scheduler_response
-        else:
-            return {
-                cls._data_dict_key_job_id: scheduler_response.job_id,
-                cls._data_dict_key_output_data_id: scheduler_response.output_data_id,
-                cls._data_dict_key_scheduler_response: scheduler_response.to_dict(),
-            }
+    data: Optional[Union[ModelExecRequestResponseBody, Dict[str, Any]]] = None
+
+    @validator("data", pre=True)
+    def _convert_data_field(cls, value: Optional[Union[SchedulerRequestResponse, ModelExecRequestResponseBody, Dict[str, Any]]]) -> Optional[Union[ModelExecRequestResponseBody, Dict[str, Any]]]:
+        if value is None:
+            return value
+
+        elif isinstance(value, SchedulerRequestResponse):
+            return ModelExecRequestResponseBody.from_scheduler_request_response(value)
+
+        return value
 
     @classmethod
     def get_job_id_key(cls) -> str:
@@ -40,7 +36,7 @@ class ModelExecRequestResponse(ExternalRequestResponse, ABC):
         str
             Serialization dictionary key for the field containing the ::attribute:`job_id` property.
         """
-        return str(cls._data_dict_key_job_id)
+        return "job_id"
 
     @classmethod
     def get_output_data_id_key(cls) -> str:
@@ -52,7 +48,7 @@ class ModelExecRequestResponse(ExternalRequestResponse, ABC):
         str
             Serialization dictionary key for the field containing the ::attribute:`output_data_id` property.
         """
-        return str(cls._data_dict_key_output_data_id)
+        return "output_data_id"
 
     @classmethod
     def get_scheduler_response_key(cls) -> str:
@@ -64,52 +60,35 @@ class ModelExecRequestResponse(ExternalRequestResponse, ABC):
         str
             Serialization dictionary key for the field containing the 'scheduler_response' value.
         """
-        return str(cls._data_dict_key_scheduler_response)
+        return "scheduler_response"
 
-    @classmethod
-    def factory_init_from_deserialized_json(cls, json_obj: dict):
-        """
-        Factory create a new instance of this type based on a JSON object dictionary deserialized from received JSON.
+    def __init__(
+        self,
+        scheduler_response: Optional[
+            Union[
+                SchedulerRequestResponse, ModelExecRequestResponseBody, Dict[str, Any]
+            ]
+        ] = None,
+        **kwargs
+    ):
+        if scheduler_response is None:
+            super().__init__(**kwargs)
+            return
 
-        Parameters
-        ----------
-        json_obj
-
-        Returns
-        -------
-        response_obj : Response
-            A new object of this type instantiated from the deserialize JSON object dictionary, or none if the provided
-            parameter could not be used to instantiated a new object.
-
-        See Also
-        -------
-        _factory_init_data_attribute
-        """
-        try:
-            return cls(
-                success=json_obj["success"],
-                reason=json_obj["reason"],
-                message=json_obj["message"],
-                scheduler_response=json_obj["data"],
-            )
-        except Exception as e:
-            return None
-
-    def __init__(self, scheduler_response=None, *args, **kwargs):
-        data = self._convert_scheduler_response_to_data_attribute(scheduler_response)
-        if data is not None:
-            kwargs["data"] = data
-        super().__init__(*args, **kwargs)
+        # NOTE: if `scheduler_response` is not None, it is given precedence over "data" that might
+        # be present in `kwargs`.
+        kwargs["data"] = scheduler_response
+        super().__init__(**kwargs)
 
     @property
-    def job_id(self):
-        if (
-            not isinstance(self.data, dict)
-            or self._data_dict_key_job_id not in self.data
-        ):
-            return -1
-        else:
-            return self.data[self._data_dict_key_job_id]
+    def job_id(self) -> int:
+        if isinstance(self.data, ModelExecRequestResponseBody):
+            return self.data.job_id
+
+        elif isinstance(self.data, dict) and "job_id" in self.data:
+            return self.data["job_id"]
+
+        return UNSUCCESSFUL_JOB
 
     @property
     def output_data_id(self) -> Optional[str]:
@@ -121,13 +100,13 @@ class ModelExecRequestResponse(ExternalRequestResponse, ABC):
         Optional[str]
             The 'data_id' of the output dataset for requested job, if request was successful; otherwise ``None``.
         """
-        if (
-            not isinstance(self.data, dict)
-            or self._data_dict_key_output_data_id not in self.data
-        ):
-            return None
-        else:
-            return self.data[self._data_dict_key_output_data_id]
+        if isinstance(self.data, ModelExecRequestResponseBody):
+            return self.data.output_data_id
+
+        elif isinstance(self.data, dict) and "output_data_id" in self.data:
+            return self.data["output_data_id"]
+
+        return None
 
     @property
     def reason_enum(self):
