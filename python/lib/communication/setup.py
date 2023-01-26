3f35a607866e9d06135df25476f49111e8b17794
Austin Raney <austin.raney@noaa.gov>
2023-01-26 13:36:29 -0500

    bump minimum required version of dmod.core to 0.4.2

--- python/lib/communication/setup.py
+++ python/lib/communication/setup.py
@@ -21,7 +21,6 @@ setup(
     url='',
     license='',
     include_package_data=True,
-    #install_requires=['websockets', 'jsonschema'],vi
-    install_requires=['dmod-core>=0.1.2', 'websockets>=8.1', 'jsonschema', 'redis', 'pydantic'],
+    install_requires=['dmod-core>=0.4.2', 'websockets>=8.1', 'jsonschema', 'redis', 'pydantic'],
     packages=find_namespace_packages(include=['dmod.*'], exclude=['dmod.test'])
 )
