445789ebee3b230e71ff9bba26dad278a5f74b81
Robert Bartel <robert.bartel@noaa.gov>
2023-03-02 10:54:18 -0600

    Add GeoPackageHydrofabric to subpackage init.

--- python/lib/modeldata/dmod/modeldata/hydrofabric/__init__.py
+++ python/lib/modeldata/dmod/modeldata/hydrofabric/__init__.py
@@ -1,3 +1,4 @@
 from .hydrofabric import Hydrofabric, HydrofabricFilesManager, GeoJsonHydrofabricReader, GeoJsonHydrofabric, \
     MappedGraphHydrofabric
 from .partition import Partition, PartitionConfig
+from .geopackage_hydrofabric import GeoPackageHydrofabric
