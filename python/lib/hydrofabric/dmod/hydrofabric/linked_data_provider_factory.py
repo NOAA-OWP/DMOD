8eee3d268404c76ced8bc5424826c3d895f18410
Austin Raney <austin.raney@noaa.gov>
2023-03-08 17:33:37 -0500

    add linked data provider factory with single implimentation

---
+++ python/lib/hydrofabric/dmod/hydrofabric/linked_data_provider_factory.py
@@ -0,0 +1,18 @@
+import sqlite3
+from pathlib import Path
+
+from .linked_data_provider import LinkedDataProvider, GPKGLinkedDataProvider
+
+
+class LinkedDataProviderFactory:
+    @staticmethod
+    def factory_create(p: Path) -> LinkedDataProvider:
+        if not p.exists() or not p.is_file():
+            raise RuntimeError
+
+        if p.suffix.lower() == ".gpkg" or p.suffix.lower() == ".db":
+            # open connection in read only mode
+            uri = f"{p.as_uri()}?mode=ro"
+            connection = sqlite3.connect(uri, uri=True)
+            return GPKGLinkedDataProvider(connection=connection)
+        raise NotImplemented
