a56bec617a2ff85744dc40e73ddc429917ed4044
Robert Bartel <robert.bartel@noaa.gov>
2023-03-13 13:56:29 -0500

    Use test abstraction for geopackage hydrofabric.
    Updating test class for hydrofabric to use abstract tester class already
    in use for specialized catchment and nexus test classes; also cleaning
    up abstract test helper a little.

--- python/lib/modeldata/dmod/test/test_geopackage_hydrofabric.py
+++ python/lib/modeldata/dmod/test/test_geopackage_hydrofabric.py
@@ -1,56 +1,23 @@
-import git
-import hashlib
-import unittest
-from pathlib import Path
-from typing import Dict, List, Optional, Set, Union
-
-import numpy as np
+from typing import Dict, Set
 
 from ..modeldata.hydrofabric import GeoPackageHydrofabric
 from ..modeldata.hydrofabric.geopackage_hydrofabric import GeoPackageCatchment, GeoPackageNexus, SubsetDefinition
+from ..test.abstract_geopackage_hydrofabric_tester import AbstractGeoPackageHydrofabricTester
 
 
-class TestGeoPackageHydrofabric(unittest.TestCase):
-
-    _HYDROFABRIC_1_RELATIVE_PATH = 'data/example_hydrofabric_2/hydrofabric.gpkg'
-
-    @classmethod
-    def find_project_root(cls, path: Optional[Path] = None) -> Path:
-        """
-        Get the project root of the repo containing the given path.
-
-        Given a path (with ``None`` implying the current directory) assumed to be at or under a Git project's root,
-        find the project root directory.
-
-        Parameters
-        ----------
-        path : Path
-            A file path, or ``None`` to imply use the current directory.
-
-        Returns
-        -------
-        Path
-            The git repository root directory.
-        """
-        if path is None:
-            path = Path('.')
-        return Path(git.Repo(path, search_parent_directories=True).git.rev_parse("--show-toplevel"))
+class TestGeoPackageHydrofabric(AbstractGeoPackageHydrofabricTester):
 
     def setUp(self) -> None:
-        proj_root: Path = self.find_project_root()
-        
-        self.hydrofabric_ex: Dict[int, GeoPackageHydrofabric] = dict()
+        super().setUp()
+
         self.hydrofabric_uids: Dict[int, str] = dict()
         self.cat_id_sets: Dict[int, Set[str]] = dict()
         self.nexus_id_sets: Dict[int, Set[str]] = dict()
         self.subset_id_sets: Dict[int, Set[str]] = dict()
         self.root_catchment_ids_sets: Dict[int, Set[str]] = dict()
 
-        # Example 1: v1.2 VPU 1
+        # Example 1: extended hydrofabric testing attributes for hydrofabric for v1.2 VPU 1
         ex_idx = 1
-        file_path = proj_root.joinpath(self._HYDROFABRIC_1_RELATIVE_PATH)
-        #self.hydrofabric_ex[ex_idx] = GeoPackageHydrofabric.from_file(geopackage_file=Path.home().joinpath('Downloads/conus.gpkg'))
-        self.hydrofabric_ex[ex_idx] = GeoPackageHydrofabric.from_file(geopackage_file=file_path)
         self.hydrofabric_uids[ex_idx] = 'a98387f704b4fbdf5a958cb1cb7797a77cdf05fb'
         self.cat_id_sets[ex_idx] = {'cat-5', 'cat-6', 'cat-7', 'cat-8', 'cat-9', 'cat-10', 'cat-11'}
         self.nexus_id_sets[ex_idx] = {'nex-7', 'nex-8', 'nex-9', 'nex-10', 'nex-11', 'nex-12', 'tnx-1000000001'}
@@ -59,12 +26,6 @@ class TestGeoPackageHydrofabric(unittest.TestCase):
 
     def tearDown(self) -> None:
         pass
-
-    # def test_subset_hydrofabric(self):
-    #     subset_def = SubsetDefinition(catchment_ids=self.cat_id_sets[1], nexus_ids=self.nexus_id_sets[1])
-    #     hydrofabric = self.hydrofabric_ex[1]
-    #     subset_hf = hydrofabric.get_subset_hydrofabric(subset_def)
-    #     subset_hf.write_file(self.find_project_root().joinpath(self._HYDROFABRIC_1_RELATIVE_PATH), overwrite_existing=True)
 
     def test_get_all_catchment_ids_1_a(self):
         """
@@ -178,6 +139,20 @@ class TestGeoPackageHydrofabric(unittest.TestCase):
 
     def test_subset_hydrofabric_1_a(self):
         """
+        Test that function returns the expected/same object type.
+        """
+        ex_index = 1
+
+        hydrofabric = self.hydrofabric_ex[ex_index]
+        subset_cat_ids = self.cat_id_sets[ex_index].intersection(self.subset_id_sets[ex_index])
+        subset_nex_ids = self.nexus_id_sets[ex_index].intersection(self.subset_id_sets[ex_index])
+        subset_def = SubsetDefinition(catchment_ids=subset_cat_ids, nexus_ids=subset_nex_ids)
+        subset_hydrofabric = hydrofabric.get_subset_hydrofabric(subset_def)
+
+        self.assertIsInstance(subset_hydrofabric, GeoPackageHydrofabric)
+
+    def test_subset_hydrofabric_1_b(self):
+        """
         Test that all expected catchments are in a subset hydrofabric.
         """
         ex_index = 1
@@ -192,7 +167,7 @@ class TestGeoPackageHydrofabric(unittest.TestCase):
 
         self.assertEqual(subset_cat_ids, set(actual_cat_ids_in_subset))
 
-    def test_subset_hydrofabric_1_b(self):
+    def test_subset_hydrofabric_1_c(self):
         """
         Test that all catchments in generated subset hydrofabric have catchment objects.
         """
@@ -207,7 +182,7 @@ class TestGeoPackageHydrofabric(unittest.TestCase):
         self.assertTrue(all([isinstance(subset_hydrofabric.get_catchment_by_id(cid), GeoPackageCatchment) for cid in
                              subset_hydrofabric.get_all_catchment_ids()]))
 
-    def test_subset_hydrofabric_1_c(self):
+    def test_subset_hydrofabric_1_d(self):
         """
         Test that all expected nexuses are in a subset hydrofabric.
         """
@@ -223,7 +198,7 @@ class TestGeoPackageHydrofabric(unittest.TestCase):
 
         self.assertEqual(subset_nex_ids, set(actual_nex_ids_in_subset))
 
-    def test_subset_hydrofabric_1_d(self):
+    def test_subset_hydrofabric_1_e(self):
         """
         Test that all nexuses in generated subset hydrofabric have nexus objects.
         """
@@ -238,8 +213,6 @@ class TestGeoPackageHydrofabric(unittest.TestCase):
         self.assertTrue(all([isinstance(subset_hydrofabric.get_nexus_by_id(nid), GeoPackageNexus) for nid in
                              subset_hydrofabric.get_all_nexus_ids()]))
 
-    # TODO: think about how we'd want to test get_subset_hydrofabric() and write_file() (probably the same way)
-    
     def test_is_catchment_recognized_1_a(self):
         """
         Test if known catchment id ``cat-10`` is recognized.
