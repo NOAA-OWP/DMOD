7b205c6c3719955372b8feb9063454574ba584c8
Robert Bartel <robert.bartel@noaa.gov>
2023-03-20 13:15:24 -0500

    Require specific shapely version for hashing.
    Requiring specific shapely version in project requirements.txt and
    dmod.modeldata package, as this version makes certain datatypes used by
    Geopandas dataframes (and thus Geopackage hydrofabrics) hashable, which
    is needed to support hydrofabric uids.

--- python/lib/modeldata/setup.py
+++ python/lib/modeldata/setup.py
@@ -21,7 +21,7 @@ setup(
     url='',
     license='',
     install_requires=['numpy>=1.20.1', 'pandas', 'fiona', 'geopandas', 'dmod-communication>=0.4.2', 'dmod-core>=0.3.0',
-                      'minio', 'aiohttp<=3.7.4',
+                      'minio', 'aiohttp<=3.7.4', 'shapely>=2.0.0',
                       'hypy@git+https://github.com/NOAA-OWP/hypy@master#egg=hypy&subdirectory=python'],
     packages=find_namespace_packages(exclude=['dmod.test', 'schemas', 'ssl', 'src'])
 )
