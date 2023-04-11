324e64a0f6799d0409b6689a54f9a7a29a867e8e
Robert Bartel <robert.bartel@noaa.gov>
2023-04-11 09:31:05 -0400

    Fix comms ngen-config dep while not in PyPI.
    Fixing communication package dependency on ngen-config, as it is not
    actually available through PyPI and must be retrieved from Github (as is
    done with the project requirements.txt file).

--- python/lib/communication/setup.py
+++ python/lib/communication/setup.py
@@ -21,6 +21,7 @@ setup(
     url='',
     license='',
     include_package_data=True,
-    install_requires=['dmod-core>=0.4.2', 'websockets>=8.1', 'jsonschema', 'redis', 'pydantic', 'ngen-config>=0.1.1'],
+    install_requires=['dmod-core>=0.4.2', 'websockets>=8.1', 'jsonschema', 'redis', 'pydantic',
+                      'ngen-config@git+https://github.com/NOAA-OWP/ngen-cal@master#egg=ngen-config&subdirectory=python/ngen_conf'],
     packages=find_namespace_packages(include=['dmod.*'], exclude=['dmod.test'])
 )
