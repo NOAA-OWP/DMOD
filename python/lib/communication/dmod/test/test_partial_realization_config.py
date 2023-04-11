716040cbeec0c1d89f6ce268f2f7d224423b1202
Robert Bartel <robert.bartel@noaa.gov>
2023-04-11 09:26:23 -0400

    More elegant refactor of test setup helper func.

--- python/lib/communication/dmod/test/test_partial_realization_config.py
+++ python/lib/communication/dmod/test/test_partial_realization_config.py
@@ -50,7 +50,6 @@ class TestPartialRealizationConfig(unittest.TestCase):
         List[NgenRealization]
             A list of example realization config objects read from files in the local repo tree.
         """
-        realization_config = list()
         config_files_dir = cls.find_project_root() / "data" / "example_realization_configs"
         if not config_files_dir.is_dir():
             msg = "Expected dir with example realization configs for {} setup not found at {}"
@@ -58,10 +57,8 @@ class TestPartialRealizationConfig(unittest.TestCase):
 
         # See docstring for the basic makeup of each of the contained configs
         files = ["ex_realization_config_01.json", "ex_realization_config_02.json"]
-        for p in [config_files_dir.joinpath(f) for f in files]:
-            with open(p) as config_data:
-                realization_config.append(NgenRealization(**json.load(config_data)))
-        return realization_config
+
+        return [NgenRealization.parse_file(config_files_dir.joinpath(f)) for f in files]
 
     def setUp(self) -> None:
         # Base examples on data sourced from some actual realization config files; start by reading those
