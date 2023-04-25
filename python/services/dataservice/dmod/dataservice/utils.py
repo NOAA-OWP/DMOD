81854952da52a2d7e6ea26700c2b8bbc04f33d85
Austin Raney <austin.raney@noaa.gov>
2023-04-25 10:20:24 -0400

    use usedforsecurity flag if python >= 3.9

--- python/services/dataservice/dmod/dataservice/utils.py
+++ python/services/dataservice/dmod/dataservice/utils.py
@@ -1,3 +1,4 @@
+import sys
 import hashlib
 
 from .reader import Reader
@@ -7,7 +8,10 @@ FOUR_K = 4096
 
 
 def buffered_md5(r: Reader, block_size: int = FOUR_K) -> str:
-    h = hashlib.md5(usedforsecurity=False)
+    if sys.version_info >= (3, 9):
+        h = hashlib.md5(usedforsecurity=False)
+    else:
+        h = hashlib.md5()
     chunk = r.read(block_size)
     while chunk:
         h.update(chunk)
