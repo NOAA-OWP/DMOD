f7518123164f835109961c6df7a37eedb62d6eb2
Austin Raney <austin.raney@noaa.gov>
2023-05-02 12:55:40 -0400

    add DatasetClient docstrings

--- python/services/dataservice/dmod/dataservice/dataset_client.py
+++ python/services/dataservice/dmod/dataservice/dataset_client.py
@@ -87,6 +87,61 @@ class DatasetClient:
         objects: Optional[Iterable[ObjectToAdd]] = None,
         read_only: bool = False,
     ) -> Result[Dataset, Union[Exception, ExceptionGroup[Exception]]]:
+        """
+        Create a new dataset. Fails if dataset already exists.
+
+        Dataset names are restricted to the following rules:
+
+            - Must be between 3 (min) and 63 (max) characters long.
+            - Can consist only of lowercase letters, numbers, dots (.), and hyphens (-).
+            - Must begin and end with a letter or number.
+            - Must not contain two adjacent periods.
+            - Must not be formatted as an IP address (for example, 192.168.5.4).
+            - Must not start with the prefix xn--.
+            - Must not end with the suffix -s3alias. This suffix is reserved for access point alias
+                names. For more information, see Using a bucket-style alias for your S3 bucket
+                access point.
+            - Must not end with the suffix --ol-s3. This suffix is reserved for Object Lambda Access
+                Point alias names. For more information, see How to use a bucket-style alias for
+                your S3 bucket Object Lambda Access Point.
+
+            see s3 bucket naming rules for more information: https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html
+
+        Example:
+        ```python
+        from dmod.core.meta_data import (
+            DataCategory,
+            DataDomain,
+            DataFormat,
+            DiscreteRestriction,
+            StandardDatasetIndex,
+        )
+        from dmod.dataservice.dataset_client import DatasetClient
+        from minio import Minio
+
+        minio_client = Minio("127.0.0.1:9000", access_key="minioadmin", secret_key="minioadmin", secure=False)
+        client = DatasetClient(minio_client)
+
+        result = client.create(
+            "example-dataset",
+            category=DataCategory.CONFIG,
+            domain=DataDomain(
+                data_format=DataFormat.BMI_CONFIG,
+                discrete=[
+                    DiscreteRestriction(
+                        variable=StandardDatasetIndex.GLOBAL_CHECKSUM, values=["42"]
+                    )
+                ],
+            ),
+        )
+        if result.is_err():
+            # handle error
+            raise result.value
+
+        dataset = result.value
+        ```
+        """
+
         # NOTE: if minio-py changes their internal api this could fail in the future.
         # if that happens, we want this to raise.
         access_location = self._access_location(name)
@@ -148,6 +203,36 @@ class DatasetClient:
         content_type: str = "application/octet-stream",
         extract: bool = False,
     ) -> None:
+        """
+        Add object to existing dataset. Existing objects will be overwritten.
+
+        The object to upload is provided to the `reader` parameter and must provide a `read()`
+        method. Specifically: `read(self, size: Union[int, None] = ...) -> bytes`.
+
+        tar and tar.gz archives will be extracted by the data store post upload if `extract` flag is
+        set and content-type is `application/x-gzip` or `application/x-tar`. This is useful when
+        uploading large files.
+
+        Example:
+        ```python
+        import io
+        from dmod.dataservice.dataset_client import DatasetClient
+        from minio import Minio
+
+        minio_client = Minio("127.0.0.1:9000", access_key="minioadmin", secret_key="minioadmin", secure=False)
+        client = DatasetClient(minio_client)
+
+        result = client.add_object(
+            name="example-dataset",
+            object_name="test_data.txt",
+            reader=io.BytesIO(b"just some test data"),
+            content_type="text/plain",
+            size=len(b"just some test data"),
+        )
+        if result.is_err():
+            raise result.value
+        ```
+        """
         # source: https://docs.aws.amazon.com/snowball/latest/developer-guide/batching-small-files.html
         # > The auto-extract feature supports the TAR, and tar.gz formats.
         content_type = (
@@ -193,6 +278,39 @@ class DatasetClient:
     def add_objects(
         self, name: str, *objects: ObjectToAdd
     ) -> Result[None, ExceptionGroup[Exception]]:
+        """
+        Add multiple objects to an existing dataset. All objects will attempt to be uploaded, meaning
+        it is possible for the first object to fail and the last to succeed.
+
+        Example:
+        ```python
+        import io
+        from dmod.dataservice.dataset_client import DatasetClient
+        from minio import Minio
+
+        minio_client = Minio("127.0.0.1:9000", access_key="minioadmin", secret_key="minioadmin", secure=False)
+        client = DatasetClient(minio_client)
+
+        objects = [
+            ObjectToAdd(
+                "test_data.txt",
+                content_type="text/plain",
+                reader=io.BytesIO(b"just some test data"),
+                size=len(b"just some test data"),
+            ),
+            ObjectToAdd(
+                "more_test_data.txt",
+                content_type="text/plain",
+                reader=io.BytesIO(b"just some more test data"),
+                size=len(b"just some more test data"),
+            ),
+        ]
+        result = self.client.add_objects("example-dataset", *data)
+
+        if result.is_err():
+            raise result.value
+        ```
+        """
         errors: List[Exception] = []
         for obj in objects:
             result = self.add_object(
@@ -217,6 +335,7 @@ class DatasetClient:
     def delete(
         self, name: str
     ) -> Result[bool, Union[Exception, ExceptionGroup[RuntimeError]]]:
+        """Delete an existing dataset."""
         result = self._bucket_exists(name)
         if result.is_err():
             return result
@@ -241,6 +360,7 @@ class DatasetClient:
 
     @as_result
     def delete_object(self, name: str, object_name: str) -> None:
+        """Delete an object from an existing dataset."""
         result = self._bucket_exists(name)
         if result.is_err():
             return result
@@ -250,6 +370,7 @@ class DatasetClient:
     def delete_objects(
         self, name: str, *object_names: str
     ) -> Result[None, Union[Exception, ExceptionGroup[RuntimeError]]]:
+        """Delete one or more object from an existing dataset."""
         if not object_names:
             return Ok(None)
 
