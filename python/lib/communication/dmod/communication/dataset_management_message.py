21e4a9d3f64aee5a87c81ec2e282020106268a30
Robert Bartel <robert.bartel@noaa.gov>
2023-05-03 11:08:48 -0400

    Fix bad SerializedDict use in dataset mgr msgs.
    Correcting name of parent module and class in import, and fixing usage
    to use correct class name (was incorrectly SerializableDict).

--- python/lib/communication/dmod/communication/dataset_management_message.py
+++ python/lib/communication/dmod/communication/dataset_management_message.py
@@ -1,6 +1,5 @@
 from .message import AbstractInitRequest, MessageEventType, Response
-from dmod.core.serializable import Serializable
-from dmod.core.serializable_dict import SerializableDict
+from dmod.core.serializable import Serializable, SerializedDict
 from .maas_request import ExternalRequest, ExternalRequestResponse
 from dmod.core.meta_data import DataCategory, DataDomain, DataFormat, DataRequirement
 from dmod.core.enum import PydanticEnum
@@ -267,7 +266,7 @@ class DatasetManagementMessage(AbstractInitRequest):
         )
 
 
-class DatasetManagementResponseBody(SerializableDict):
+class DatasetManagementResponseBody(SerializedDict):
     action: Optional[ManagementAction]
     data_id: Optional[str]
     dataset_name: Optional[str]
