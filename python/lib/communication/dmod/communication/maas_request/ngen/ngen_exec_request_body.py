2749b934d584184f5cbaa11f7c61871539c9160e
Robert Bartel <robert.bartel@noaa.gov>
2023-05-16 13:19:26 -0400

    Update NGENRequestBody for Pydantic changes.

--- python/lib/communication/dmod/communication/maas_request/ngen/ngen_exec_request_body.py
+++ python/lib/communication/dmod/communication/maas_request/ngen/ngen_exec_request_body.py
@@ -1,17 +1,18 @@
-from pydantic import validator
+from pydantic import Field, validator
 
 from dmod.core.meta_data import TimeRange
-from ..model_exec_request_body import ModelExecRequestBody
+from dmod.core.serializable import Serializable
 
-from typing import List, Literal, Optional
+from typing import List, Optional
 
 
-class NGENRequestBody(ModelExecRequestBody):
-    name: Literal["ngen"] = "ngen"
+class NGENRequestBody(Serializable):
 
     time_range: TimeRange
     hydrofabric_uid: str
     hydrofabric_data_id: str
+    realization_config_data_id: str = Field(description="Unique id of the realization config dataset for this request.")
+    forcings_data_id: Optional[str] = Field(None, description="Unique id of forcings dataset, if provided.")
     bmi_config_data_id: str
     # NOTE: consider pydantic.conlist to constrain this type rather than using validators
     catchments: Optional[List[str]]
@@ -31,3 +32,10 @@ class NGENRequestBody(ModelExecRequestBody):
         fields = {
             "partition_cfg_data_id": {"alias": "partition_config_data_id"},
         }
+
+    def dict(self, **kwargs) -> dict:
+        # if exclude is set, ignore this _get_exclude_fields()
+        only_if_set = ("catchments", "partition_cfg_data_id", "forcings_data_id")
+        if kwargs.get("exclude", False) is False:
+            kwargs["exclude"] = {f for f in only_if_set if not self.__getattribute__(f)}
+        return super().dict(**kwargs)
