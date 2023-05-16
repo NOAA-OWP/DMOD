ae3d969c879a5475744cb6d7cea3005e87dcfacf
Robert Bartel <robert.bartel@noaa.gov>
2023-05-16 13:26:31 -0400

    Update cal request for Pydantic, upstream changes.

--- python/lib/communication/dmod/communication/maas_request/ngen/ngen_calibration_request.py
+++ python/lib/communication/dmod/communication/maas_request/ngen/ngen_calibration_request.py
@@ -1,57 +1,28 @@
-from numbers import Number
-from dmod.core.meta_data import TimeRange
-from typing import Dict, List, Optional, Set, Tuple, Union
-
+from pydantic import PrivateAttr
+from dmod.core.meta_data import DataCategory, DataDomain, DataFormat, DataRequirement, DiscreteRestriction
+from typing import ClassVar, List, Literal
+from datetime import datetime
 from ...message import MessageEventType
 from ...maas_request import ModelExecRequestResponse
 from .ngen_request import ExternalAbstractNgenRequest
+from .ngen_cal_request_estimation_config import NgenCalRequestEstimationConfig
 
 
 class NgenCalibrationRequest(ExternalAbstractNgenRequest):
     """
     An extension of ::class:`ExternalAbstractNgenRequest` for requesting ngen framework calibration jobs using ngen-cal.
     """
+    request_body: NgenCalRequestEstimationConfig
 
-    event_type: MessageEventType = MessageEventType.CALIBRATION_REQUEST
-    job_exec_name = 'ngen-cal' #FIXME case sentitivity
+    event_type: ClassVar[MessageEventType] = MessageEventType.CALIBRATION_REQUEST
 
-    _KEY_CAL_STRATEGY_ALGO = 'strategy_algorithm'
-    _KEY_CAL_STRATEGY_OBJ_FUNC = 'strategy_objective_function'
-    _KEY_CAL_STRATEGY_TYPE = 'strategy_type'
-    _KEY_IS_OBJ_FUNC_MIN = 'is_obj_func_min'
-    _KEY_IS_RESTART = 'is_restart'
-    _KEY_ITERATIONS = 'iterations'
-    _KEY_JOB_NAME = 'job_name'
-    _KEY_MODEL_CAL_PARAMS = 'model_cal_params'
-    _KEY_MODEL_STRATEGY = 'model_strategy'
+    # See comments in DmodJobRequest super class for this field for more details on its purpose.
+    job_type: Literal["ngen-cal"] = "ngen-cal"
+    """ The name for the job type being requested. """
 
-    @classmethod
-    def deserialize_for_init(cls, json_obj: dict) -> dict:
-        """
-        Deserialize a JSON representation to the keyword args needed for use with this type's ::method:`__init__`.
+    job_name: str
 
-        Parameters
-        ----------
-        json_obj: dict
-            A serialized JSON representation of an instance.
-
-        Returns
-        -------
-        dict
-            A dictionary containing the keyword args (both required and any contained optional) necessary for
-            initializing an instance, with the values deserialized from the received JSON.
-        """
-        deserialized_init_params = super().deserialize_for_init(json_obj)
-        deserialized_init_params['cal_strategy_algorithm'] = json_obj[cls._KEY_CAL_STRATEGY_ALGO]
-        deserialized_init_params['cal_strategy_objective_func'] = json_obj[cls._KEY_CAL_STRATEGY_OBJ_FUNC]
-        deserialized_init_params['cal_strategy_type'] = json_obj[cls._KEY_CAL_STRATEGY_TYPE]
-        deserialized_init_params['is_objective_func_minimized'] = json_obj[cls._KEY_IS_OBJ_FUNC_MIN]
-        deserialized_init_params['is_restart'] = json_obj[cls._KEY_IS_RESTART]
-        deserialized_init_params['iterations'] = json_obj[cls._KEY_ITERATIONS]
-        deserialized_init_params['job_name'] = json_obj[cls._KEY_JOB_NAME]
-        deserialized_init_params['model_cal_params'] = json_obj[cls._KEY_MODEL_CAL_PARAMS]
-        deserialized_init_params['model_strategy'] = json_obj[cls._KEY_MODEL_STRATEGY]
-        return deserialized_init_params
+    _calibration_config_data_requirement = PrivateAttr(None)
 
     @classmethod
     def factory_init_correct_response_subtype(cls, json_obj: dict) -> 'NgenCalibrationResponse':
@@ -69,117 +40,70 @@ class NgenCalibrationRequest(ExternalAbstractNgenRequest):
         """
         return NgenCalibrationResponse.factory_init_from_deserialized_json(json_obj=json_obj)
 
-    def __init__(self,
-                 model_cal_params: Dict[str, Tuple[float, float, float]],
-                 iterations: int,
-                 cal_strategy_type: str = 'estimation',
-                 cal_strategy_algorithm: str = 'dds',
-                 cal_strategy_objective_func: str = 'nnse',
-                 is_objective_func_minimized: bool = True,
-                 model_strategy: str = 'uniform',
-                 job_name: Optional[str] = None,
-                 is_restart: bool = False,
-                 parallel: Optional[int] = None,
-                 *args,
-                 **kwargs):
+    @property
+    def calibration_config_data_requirement(self) -> DataRequirement:
         """
-        Initialize an instance.
+        A requirement object defining of the ngen-cal configuration needed to execute this request.
 
-        Parameters
-        ----------
-        model_cal_params : Dict[str, Tuple[float, float, float]]
-            A collection of the calibratable params, keyed by name, with a tuple of the min, max, and initial values.
-        iterations : int
-            The total number of search iterations to run.
-        cal_strategy_type : str
-            The ngen-cal general strategy type for the calibration config (default: ``estimation``).
-        cal_strategy_algorithm : str
-            Calibration strategy algorithm ("dds" by default).
-        cal_strategy_objective_func : str
-            The standard name ("kling_gupta", "nnse", "custom", "single_peak", "volume") or full ngen_cal package module
-            name for the objective function to use ("nnse" by default).
-        is_objective_func_minimized : bool
-            Whether to minimize the objective function (implies maximize when ``False``; default value: ``True``).
-        model_strategy : str
-            The ngen-cal model calibration strategy; one of :
-                'uniform' : Each catchment shares the same parameter space, evaluates at one observable nexus
-                'independent' : Each catchment upstream of observable nexus gets its own permutated parameter space,
-                                evaluates at one observable nexus
-                'explicit' : Only calibrates basins in the realization_config with a "calibration" definition and an
-                observable nexus
-        job_name : Optional[str]
-            Optional job name for the calibration run, which can be used by ngen-cal when generating files.
-        is_restart : bool
-            Whether this represents restarting a previous job; ``False`` by default.
-        parallel: Optional[int]
-            Optional setting for number of parallel ngen processes used by ngen-cal.
-
-        Keyword Args
-        -----------
-        time_range : TimeRange
-            A definition of the time range for the configured execution of the ngen framework.
-        hydrofabric_uid : str
-            The unique ID of the applicable hydrofabric for modeling, which provides the outermost geospatial domain.
-        hydrofabric_data_id : str
-            A data identifier for the hydrofabric, for distinguishing between different hydrofabrics that cover the same
-            set of catchments and nexuses (i.e., the same sets of catchment and nexus ids).
-        catchments : Optional[Union[Set[str], List[str]]]
-            An optional collection of the catchment ids to narrow the geospatial domain, where the default of ``None``
-            or an empty collection implies all catchments in the hydrofabric.
-        bmi_cfg_data_id : Optional[str]
-            The optional BMI init config ``data_id`` index, for identifying the particular BMI init config datasets
-            applicable to this request.
-        config_data_id : str
-            The config data id index, for identifying the particular configuration datasets applicable to this request.
-        session_secret : str
-            The session secret for the right session when communicating with the MaaS request handler
+        Returns
+        -------
+        DataRequirement
+            A requirement object defining of the ngen-cal configuration needed to execute this request.
         """
-        super(NgenCalibrationRequest, self).__init__(*args, **kwargs)
-        self.model_cal_params = model_cal_params
-        self.iterations = iterations
-        self.cal_strategy_type = cal_strategy_type
-        self.cal_strategy_algorithm = cal_strategy_algorithm
-        self.cal_strategy_objective_function = cal_strategy_objective_func
-        self.is_objective_func_minimized = is_objective_func_minimized
-        self.model_strategy = model_strategy
-        self.job_name = job_name
-        self.parallel = parallel
+        # TODO: #needs_issue - Should all these individual requirement properties (here and elsewhere) be done away with?
+        if self._calibration_config_data_requirement is None:
+            if self.request_body.ngen_cal_config_data_id is None:
+                self.request_body.ngen_cal_config_data_id = "{}-{}".format(self.job_type, self.job_name)
+            ngen_cal_restrict = [
+                DiscreteRestriction(
+                    variable="data_id", values=[self.request_body.ngen_cal_config_data_id]
+                )
+            ]
+            ngen_cal_config_domain = DataDomain(
+                data_format=DataFormat.NGEN_CAL_CONFIG,
+                discrete_restrictions=ngen_cal_restrict,
+            )
+            self._calibration_config_data_requirement = DataRequirement(
+                domain=ngen_cal_config_domain, is_input=True, category=DataCategory.CONFIG
+            )
+        return self._calibration_config_data_requirement
 
-        self.is_restart = is_restart
+    @property
+    def data_requirements(self) -> List[DataRequirement]:
+        """
+        List of all the explicit and implied data requirements for this request, as needed for creating a job object.
 
-        # TODO: may need to modify this to have (realization) config dataset start empty (at least optionally) and apply
+        Returns
+        -------
+        List[DataRequirement]
+            List of all the explicit and implied data requirements for this request.
+        """
+        data_requirements = super().data_requirements
+        return [self.calibration_config_data_requirement, *data_requirements]
 
-    def to_dict(self) -> Dict[str, Union[str, Number, dict, list]]:
-        serial = super().to_dict()
-        serial["name"] = self.job_exec_name
-        serial[self._KEY_MODEL_CAL_PARAMS] = self.model_cal_params
-        serial[self._KEY_CAL_STRATEGY_TYPE] = self.cal_strategy_type
-        serial[self._KEY_CAL_STRATEGY_ALGO] = self.cal_strategy_algorithm
-        serial[self._KEY_CAL_STRATEGY_OBJ_FUNC] = self.cal_strategy_objective_function
-        serial[self._KEY_IS_OBJ_FUNC_MIN] = self.is_objective_func_minimized
-        serial[self._KEY_ITERATIONS] = self.iterations
-        serial[self._KEY_JOB_NAME] = self.job_name
-        serial[self._KEY_MODEL_STRATEGY] = self.model_strategy
-        serial[self._KEY_IS_RESTART] = self.is_restart
-        return serial
+    @property
+    def evaluation_start(self) -> datetime:
+        return self.request_body.time_range.begin
+
+    @property
+    def evaluation_stop(self) -> datetime:
+        return self.request_body.time_range.end
+
+    @property
+    def is_restart(self) -> bool:
+        """
+        Whether this is a request to restart a previously running job.
+
+        Returns
+        -------
+        bool
+            Whether this is a request to restart a previously running job.
+        """
+        return self.request_body.start_iteration != 0
 
     @property
     def use_serial_ngen(self) -> bool:
-        return self.parallel is None or self.parallel < 2
-
-    # TODO: This should likely be created or determined if it already exsits on the fly
-    # @property
-    # def data_requirements(self) -> List[DataRequirement]:
-    #     """
-    #     List of all the explicit and implied data requirements for this request, as needed fo    r creating a job object.
-
-    #     Returns
-    #     -------
-    #     List[DataRequirement]
-    #         List of all the explicit and implied data requirements for this request.
-    #     """
-    #     data_requirements = super().data_requirements
-    #     return [self.calibration_cfg_data_requirement ,*data_requirements]
+        return self.request_body.parallel_proc < 2
 
 
 # TODO: aaraney. this looks unfinished
