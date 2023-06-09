7d1fd5f0bfb0452302c95edc416e54ea281c9036
Austin Raney <austin.raney@noaa.gov>
2023-06-09 11:37:39 -0400

    flatten Distribution class

--- python/lib/communication/dmod/communication/maas_request/distribution.py
+++ python/lib/communication/dmod/communication/maas_request/distribution.py
@@ -3,66 +3,26 @@ from dmod.core.serializable import Serializable
 from typing import Literal
 
 
-class DistributionBounds(Serializable):
-    minimum: int = 0
-    maximum: int = 0
-    distribution_type: Literal["normal"] = "normal"
-
-    class Config:
-        feilds = {
-            "distribution_type": {"alias": "type"},
-            "minimum": {"alias": "min"},
-            "maximum": {"alias": "max"},
-        }
-
-
 class Distribution(Serializable):
     """
     Represents the definition of a distribution of numbers
     """
 
-    distribution: DistributionBounds
+    minimum: int = 0
+    """The lower bound for the distribution"""
 
-    def __init__(
-        self, minimum: int = 0, maximum: int = 0, distribution_type: str = "normal"
-    ):
-        """
-        :param int minimum: The lower bound for the distribution
-        :param int maximum: The upper bound of the distribution
-        :param str distribution_type: The type of the distribution
-        """
-        super().__init__(
-            distribution=DistributionBounds(
-                minimum=minimum, maximum=maximum, distribution_type=distribution_type
-            )
-        )
+    maximum: int = 0
+    """The upper bound for the distribution"""
 
-    @property
-    def minimum(self) -> int:
-        """The lower bound for the distribution"""
-        return self.distribution.minimum
+    distribution_type: Literal["normal"] = "normal"
+    """The type of the distribution"""
 
-    @minimum.setter
-    def minimum(self, value: int):
-        self.distribution.minimum = value
-
-    @property
-    def maximum(self) -> int:
-        """The upper bound for the distribution"""
-        return self.distribution.maximum
-
-    @maximum.setter
-    def maximum(self, value: int):
-        self.distribution.maximum = value
-
-    @property
-    def distribution_type(self) -> str:
-        """The type of the distribution"""
-        return self.distribution.distribution_type
-
-    @distribution_type.setter
-    def distribution_type(self, value: str) -> str:
-        self.distribution.distribution_type = value
+    class Config(Serializable.Config):
+        feilds = {
+            "distribution_type": {"alias": "type"},
+            "minimum": {"alias": "min"},
+            "maximum": {"alias": "max"},
+        }
 
     def __str__(self):
         return str(self.to_dict())
