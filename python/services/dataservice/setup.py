cd09ed5965c0f451e92580ec2bdc01ac99e9a651
Austin Raney <austin.raney@noaa.gov>
2023-06-30 12:50:00 -0400

    pin pydantic<2

--- python/services/dataservice/setup.py
+++ python/services/dataservice/setup.py
@@ -18,7 +18,7 @@ setup(
     url='',
     license='',
     install_requires=['dmod-core>=0.8.0', 'dmod-communication>=0.13.0', 'dmod-scheduler>=0.10.0',
-                      'dmod-modeldata>=0.9.0', 'redis', "pydantic", "fastapi", "uvicorn[standard]",
+                      'dmod-modeldata>=0.9.0', 'redis', "pydantic>=1.10.8,~=1.10", "fastapi", "uvicorn[standard]",
                       "ngen-config>=0.1.1"],
     packages=find_namespace_packages(exclude=['dmod.test', 'deprecated', 'conf', 'schemas', 'ssl', 'src'])
 )
