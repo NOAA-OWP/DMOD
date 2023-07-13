6943014d2bf8ee97d731ad4da25d75a374fab56f
Robert Bartel <robert.bartel@noaa.gov>
2023-07-13 15:08:47 -0400

    Adjust dep defs using NOAA-OWP in git url.
    Updating dependency definitions to OWP packages not published to pypi,
    which therefore use git urls, to change usage of NOAA-OWP to noaa-owp in
    order to be consistent with usage of such urls within dependencies.

--- python/lib/communication/setup.py
+++ python/lib/communication/setup.py
@@ -22,6 +22,6 @@ setup(
     license='',
     include_package_data=True,
     install_requires=['dmod-core>=0.4.2', 'websockets>=8.1', 'jsonschema', 'redis', 'pydantic>=1.10.8,~=1.10',
-                      'ngen-config@git+https://github.com/NOAA-OWP/ngen-cal@master#egg=ngen-config&subdirectory=python/ngen_conf'],
+                      'ngen-config@git+https://github.com/noaa-owp/ngen-cal@master#egg=ngen-config&subdirectory=python/ngen_conf'],
     packages=find_namespace_packages(include=['dmod.*'], exclude=['dmod.test'])
 )
