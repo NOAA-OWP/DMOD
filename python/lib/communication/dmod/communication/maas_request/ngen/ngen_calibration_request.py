4992b6d6650b6395fd6305a384e0e3bd8dd6a733
Robert Bartel <robert.bartel@noaa.gov>
2023-07-17 14:31:56 -0400

    Update ngen-cal request for composite cfg support.

--- python/lib/communication/dmod/communication/maas_request/ngen/ngen_calibration_request.py
+++ python/lib/communication/dmod/communication/maas_request/ngen/ngen_calibration_request.py
@@ -1,6 +1,4 @@
-from pydantic import PrivateAttr
-from dmod.core.meta_data import DataCategory, DataDomain, DataFormat, DataRequirement, DiscreteRestriction
-from typing import ClassVar, List, Literal
+from typing import ClassVar, Literal
 from datetime import datetime
 from ...message import MessageEventType
 from ...maas_request import ModelExecRequestResponse
@@ -20,10 +18,6 @@ class NgenCalibrationRequest(ExternalAbstractNgenRequest):
     job_type: Literal["ngen-cal"] = "ngen-cal"
     """ The name for the job type being requested. """
 
-    job_name: str
-
-    _calibration_config_data_requirement = PrivateAttr(None)
-
     @classmethod
     def factory_init_correct_response_subtype(cls, json_obj: dict) -> 'NgenCalibrationResponse':
         """
@@ -39,47 +33,6 @@ class NgenCalibrationRequest(ExternalAbstractNgenRequest):
             A response of the correct type, with state details from the provided JSON.
         """
         return NgenCalibrationResponse.factory_init_from_deserialized_json(json_obj=json_obj)
-
-    @property
-    def calibration_config_data_requirement(self) -> DataRequirement:
-        """
-        A requirement object defining of the ngen-cal configuration needed to execute this request.
-
-        Returns
-        -------
-        DataRequirement
-            A requirement object defining of the ngen-cal configuration needed to execute this request.
-        """
-        # TODO: #needs_issue - Should all these individual requirement properties (here and elsewhere) be done away with?
-        if self._calibration_config_data_requirement is None:
-            if self.request_body.ngen_cal_config_data_id is None:
-                self.request_body.ngen_cal_config_data_id = "{}-{}".format(self.job_type, self.job_name)
-            ngen_cal_restrict = [
-                DiscreteRestriction(
-                    variable="data_id", values=[self.request_body.ngen_cal_config_data_id]
-                )
-            ]
-            ngen_cal_config_domain = DataDomain(
-                data_format=DataFormat.NGEN_CAL_CONFIG,
-                discrete_restrictions=ngen_cal_restrict,
-            )
-            self._calibration_config_data_requirement = DataRequirement(
-                domain=ngen_cal_config_domain, is_input=True, category=DataCategory.CONFIG
-            )
-        return self._calibration_config_data_requirement
-
-    @property
-    def data_requirements(self) -> List[DataRequirement]:
-        """
-        List of all the explicit and implied data requirements for this request, as needed for creating a job object.
-
-        Returns
-        -------
-        List[DataRequirement]
-            List of all the explicit and implied data requirements for this request.
-        """
-        data_requirements = super().data_requirements
-        return [self.calibration_config_data_requirement, *data_requirements]
 
     @property
     def evaluation_start(self) -> datetime:
