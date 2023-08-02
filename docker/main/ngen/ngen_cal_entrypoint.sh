d4695f4c17e4acb3d73eebbe6117ff326bc43d67
Robert Bartel <robert.bartel@noaa.gov>
2023-08-02 10:16:58 -0400

    Refactor ngen_cal entrypoint for shared scripts.

--- docker/main/ngen/ngen_cal_entrypoint.sh
+++ docker/main/ngen/ngen_cal_entrypoint.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
 # Args are managed by the _generate_docker_cmd_args function in scheduler.py of dmod.scheduler
 
-# TODO: Docker secret variable values need to be parameterized
-ACCESS_KEY_SECRET="object_store_exec_user_name"
-SECRET_KEY_SECRET="object_store_exec_user_passwd"
-DOCKER_SECRETS_DIR="/run/secrets"
-ACCESS_KEY_FILE="${DOCKER_SECRETS_DIR}/${ACCESS_KEY_SECRET}"
-SECRET_KEY_FILE="${DOCKER_SECRETS_DIR}/${SECRET_KEY_SECRET}"
-
-MPI_USER="mpi"
-if [ "$(whoami)" = "${MPI_USER}" ]; then
-    MPI_HOSTS_FILE="${HOME}/.mpi_hosts"
-else
-    MPI_HOSTS_FILE="$(su ${MPI_USER} -c 'echo "${HOME}"')/.mpi_hosts"
-fi
-RUN_SENTINEL="/home/${MPI_USER}/.run_sentinel"
-
-MPI_RUN="mpirun"
-
-NGEN_SERIAL_EXECUTABLE="/ngen/ngen/cmake_build_serial/ngen"
-NGEN_PARALLEL_EXECUTABLE="/ngen/ngen/cmake_build_parallel/ngen"
-# This will be symlinked to the parallel one currently
-NGEN_EXECUTABLE="/ngen/ngen/cmake_build/ngen"
-
-ALL_DATASET_DIR="/dmod/datasets"
-
 while [ ${#} -gt 0 ]; do
     case "${1}" in
         --config-dataset)
@@ -63,73 +39,22 @@ while [ ${#} -gt 0 ]; do
     shift
 done
 
-# Run some sanity checks
-# TODO: not sure if this is appropriate or not for calibration exec
-if [ -z "${MPI_HOST_STRING:?No MPI hosts string given}" ]; then
-    echo "Error: MPI host string is empty" > 2>&1
-    exit 1
-fi
-# Using complement of valid range to catch non-integer values
-if ! [ "${WORKER_INDEX:?No MPI worker index/rank given}" -ge 0 ] 2>/dev/null; then
-    echo "Error: invalid value '${WORKER_INDEX}' given for MPI worker index/rank" > 2>&1
-    exit 1
-fi
-if ! [ "${MPI_NODE_COUNT:?No MPI node count provided}" -gt 0 ] 2>/dev/null; then
-    echo "Error: invalid value '${MPI_NODE_COUNT}' given for MPI node count" > 2>&1
-    exit 1
-fi
+# Get some universally applicable functions and constants
+source ./funcs.sh
 
-# These serve as both sanity checks and initialization of some derived values
-OUTPUT_DATASET_DIR="${ALL_DATASET_DIR:?}/output/${OUTPUT_DATASET_NAME:?No output dataset provided}"
-HYDROFABRIC_DATASET_DIR="${ALL_DATASET_DIR}/hydrofabric/${HYDROFABRIC_DATASET_NAME:?No hydrofabric dataset provided}"
-CONFIG_DATASET_DIR="${ALL_DATASET_DIR}/config/${CONFIG_DATASET_NAME:?No config dataset provided}"
+ngen_sanity_checks_and_derived_init
+init_script_mpi_vars
+init_ngen_executable_paths
 
-# Require a partitioning config unless using just a single node for serial exec
-# TODO: account for CPU count more appropriately when that gets merged in for ngen image
-if [ ${MPI_NODE_COUNT:?No MPI node count provided} -gt 1 ]; then
-    PARTITION_DATASET_DIR="${ALL_DATASET_DIR:?}/config/${PARTITION_DATASET_NAME:?No partition config dataset name given}"
-fi
-
-print_date() {
-    date "+%Y-%m-%d,%H:%M:%S"
-}
-
-check_for_dataset_dir() {
-    # Dataset dir is $1
-    _CATEG="$(echo "${1}" | sed "s|${ALL_DATASET_DIR}/\([^/]*\)/.*|\1|" | awk '{print toupper($0)}')"
-    if [ ! -d "${1}" ]; then
-        echo "Error: expected ${_CATEG} dataset directory ${1} not found." 2>&1
-        exit 1
-    fi
-}
-
-load_object_store_keys_from_docker_secrets() {
-    # Read Docker Secrets files for Object Store access, if they exist
-    if [ -z "${ACCESS_KEY_FILE:-}" ]; then
-        echo "WARN: Cannot load object store access key when Docker secret file name not set"
-    elif [ -e "${ACCESS_KEY_FILE}" ]; then
-        ACCESS_KEY="$(cat "${ACCESS_KEY_FILE}")"
-    else
-        echo "WARN: Cannot load object store access key when Docker secret file does not exist"
-    fi
-
-    if [ -z "${SECRET_KEY_FILE:-}" ]; then
-        echo "WARN: Cannot load object store secret key when Docker secret file name not set"
-    elif [ -e "${SECRET_KEY_FILE}" ]; then
-        SECRET_KEY="$(cat "${SECRET_KEY_FILE}")"
-    else
-        echo "WARN: Cannot load object store secret key when Docker secret file does not exist"
-    fi
-
-    test -n "${ACCESS_KEY:-}" && test -n "${SECRET_KEY:-}"
-}
+# Move to the output dataset mounted directory
+cd ${OUTPUT_DATASET_DIR:?Output dataset directory not defined}
 
 start_calibration() {
     # Start ngen calibration
     echo "$(print_date) Starting serial ngen calibration"
 
     # Find and use copy of config in output dataset
-    CALIBRATION_CONFIG_FILE=$(find ${OUTPUT_DATASET_DIR} -type f -iname "*.yaml" -o -iname "*.yml" -maxdepth 1 | head -1)
+    CALIBRATION_CONFIG_FILE=$(find ${OUTPUT_DATASET_DIR:?} -type f -iname "*.yaml" -o -iname "*.yml" -maxdepth 1 | head -1)
 
     if [ -z "${CALIBRATION_CONFIG_FILE}" ]; then
         echo "Error: NGEN calibration yaml file not found" 2>&1
@@ -146,26 +71,16 @@ start_calibration() {
     return ${NGEN_RETURN}
 }
 
-# Sanity check that the output, hydrofabric, and config datasets are available (i.e., their directories are in place)
-check_for_dataset_dir "${OUTPUT_DATASET_DIR}"
-check_for_dataset_dir "${CONFIG_DATASET_DIR}"
-if [ -n "${PARTITION_DATASET_DIR:-}" ]; then
-    check_for_dataset_dir "${PARTITION_DATASET_DIR:?No partition dataset directory defined}"
-fi
-check_for_dataset_dir "${HYDROFABRIC_DATASET_DIR}"
-
 # Copy config files to output dataset for record keeping, but only from the "main" worker node
-if [ ${WORKER_INDEX} -eq 0 ]; then
+# We can allow worker index to not be supplied when executing serially, so apply default substitution
+if [ ${WORKER_INDEX:-0} -eq 0 ]; then
     # TODO: perform copy of configs to output dataset outside of image (in service) for better performance
-    cp -a ${CONFIG_DATASET_DIR}/. ${OUTPUT_DATASET_DIR}
+    cp -a ${CONFIG_DATASET_DIR:?Config dataset directory not defined}/. ${OUTPUT_DATASET_DIR:?}
     if [ -n "${PARTITION_DATASET_DIR:-}" ]; then
         # Also, when partition config present, copy that for record keeping
         # TODO: perform copy of configs to output dataset outside of image (in service) for better performance
-        cp -a ${PARTITION_DATASET_DIR}/. ${OUTPUT_DATASET_DIR}
+        cp -a ${PARTITION_DATASET_DIR}/. ${OUTPUT_DATASET_DIR:?}
     fi
 fi
-
-# Move to the output dataset mounted directory
-cd ${OUTPUT_DATASET_DIR}
 
 start_calibration
