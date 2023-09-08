2eb58dd77d4d78a26bc9df9a17ff2c70ab951d1c
Robert Bartel <robert.bartel@noaa.gov>
2023-09-08 15:19:34 -0400

    Fix unit tests for [Dataset/DataService]Client.

--- python/lib/client/dmod/test/test_dataset_client.py
+++ python/lib/client/dmod/test/test_dataset_client.py
@@ -1,46 +1,48 @@
 import unittest
-from ..client.request_clients import DataCategory, DatasetClient, DatasetManagementResponse, MaaSDatasetManagementResponse
+from ..client.request_clients import (DataCategory, DataDomain, DataServiceClient, DatasetManagementResponse, 
+                                      MaaSDatasetManagementResponse, ManagementAction, ResultIndicator)
 from pathlib import Path
-from typing import List, Optional
+from typing import List, Optional, Sequence, Union
 
 
-class SimpleMockDatasetClient(DatasetClient):
+class SimpleMockDataServiceClient(DataServiceClient):
     """
-    Mock subtype, primarily for testing base implementation of ::method:`_parse_list_of_dataset_names_from_response`.
+    Mock subtype, primarily for testing base implementation of ::method:`extract_dataset_names`.
     """
 
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+        #super().__init__(*args, **kwargs)
+        pass
 
-    async def create_dataset(self, name: str, category: DataCategory) -> bool:
+    async def create_dataset(self, name: str, category: DataCategory, domain: DataDomain,
+                             upload_paths: Optional[List[Path]] = None, data_root: Optional[Path] = None,
+                             **kwargs) -> DatasetManagementResponse:
+        """ Mock implementation, always returning an unsuccessful result. """
+        return DatasetManagementResponse(success=False, action=ManagementAction.CREATE, reason="Mock")
+
+    async def delete_dataset(self, name: str, **kwargs) -> DatasetManagementResponse:
         """ Mock implementation, always returning ``False``. """
-        return False
+        return DatasetManagementResponse(success=False, action=ManagementAction.DELETE, reason="Mock")
 
-    async def delete_dataset(self, name: str, **kwargs) -> bool:
+    async def retrieve_from_dataset(self, dataset_name: str, dest_dir: Path,
+                                    item_names: Optional[Union[str, Sequence[str]]] = None, **kwargs) -> ResultIndicator:
         """ Mock implementation, always returning ``False``. """
-        return False
+        return DatasetManagementResponse(success=False, action=ManagementAction.REQUEST_DATA, reason="Mock")
 
-    async def download_dataset(self, dataset_name: str, dest_dir: Path) -> bool:
-        """ Mock implementation, always returning ``False``. """
-        return False
-
-    async def download_from_dataset(self, dataset_name: str, item_name: str, dest: Path) -> bool:
-        """ Mock implementation, always returning ``False``. """
-        return False
-
-    async def list_datasets(self, category: Optional[DataCategory] = None) -> List[str]:
+    async def list_datasets(self, category: Optional[DataCategory] = None, **kwargs) -> List[str]:
         """ Mock implementation, always returning an empty list. """
         return []
 
-    async def upload_to_dataset(self, dataset_name: str, paths: List[Path]) -> bool:
+    async def upload_to_dataset(self, dataset_name: str, paths: Union[Path, List[Path]],
+                                data_root: Optional[Path] = None, **kwargs) -> ResultIndicator:
         """ Mock implementation, always returning ``False``. """
-        return False
+        return DatasetManagementResponse(success=False, action=ManagementAction.ADD_DATA, reason="Mock")
 
 
 class TestDatasetClient(unittest.TestCase):
 
     def setUp(self) -> None:
-        self.client = SimpleMockDatasetClient()
+        self.client = SimpleMockDataServiceClient()
 
         self.example_responses = []
         self.example_dataset_names_lists = []
@@ -69,42 +71,42 @@ class TestDatasetClient(unittest.TestCase):
             {'success': True, 'reason': 'List Assembled', 'message': '',
              'data': {'datasets': list(dataset_names_list), 'action': 'LIST_ALL', 'is_awaiting': False}}))
 
-    def test__parse_list_of_dataset_names_from_response_0_a(self):
+    def test_extract_dataset_names_0_a(self):
         """ Test example 0 with base ::class:`DatasetManagementResponse` and empty list is parsed correctly """
         ex_indx = 0
 
         expected_names = self.example_dataset_names_lists[ex_indx]
         response = self.example_responses[ex_indx]
 
-        dataset_names = self.client._parse_list_of_dataset_names_from_response(response)
+        dataset_names = self.client.extract_dataset_names(response)
         self.assertEqual(expected_names, dataset_names)
 
-    def test__parse_list_of_dataset_names_from_response_1_a(self):
+    def test_extract_dataset_names_1_a(self):
         """ Test example 1 with base ::class:`DatasetManagementResponse` and non-empty list is parsed correctly """
         ex_indx = 1
 
         expected_names = self.example_dataset_names_lists[ex_indx]
         response = self.example_responses[ex_indx]
 
-        dataset_names = self.client._parse_list_of_dataset_names_from_response(response)
+        dataset_names = self.client.extract_dataset_names(response)
         self.assertEqual(expected_names, dataset_names)
 
-    def test__parse_list_of_dataset_names_from_response_2_a(self):
+    def test_extract_dataset_names_2_a(self):
         """ Test example 2 with subtype ::class:`MaaSDatasetManagementResponse` and empty list is parsed correctly """
         ex_indx = 2
 
         expected_names = self.example_dataset_names_lists[ex_indx]
         response = self.example_responses[ex_indx]
 
-        dataset_names = self.client._parse_list_of_dataset_names_from_response(response)
+        dataset_names = self.client.extract_dataset_names(response)
         self.assertEqual(expected_names, dataset_names)
 
-    def test__parse_list_of_dataset_names_from_response_3_a(self):
+    def test_extract_dataset_names_3_a(self):
         """ Test example 3 w/ subtype ::class:`MaaSDatasetManagementResponse` and non-empty list is parsed correctly """
         ex_indx = 3
 
         expected_names = self.example_dataset_names_lists[ex_indx]
         response = self.example_responses[ex_indx]
 
-        dataset_names = self.client._parse_list_of_dataset_names_from_response(response)
+        dataset_names = self.client.extract_dataset_names(response)
         self.assertEqual(expected_names, dataset_names)
