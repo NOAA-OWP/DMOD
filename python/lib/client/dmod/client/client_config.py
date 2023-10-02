5fa35aec145b30624a4f3c29e8b4e1feed80d8d9
Robert Bartel <robert.bartel@noaa.gov>
2023-10-02 16:23:22 -0400

    Optional remote debug info in client config.

--- python/lib/client/dmod/client/client_config.py
+++ python/lib/client/dmod/client/client_config.py
@@ -16,8 +16,27 @@ class ConnectionConfig(Serializable):
     use_default_context: bool = False
 
 
-class ClientConfig(Serializable):
+class PycharmRemoteDebugConfig(Serializable):
+    egg_path: Path = Field(description="Path to egg for remote Pycharm debugging", alias="egg-path")
+    debug_host: str = Field("host.docker.internal", description="Debug host to connect back to for remote debugger",
+                            alias="host")
+    port: int = Field(55875, description="Port to connect back to for remote debugging")
 
+    @validator("egg_path")
+    def validate_egg_path(cls, value):
+        if not isinstance(value, Path):
+            value = Path(value)
+        if not value.exists():
+            raise RuntimeError(f"No file exists at '{value!s}' received by {cls.__name__} for egg path!")
+        elif not value.is_file():
+            raise RuntimeError(f"{cls.__name__} received '{value!s}' for egg path, but this is not a regular file!")
+        return value
+
+
+class ClientConfig(Serializable):
+    pycharm_debug_config: Optional[PycharmRemoteDebugConfig] = Field(None,
+                                                                     description="Config for remote Pycharm debugging",
+                                                                     alias="remote-debug")
     request_service: ConnectionConfig = Field(description="The config for connecting to the request service",
                                               alias="request-service")
     data_service: Optional[ConnectionConfig] = Field(None, description="The config for connecting to the data service",
