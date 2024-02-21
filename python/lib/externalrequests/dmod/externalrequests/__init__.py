2319d168078b88f18a6cc4a95011586287dcf650
Robert Bartel <robert.bartel@noaa.gov>
2024-02-21 11:00:55 -0500

    Add ExistingJobRequestHandler to package __init__.

--- python/lib/externalrequests/dmod/externalrequests/__init__.py
+++ python/lib/externalrequests/dmod/externalrequests/__init__.py
@@ -1,5 +1,6 @@
 from .auth_handler import AuthHandler
-from .maas_request_handlers import DatasetRequestHandler, MaaSRequestHandler, PartitionRequestHandler
+from .maas_request_handlers import (DatasetRequestHandler, MaaSRequestHandler, PartitionRequestHandler,
+                                    ExistingJobRequestHandler)
 from .model_exec_request_handler import ModelExecRequestHandler, NgenCalibrationRequestHandler
 from .evaluation_request_handler import EvaluationRequestHandler
 from .evaluation_request_handler import LaunchEvaluationMessage
