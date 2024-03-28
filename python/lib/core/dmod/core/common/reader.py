f4657abaa4cc34c3dcce34bd42632cb49e1a7f82
Robert Bartel <robert.bartel@noaa.gov>
2024-03-28 09:54:54 -0400

    Extend Reader with RepeatableReader interface.
    Adding extended interface for reader that can reset for repeated data
    reads.

--- python/lib/core/dmod/core/common/reader.py
+++ python/lib/core/dmod/core/common/reader.py
@@ -5,3 +5,12 @@ from typing_extensions import Protocol, runtime_checkable
 class Reader(Protocol):
     def read(self, size: int = -1, /) -> bytes:
         """EOF if empty b''."""
+
+
+class RepeatableReader(Reader):
+    """
+    Extension of ::class:`Reader` that provides a reset mechanism that allows its data can be read multiple times.
+    """
+
+    def reset(self):
+        """ Reset such that ::method:`read` returns to the start of the data this instance reads. """
