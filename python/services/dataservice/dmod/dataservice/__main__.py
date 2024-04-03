8d84275b4838601c61a3c943a833b5a6b2f6e6cf
Austin Raney <austin.raney@noaa.gov>
2024-04-03 17:42:59 -0400

    feat: configure data service using env vars, .env file, or secrets. drop cli support

--- python/services/dataservice/dmod/dataservice/__main__.py
+++ python/services/dataservice/dmod/dataservice/__main__.py
@@ -1,100 +1,74 @@
 import logging
+import sys
+import typing
+
+import pydantic
+
 logging.basicConfig(
     level=logging.INFO,
     format="%(asctime)s,%(msecs)d %(levelname)s: %(message)s",
-    datefmt="%H:%M:%S"
+    datefmt="%H:%M:%S",
 )
 
-import argparse
-from pathlib import Path
-from socket import gethostname
 
-from dmod.scheduler.job import DefaultJobUtilFactory
-from dmod.modeldata.data.filesystem_manager import FilesystemDatasetManager
 from dmod.modeldata.data.object_store_manager import ObjectStoreDatasetManager
-from dmod.core.dataset import DatasetType
+from dmod.scheduler.job import DefaultJobUtilFactory
 
-from . import name as package_name
 from .data_derive_util import DataDeriveUtil
 from .dataset_inquery_util import DatasetInqueryUtil
 from .dataset_manager_collection import DatasetManagerCollection
 from .service import (
     ActiveOperationTracker,
     DataProvisionManager,
+    DockerS3FSPluginHelper,
     RequiredDataChecksManager,
     ServiceManager,
     TempDataTaskManager,
-    DockerS3FSPluginHelper,
 )
-from .service_settings import ServiceSettings
+from .service_settings import DebugSettings, ServiceSettings, field_usage
 
 
 def main():
-    args = _handle_args()
+    settings = _handle_service_settings()
 
-    if args.pycharm_debug:
-        _setup_remote_debugging(args)
+    if settings.pycharm_debug:
+        _setup_remote_debugging()
     else:
         logging.info("Skipping data service remote debugging setup.")
 
-    listen_host = gethostname() if args.host is None else args.host
-    # Flip this here to be less confusing
-    use_obj_store = not args.no_obj_store
-    # TODO: DataProvisionManager requires object store, so this is currently required. Add functionality to allow usage
-    # without object store. fail fast for now.
-    assert use_obj_store, "Object store is currently required"
-
-    secrets_dir = Path('/run/secrets')
-
-    # Figure out Redis password, trying for a Docker secret first
-    if args.redis_pass_secret is not None:
-        redis_pass_secret_file = secrets_dir.joinpath(args.redis_pass_secret)
-        redis_pass = redis_pass_secret_file.read_text().strip()
-    else:
-        redis_pass = args.redis_pass
-
     # Initialize objects that will be injected and shared by service subsystems
-    service_settings = ServiceSettings()
     # Initialize a job util via the default factory, which requires some Redis params
-    job_util = DefaultJobUtilFactory.factory_create(redis_host=args.redis_host, redis_port=args.redis_port,
-                                                    redis_pass=redis_pass)
+    job_util = DefaultJobUtilFactory.factory_create(
+        redis_host=settings.redis_host,
+        redis_port=settings.redis_port,
+        redis_pass=settings.redis_pass,
+    )
     # Datasets creation and access go through this object
     dataset_manager_collection = DatasetManagerCollection()
-    data_derive_util = DataDeriveUtil(dataset_manager_collection=dataset_manager_collection)
-    dataset_inquery_util = DatasetInqueryUtil(dataset_manager_collection=dataset_manager_collection, derive_util=data_derive_util)
+    data_derive_util = DataDeriveUtil(
+        dataset_manager_collection=dataset_manager_collection
+    )
+    dataset_inquery_util = DatasetInqueryUtil(
+        dataset_manager_collection=dataset_manager_collection,
+        derive_util=data_derive_util,
+    )
 
-    # TODO: need to adjust arg groupings to allow for this to be cleaned up some
-    access_key = (
-        None
-        if args.obj_store_access_key is None
-        else (secrets_dir / args.obj_store_access_key).read_text().strip()
-    )
-    secret_key = (
-        None
-        if args.obj_store_secret_key is None
-        else (secrets_dir / args.obj_store_secret_key).read_text().strip()
-    )
     object_store_dataset_manager = _init_object_store_dataset_manager(
-        obj_store_host=args.obj_store_host,
-        port=args.obj_store_port,
-        access_key=access_key,
-        secret_key=secret_key,
+        obj_store_host=settings.object_store_host,
+        port=settings.object_store_port,
+        access_key=settings.object_store_exec_user_name,
+        secret_key=settings.object_store_exec_user_passwd,
     )
     dataset_manager_collection.add(object_store_dataset_manager)
 
     docker_s3fs_plugin_helper = _init_docker_s3fs_plugin_helper(
         dataset_manager_collection=dataset_manager_collection,
-        access_key=access_key,
-        secret_key=secret_key,
-        settings=service_settings,
+        access_key=settings.object_store_exec_user_name,
+        secret_key=settings.object_store_exec_user_passwd,
+        settings=settings,
     )
 
-    if args.file_dataset_config_dir is not None:
-        filesystem_dataset_manager = _init_filesystem_dataset_manager(
-            Path(args.file_dataset_config_dir)
-        )
-        dataset_manager_collection.add(filesystem_dataset_manager)
-
+    # TODO: Add functionality to allow usage with other backend storage providers
 
     # count is used to signal when it is okay to remove temporary datasets
     count = ActiveOperationTracker()
@@ -106,20 +80,24 @@ def main():
         checks_underway_tracker=count,
         dataset_inquery_util=dataset_inquery_util,
     )
-    data_provision_manager = DataProvisionManager(job_util=job_util,
-                                                  dataset_manager_collection=dataset_manager_collection,
-                                                  docker_s3fs_helper=docker_s3fs_plugin_helper,
-                                                  data_derive_util=data_derive_util,
-                                                  provision_underway_tracker=count,
-                                                  )
-    temp_datasets_manager = TempDataTaskManager(dataset_manager_collection=dataset_manager_collection, safe_to_exec_tracker=count)
+    data_provision_manager = DataProvisionManager(
+        job_util=job_util,
+        dataset_manager_collection=dataset_manager_collection,
+        docker_s3fs_helper=docker_s3fs_plugin_helper,
+        data_derive_util=data_derive_util,
+        provision_underway_tracker=count,
+    )
+    temp_datasets_manager = TempDataTaskManager(
+        dataset_manager_collection=dataset_manager_collection,
+        safe_to_exec_tracker=count,
+    )
 
     # Handles websocket communication and async task loop
     service_manager = ServiceManager(
         job_util=job_util,
-        listen_host=listen_host,
-        port=args.port,
-        ssl_dir=Path(args.ssl_dir),
+        listen_host=settings.host,
+        port=settings.port,
+        ssl_dir=settings.ssl_dir,
         dataset_manager_collection=dataset_manager_collection,
         dataset_inquery_util=dataset_inquery_util,
     )
@@ -132,122 +110,61 @@ def main():
     service_manager.run()
 
 
-def _handle_args():
-    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('--host',
-                        help='Set the appropriate listening host name or address value (NOTE: must match SSL cert)',
-                        dest='host',
-                        default=None)
-    parser.add_argument('--port',
-                        help='Set the appropriate listening port value',
-                        dest='port',
-                        default='3012')
-    parser.add_argument('--ssl-dir',
-                        help='Change the base directory when using SSL certificate and key files with default names',
-                        dest='ssl_dir',
-                        default=None)
-    parser.add_argument('--cert',
-                        help='Specify path for a particular SSL certificate file to use',
-                        dest='cert_path',
-                        default=None)
-    parser.add_argument('--key',
-                        help='Specify path for a particular SSL private key file to use',
-                        dest='key_path',
-                        default=None)
-    parser.add_argument('--file-dataset-config-dir',
-                        help='Path to directory containing serialized, filesystem-based Dataset objects',
-                        dest='file_dataset_config_dir',
-                        default=None)
-    parser.add_argument('--object-store-host',
-                        help='Set hostname for connection to object store',
-                        dest='obj_store_host',
-                        default='minio-proxy')
-    parser.add_argument('--object-store-port',
-                        help='Set port for connection to object store',
-                        dest='obj_store_port',
-                        default=9000)
-    parser.add_argument('--object-store-user-secret-name',
-                        help='Set name of the Docker secret containing the object store user access key',
-                        dest='obj_store_access_key',
-                        default=None)
-    parser.add_argument('--object-store-passwd-secret-name',
-                        help='Set name of the Docker secret containing the object store user secret key',
-                        dest='obj_store_secret_key',
-                        default=None)
-    parser.add_argument('--no-object-store',
-                        help='Disable object store functionality and do not try to connect to one',
-                        dest='no_obj_store',
-                        action='store_true',
-                        default=False)
-    parser.add_argument('--redis-host',
-                        help='Set the host value for making Redis connections',
-                        dest='redis_host',
-                        default='myredis')
-    parser.add_argument('--redis-port',
-                        help='Set the port value for making Redis connections',
-                        dest='redis_port',
-                        default=6379)
-    parser.add_argument('--redis-pass',
-                        help='Set the password value for making Redis connections',
-                        dest='redis_pass',
-                        default='noaaOwp')
-    parser.add_argument('--redis-pass-secret-name',
-                        help='Set the name of the Docker secret containing the password for Redis connections',
-                        dest='redis_pass_secret',
-                        default=None)
-    parser.add_argument('--pycharm-remote-debug',
-                        help='Activate Pycharm remote debugging support',
-                        dest='pycharm_debug',
-                        action='store_true')
-    parser.add_argument('--pycharm-remote-debug-egg',
-                        help='Set path to .egg file for Python remote debugger util',
-                        dest='remote_debug_egg_path',
-                        default='/pydevd-pycharm.egg')
-    parser.add_argument('--remote-debug-host',
-                        help='Set remote debug host to connect back to debugger',
-                        dest='remote_debug_host',
-                        default='host.docker.internal')
-    parser.add_argument('--remote-debug-port',
-                        help='Set remote debug port to connect back to debugger',
-                        dest='remote_debug_port',
-                        type=int,
-                        default=55871)
+def _handle_service_settings() -> ServiceSettings:
+    return _handle_settings(ServiceSettings)
 
-    parser.prog = package_name
-    return parser.parse_args()
 
-def _setup_remote_debugging(args: argparse.Namespace):
+def _handle_debug_settings() -> DebugSettings:
+    return _handle_settings(DebugSettings)
+
+
+def _setup_remote_debugging():
+    settings = _handle_debug_settings()
     logging.info("Preparing remote debugging connection for data service.")
-    if args.remote_debug_egg_path == '':
-        print('Error: set to debug with Pycharm, but no path to remote debugger egg file provided')
-        exit(1)
-    if not Path(args.remote_debug_egg_path).exists():
-        print('Error: no file at given path to remote debugger egg file "{}"'.format(args.remote_debug_egg_path))
-        exit(1)
-    import sys
-    sys.path.append(args.remote_debug_egg_path)
-    import pydevd_pycharm
-    try:
-        pydevd_pycharm.settrace(args.remote_debug_host, port=args.remote_debug_port, stdoutToServer=True,
-                                stderrToServer=True)
-    except Exception as error:
-        msg = 'Warning: could not set debugging trace to {} on {} due to {} - {}'
-        print(msg.format(args.remote_debug_host, args.remote_debug_port, error.__class__.__name__, str(error)))
-
-
-def _init_filesystem_dataset_manager(
-    file_dataset_config_dir: Path,
-) -> FilesystemDatasetManager:
-    logging.info(
-        "Initializing manager for {} type datasets".format(DatasetType.FILESYSTEM.name)
-    )
-    mgr = FilesystemDatasetManager(serialized_files_directory=file_dataset_config_dir)
-    logging.info(
-        "{} initialized with {} existing datasets".format(
-            mgr.__class__.__name__, len(mgr.datasets)
+    if not settings.pycharm_remote_debug_egg.exists():
+        print(
+            f'Error: no file at given path to remote debugger egg file "{settings.pycharm_remote_debug_egg!s}"',
+            file=sys.stderr,
         )
-    )
-    return mgr
+        sys.exit(1)
+    sys.path.append(str(settings.pycharm_remote_debug_egg))
+    import pydevd_pycharm
+
+    try:
+        pydevd_pycharm.settrace(
+            settings.remote_debug_host,
+            port=settings.remote_debug_port,
+            stdoutToServer=True,
+            stderrToServer=True,
+        )
+    except Exception as error:
+        msg = "Warning: could not set debugging trace to {} on {} due to {} - {}"
+        print(
+            msg.format(
+                settings.remote_debug_host,
+                settings.remote_debug_port,
+                error.__class__.__name__,
+                str(error),
+            ),
+            file=sys.stderr,
+        )
+
+
+_T = typing.TypeVar("_T", bound=pydantic.BaseSettings)
+
+
+def _handle_settings(cls: typing.Type[_T], **kwargs) -> _T:
+    try:
+        return cls(**kwargs)
+    except pydantic.ValidationError as e:
+        usage: typing.List[str] = []
+        for err in e.errors():
+            field_name: str = err["loc"][0]
+            msg = f"{field_usage(ServiceSettings, field_name)}\nError: {err['msg']}\n"
+            usage.append(msg)
+        error_msg = "\n".join(usage)
+        print(f"Service configuration failure: {error_msg}", file=sys.stderr)
+        sys.exit(1)
 
 
 def _init_object_store_dataset_manager(
@@ -272,7 +189,7 @@ def _init_docker_s3fs_plugin_helper(
     secret_key: str,
     settings: ServiceSettings,
     *args,
-    **kwargs
+    **kwargs,
 ) -> DockerS3FSPluginHelper:
     s3fs_url_proto = settings.s3fs_url_protocol
     s3fs_url_host = settings.s3fs_url_host
@@ -294,11 +211,10 @@ def _init_docker_s3fs_plugin_helper(
         docker_plugin_alias=settings.s3fs_plugin_alias,
         obj_store_url=s3fs_helper_url,
         *args,
-        **kwargs
+        **kwargs,
     )
     return docker_s3fs_helper
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
