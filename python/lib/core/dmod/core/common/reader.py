fd4034d5f1715ed88df9e90b64ca848f13678450
Robert Bartel <robert.bartel@noaa.gov>
2024-04-03 16:18:42 -0400

    Add Seeker protocol definition.

--- python/lib/core/dmod/core/common/reader.py
+++ python/lib/core/dmod/core/common/reader.py
@@ -1,10 +1,17 @@
 from typing_extensions import Protocol, runtime_checkable
+from os import SEEK_SET
 
 
 @runtime_checkable
 class Reader(Protocol):
     def read(self, size: int = -1, /) -> bytes:
         """EOF if empty b''."""
+
+
+@runtime_checkable
+class Seeker(Protocol):
+    def seek(self, offset: int, whence: int = SEEK_SET):
+        """ Change the position to the given offset. """
 
 
 class RepeatableReader(Reader):
