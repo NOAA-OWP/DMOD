bfd6272ef482071a0697e4028072b53d4b474845
christopher.tubbs <christopher.tubbs@noaa.gov>
2025-03-11 15:52:36 -0400

    Removed the now unused next.js experiment and updated the github action for testing to use a more recent version of actions/cache

--- ts/dmod_gui/hooks/useToggle.ts
+++
@@ -1,9 +0,0 @@
-import { useCallback, useState } from 'react';
-
-export const useToggle = (initialState: boolean = false): [boolean, any] => {
-    const [state, setState] = useState<boolean>(initialState);
-    const toggle = useCallback((): void => setState(state => !state), []);
-    return [state, toggle]
-}
-
-export default useToggle;
--- ts/dmod_gui/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-import Head from '../components/Head'
-
-export default function Home() {
-  return (
-    <Head title="OWP Model as a Service" />
-  )
-}
